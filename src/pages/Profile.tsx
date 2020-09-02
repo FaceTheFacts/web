4572a5558965d90ece44c0b7610a07a8fc737d09
Kintsugi <liam_going@hotmail.com>
2020-09-02 12:19:49 +0200

    Profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,10 +1,11 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonItem, IonIcon, IonLabel, IonButton } from '@ionic/react';
+import { pin, wifi, wine, warning, walk } from 'ionicons/icons';
 import { useParams } from 'react-router';
 import ExploreContainer from '../components/ExploreContainer';
 import './Page.css';
 
-const Profile: React.FC = () => {
+export const Profile: React.FC = () => {
 
   const { name } = useParams<{ name: string; }>();
 
@@ -12,23 +13,60 @@ const Profile: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonButtons slot="start">
-            <IonMenuButton />
-          </IonButtons>
-          <IonTitle>{name} - Profile</IonTitle>
+          <IonTitle>CardExamples</IonTitle>
         </IonToolbar>
       </IonHeader>
+      <IonContent>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+            <IonCardTitle>Card Title</IonCardTitle>
+          </IonCardHeader>
 
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">{name}</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name={name} />
+          <IonCardContent>
+            Keep close to Nature's heart... and break clear away, once in awhile,
+            and climb a mountain or spend a week in the woods. Wash your spirit clean.
+      </IonCardContent>
+        </IonCard>
+
+        <IonCard>
+          <IonItem>
+            <IonIcon icon={pin} slot="start" />
+            <IonLabel>ion-item in a card, icon left, button right</IonLabel>
+            <IonButton fill="outline" slot="end">View</IonButton>
+          </IonItem>
+
+          <IonCardContent>
+            This is content, without any paragraph or header tags,
+            within an ion-cardContent element.
+      </IonCardContent>
+        </IonCard>
+
+        <IonCard>
+          <IonItem href="#" className="ion-activated">
+            <IonIcon icon={wifi} slot="start" />
+            <IonLabel>Card Link Item 1 activated</IonLabel>
+          </IonItem>
+
+          <IonItem href="#">
+            <IonIcon icon={wine} slot="start" />
+            <IonLabel>Card Link Item 2</IonLabel>
+          </IonItem>
+
+          <IonItem className="ion-activated">
+            <IonIcon icon={warning} slot="start" />
+            <IonLabel>Card Button Item 1 activated</IonLabel>
+          </IonItem>
+
+          <IonItem>
+            <IonIcon icon={walk} slot="start" />
+            <IonLabel>Card Button Item 2</IonLabel>
+          </IonItem>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
 };
 
 export default Profile;
+
