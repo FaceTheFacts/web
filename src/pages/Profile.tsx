524f787dddd2a3f13bc109f151f632f292252b7b
Kintsugi <liam_going@hotmail.com>
2020-09-02 14:02:36 +0200

    .

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonItem, IonIcon, IonLabel, IonButton } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonItem, IonIcon, IonLabel, IonButton, IonButtons, IonMenuButton } from '@ionic/react';
 import { pin, wifi, wine, warning, walk } from 'ionicons/icons';
 import { useParams } from 'react-router';
-import ExploreContainer from '../components/ExploreContainer';
 import './Page.css';
+import ExploreContainer from '../components/ExploreContainer';
+import TopicFilter from '../components/TopicFilter';
+import SubHeading from '../components/SubHeading';
 
 export const Profile: React.FC = () => {
 
@@ -12,20 +14,23 @@ export const Profile: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
+
         <IonToolbar>
-          <IonTitle>CardExamples</IonTitle>
+          <IonTitle>Philipp Amthor</IonTitle>
         </IonToolbar>
+
       </IonHeader>
+
       <IonContent>
         <IonCard>
+          
           <IonCardHeader>
-            <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
-            <IonCardTitle>Card Title</IonCardTitle>
+            <IonCardSubtitle>Beschlussempfehlung</IonCardSubtitle>
+            <IonCardTitle>Bundeswehreinsatz in Afghanistan</IonCardTitle>
           </IonCardHeader>
 
           <IonCardContent>
-            Keep close to Nature's heart... and break clear away, once in awhile,
-            and climb a mountain or spend a week in the woods. Wash your spirit clean.
+            .. .. .. ..
       </IonCardContent>
         </IonCard>
 
