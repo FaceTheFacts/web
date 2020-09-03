dd0b60073508f575bc7084bd80792baf8261a4dc
Fabian Volkers <fabian.volkers@gmail.com>
2020-09-03 22:51:05 +0200

    start implementing camera functionality

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 ReactDOM.render(<App />, document.getElementById('root'));
 
+defineCustomElements(window);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
