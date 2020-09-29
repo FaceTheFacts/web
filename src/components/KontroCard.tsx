d678576394cad42cf0fa9271d0a149c8b2df4fec
Fabian Volkers <fabian.volkers@gmail.com>
2020-09-29 15:45:50 +0200

    add card inside card

--- src/components/KontroCard.tsx
+++ src/components/KontroCard.tsx
@@ -1,28 +1,33 @@
-import React from 'react';
-import { IonCardContent, IonButton, IonCard, IonContent, IonCardHeader, IonCardTitle } from '@ionic/react';
+import React from "react";
+import {
+	IonCardContent,
+	IonButton,
+	IonCard,
+	IonContent,
+	IonCardHeader,
+	IonCardTitle,
+} from "@ionic/react";
 
 import "./KontroCard.css";
-
-
 
 interface ContainerProps {
 	kontro: string;
 }
 
-
-
 const KontroCard: React.FC<ContainerProps> = ({ kontro }) => {
-	
 	//State Hook to alter state when clicked
-	const [isOpen, setIsOpen] = React.useState (false);
-
+	const [isOpen, setIsOpen] = React.useState(false);
 
 	return (
-			<IonCard className = "kontro-layout">
-				<IonCardHeader className="accordion" onClick={() => setIsOpen(!isOpen)}>
-						{kontro} {isOpen? "open" : "closed"}
-				</IonCardHeader>
-			</IonCard>        
+		<IonCard className="kontro-layout">
+			<IonCardHeader
+				className="accordion"
+				onClick={() => setIsOpen(!isOpen)}
+			>
+				{kontro} {isOpen ? "open" : "closed"}
+			</IonCardHeader>
+			<IonCard></IonCard>
+		</IonCard>
 	);
 };
 
