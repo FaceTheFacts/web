171c50e343aee0125edd1eba05aec3e7bb2d34e8
Fabian Volkers <fabian.volkers@gmail.com>
2020-10-01 10:12:30 +0200

    add image and url to kontro props

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -38,12 +38,35 @@ import KontroCard from "../components/KontroCard";
 import "./Profile.css";
 
 // Hardcoded Kontroversen until we connect to our API
-const kontros =["Lobbyismus-Affäre",'"Hey Rezo, du alter Zerstörer!"'];
+const kontros = [
+	{
+		label: "Lobbyismus-Affäre",
+		articles: [
+			{
+				label: "Article 1",
+				image: "Image url",
+				url: "article url",
+			},
+		],
+	},
+	{
+		label: '"Hey Rezo, du alter Zerstörer!"',
+		articles: [
+			{
+				label: "Article 1",
+				image: "Image url",
+				url: "article url",
+			},
+		],
+	},
+];
 
 // Hardcoded chips until we connect to our API
 const chips = ["CDU/CSU", "Mitglied des Bundestags"];
 
-{/* Hardcoded for now until passed in from the API */}
+{
+	/* Hardcoded for now until passed in from the API */
+}
 const nebens = [
 	{
 		subtitle: "Mitglied des Verwaltungsrates",
@@ -63,13 +86,8 @@ const votes = [
 			abstain: 21,
 			none: 173,
 		},
-	}
-	
+	},
 ];
-
-
-
-	
 
 /* Define the React component (FC stands for Functional Components, as opposed to class-based components) */
 const Profile: React.FC = () => {
@@ -81,80 +99,68 @@ const Profile: React.FC = () => {
 		<IonPage>
 			{" "}
 			{/* Page Tag */}
-				<IonHeader>
-					{" "}
-					{/* Header Tag */}
-					
-						{" "}
-						{/* Toolbar tag, this is the title bar / top bar */}					
-
-				</IonHeader>
-
+			<IonHeader>
+				{" "}
+				{/* Header Tag */}{" "}
+				{/* Toolbar tag, this is the title bar / top bar */}
+			</IonHeader>
 			{/* Here the content of our page starts */}
 			<IonContent>
-
 				{/* ProfileImg component that holds all the images of the politicians. 
 				Right now the name property is not being used, maybe it's an idea to dynamically pass in images 
 				to make our life easier when we add the profiles images of new politicians.
 				The politicans name is included on the ProfileImg */}
-				<div className ="profile-header">
+				<div className="profile-header">
 					{/* ProfileImage needs a gradient overlay added! */}
 					<ProfileImage name={name} />
 					<ProfileName name={name} />
 					{/* For each vote in votes, render a VoteCard component */}
 					{chips.map((chip, index) => {
 						return <ProfileChip chip={chip} />;
-					})}	
-				{/* Here we include the Fab menu button */}
+					})}
+					{/* Here we include the Fab menu button */}
 					<IonFab vertical="top" horizontal="end">
-						<MenuButton/>
+						<MenuButton />
 					</IonFab>
 				</div>
 
-
 				{/* Subheading-button created by using a div for the background color and placing a button over part of it*/}
-				
+
 				<div className="subheading-button-underlay">
-					<SubHeadingButtonInfo name ="Abstimmungsverhalten >" />
+					<SubHeadingButtonInfo name="Abstimmungsverhalten >" />
 				</div>
 
-					<div className="grey-back">
-						{/* For each vote in votes, render a VoteCard component */}
-						{votes.map((vote, index) => {
-							return <VoteCard vote={vote} />;
-						})}
-					</div>		
-					
+				<div className="grey-back">
+					{/* For each vote in votes, render a VoteCard component */}
+					{votes.map((vote, index) => {
+						return <VoteCard vote={vote} />;
+					})}
+				</div>
 
 				{/* Pass name to Subheading to be in control of the sub heading text */}
-				
+
 				<div>
 					<SubHeadingInfo name="Kontroversen" />
 				</div>
-				
-					
-					<div className="grey-back">
-						{/* For each item in kontro, render a KontroCard component */}
-						{kontros.map((kontro, index) => {
-								return <KontroCard kontro={kontro} />;
-						})}	
-					</div>
-					
-				
+
+				<div className="grey-back">
+					{/* For each item in kontro, render a KontroCard component */}
+					{kontros.map((kontro, index) => {
+						return <KontroCard kontro={kontro} />;
+					})}
+				</div>
+
 				<div>
 					<SubHeadingInfo name="Bekannte Nebentätigkeiten" />
 				</div>
-				
-					<div className="last-grey-back">
-						{/* For each item in title, render a NebenCard component */}
-						{nebens.map((neben, index) => {
-								return <NebenCard neben={neben} />;
-						})}	
-						
-					</div>
-					
+
+				<div className="last-grey-back">
+					{/* For each item in title, render a NebenCard component */}
+					{nebens.map((neben, index) => {
+						return <NebenCard neben={neben} />;
+					})}
+				</div>
 			</IonContent>
-			
 		</IonPage>
 	);
 };
