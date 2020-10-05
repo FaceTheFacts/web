14122cc79d356c0235db54138a9814326fccb36e
Fabian Volkers <fabian.volkers@gmail.com>
2020-10-05 16:38:08 +0200

    refactor into smart component

--- src/components/SubHeading.tsx
+++ src/components/SubHeading.tsx
@@ -1,16 +1,43 @@
 import React from "react";
-import { IonFabButton, IonFab} from '@ionic/react';
+import { IonButton, IonFabButton, IonFab } from "@ionic/react";
 import "./SubHeading.css";
 
-interface ContainerProps {
-	name?: string;
+interface SubheadingProps {
+	icon?: string;
+	button?: string;
+	name: string;
 }
 
 /* This is just a simple component to repeat the subheading wherever we need it */
-const SubHeading: React.FC<ContainerProps> = ({ name }) => {
+const SubHeading: React.FC<SubheadingProps> = ({ name, button, icon }) => {
+	var heading = null;
+	var headingIcon = null;
+
+	if (button !== undefined) {
+		heading = (
+			<IonButton className="sub-heading-button">
+				<strong>{name}</strong>
+			</IonButton>
+		);
+	} else {
+		heading = <strong>{name}</strong>;
+	}
+
+	if (icon !== undefined) {
+		const iconSrc = `../assets/icon/${icon}`;
+		headingIcon = (
+			<IonFab horizontal="end" slot="fixed" className="scrollhidden">
+				<IonFabButton className="info-button">
+					<img src={iconSrc} />
+				</IonFabButton>
+			</IonFab>
+		);
+	}
+
 	return (
 		<div className="sub-heading">
-			<strong>{name}</strong>
+			{heading}
+			{headingIcon}
 		</div>
 	);
 };
