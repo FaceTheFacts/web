e2b53a78c829d8e96417ae02de1a716329db1fa1
Fabian Volkers <fabian.volkers@gmail.com>
2020-10-06 15:23:05 +0200

    add camera modal

--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -1,38 +1,70 @@
 import {
-	IonButtons,
+	IonButton,
+	IonInput,
 	IonContent,
+	IonImg,
+	IonModal,
 	IonHeader,
 	IonMenuButton,
 	IonPage,
 	IonTitle,
 	IonToolbar,
 } from "@ionic/react";
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router";
-import ExploreContainer from "../components/ExploreContainer";
-import "./Page.css";
+import CameraView from "../components/CameraView";
+import "./Discover.css";
 
 const Discover: React.FC = () => {
 	const { name } = useParams<{ name: string }>();
-
+	const [text, setText] = useState<string>();
+	const [showCamera, setShowCamera] = useState<boolean>(false);
 	return (
 		<IonPage>
-			<IonHeader>
+			{/* <IonHeader>
 				<IonToolbar>
 					<IonButtons slot="start">
 						<IonMenuButton />
 					</IonButtons>
-					<IonTitle>{name}</IonTitle>
+					<IonTitle>Discover</IonTitle>
 				</IonToolbar>
-			</IonHeader>
+			</IonHeader> */}
 
 			<IonContent fullscreen>
-				<IonHeader collapse="condense">
+				<IonModal isOpen={showCamera} cssClass="my-custom-class">
+					<CameraView />
+					<IonButton onClick={() => setShowCamera(false)}>
+						Close Camera
+					</IonButton>
+				</IonModal>
+				<div
+					className="discover-page"
+					onClick={() => setShowCamera(true)}
+				>
+					<div className="camera-icon-background">
+						<IonImg
+							id="camera-square"
+							src="../assets/icon/camera-square.svg"
+						></IonImg>
+						<IonImg
+							id="camera-icon"
+							src="../assets/icon/camera-solid.svg"
+						></IonImg>
+					</div>
+
+					<IonInput
+						className="search-bar"
+						value={text}
+						placeholder="Kandidat:in, PLZ oder Ort suchen"
+						disabled={true}
+						onIonChange={(e) => setText(e.detail.value!)}
+					></IonInput>
+					{/* <IonHeader collapse="condense">
 					<IonToolbar>
-						<IonTitle size="large">{name}</IonTitle>
+						<IonTitle size="large">Discover</IonTitle>
 					</IonToolbar>
-				</IonHeader>
-				<ExploreContainer name={name} />
+				</IonHeader> */}
+				</div>
 			</IonContent>
 		</IonPage>
 	);
