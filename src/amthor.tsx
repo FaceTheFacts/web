0fdb458920401d26e3dd0547f7bcdd944b1f80e0
Fabian Volkers <fabian.volkers@gmail.com>
2020-10-17 18:52:14 +0200

    move hardcoded amthor to separate file

---
+++ src/amthor.tsx
@@ -0,0 +1,13 @@
+export const amthor = {
+    name: 'Philipp Amthor',
+	chips: ['CDU/CSU', 'Mitglied des Bundestags'],
+	image:
+		new URL('https://www.abgeordnetenwatch.de/sites/default/files/styles/opengraph_image/public/politicians-profile-pictures/philipp_amthor.jpg?itok=_-cUhevr'),
+	party: 'CDU/CSU',
+	sideJobs: [
+		{
+			organisation: 'Sparkasse Uecker-Randow',
+			position: 'Mitglied des Verwaltungsrates',
+		},
+	],
+}
