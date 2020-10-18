ad30e8d6883db1d137b0bcf12b4e4ffaac063eea
Fabian Volkers <fabian.volkers@gmail.com>
2020-10-18 11:21:58 +0200

    fix linter errors in kontro article

--- src/components/KontroArticle.tsx
+++ src/components/KontroArticle.tsx
@@ -6,11 +6,11 @@ interface ContainerProps {
 	article: ControversyArticle;
 }
 
-const KontroArticle: React.FC<ContainerProps> = ({ article }) => {
-	const publisher_logo = `../assets/image/${article.publisher}.png`;
+const KontroArticle: React.FC<ContainerProps> = ({ article }: ContainerProps) => {
+	const publisherLogo = `../assets/image/${article.publisher}.png`;
 	return (
 		<IonCard className="article-card" href={String(article.url)}>
-			<img className="publisher-image" src={publisher_logo}></img>
+			<img className="publisher-image" src={publisherLogo}></img>
 			<img className="article-image" src={String(article.image)}></img>
 			<IonCardHeader className="article-header">
 				<IonCardSubtitle className="article-label">{article.label}</IonCardSubtitle>
