12f057da191b2bf6dfe942045bd9a3cd636a142c
Fabian Volkers <fabian.volkers@gmail.com>
2020-10-18 13:49:13 +0200

    add return types to serviceworker.ts

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -23,7 +23,7 @@ type Config = {
 	onUpdate?: (registration: ServiceWorkerRegistration) => void;
 };
 
-export function register(config?: Config) {
+export function register(config?: Config): void {
 	if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
 		// The URL constructor is available in all browsers that support SW.
 		const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
@@ -57,16 +57,16 @@ export function register(config?: Config) {
 	}
 }
 
-function registerValidSW(swUrl: string, config?: Config) {
+function registerValidSW(swUrl: string, config?: Config): void {
 	navigator.serviceWorker
 		.register(swUrl)
 		.then((registration) => {
-			registration.onupdatefound = () => {
+			registration.onupdatefound = (): void => {
 				const installingWorker = registration.installing;
 				if (installingWorker == null) {
 					return;
 				}
-				installingWorker.onstatechange = () => {
+				installingWorker.onstatechange = (): void => {
 					if (installingWorker.state === 'installed') {
 						if (navigator.serviceWorker.controller) {
 							// At this point, the updated precached content has been fetched,
@@ -101,7 +101,7 @@ function registerValidSW(swUrl: string, config?: Config) {
 		});
 }
 
-function checkValidServiceWorker(swUrl: string, config?: Config) {
+function checkValidServiceWorker(swUrl: string, config?: Config): void {
 	// Check if the service worker can be found. If it can't reload the page.
 	fetch(swUrl, {
 		headers: { 'Service-Worker': 'script' },
@@ -129,7 +129,7 @@ function checkValidServiceWorker(swUrl: string, config?: Config) {
 		});
 }
 
-export function unregister() {
+export function unregister(): void {
 	if ('serviceWorker' in navigator) {
 		navigator.serviceWorker.ready.then((registration) => {
 			registration.unregister();
