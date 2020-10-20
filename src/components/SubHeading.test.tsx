5be7e0ff8429485e992c6ba6c14351aba482c3eb
Fabian Volkers <fabian.volkers@gmail.com>
2020-10-20 10:50:50 +0200

    wrap tests in describe block based on type of test

--- src/components/SubHeading.test.tsx
+++ src/components/SubHeading.test.tsx
@@ -23,78 +23,83 @@ afterEach(() => {
 	}
 });
 
-it('renders with correct heading', () => {
-	// given
-	const subheading = 'Sub Heading';
-
-	// when
-	act(() => {
-		render(<SubHeading name={subheading} />, container);
-	});
-
-	// then
-	if (container !== null) {
-		expect(container.textContent).toBe(subheading);
-	}
-});
-
-it('renders with a button', () => {
-	// given
-	const subheading = 'test';
-	const buttonAction = 'votes';
-
-	// when
-	act(() => {
-		render(<SubHeading name={subheading} buttonAction={buttonAction} />, container);
-	});
-
-	// then
-	if (container !== null) {
-		expect(container.getElementsByTagName('ion-button').length).toBe(1);
-	}
-});
-
-it('renders with an icon', () => {
-	// given
-	const subheading = 'test';
-	const icon = 'infobutton.svg';
-
-	// when
-	act(() => {
-		render(<SubHeading name={subheading} icon={icon} />, container);
-	});
-
-	// then
-	if (container !== null) {
-		expect((getByTestId(container, 'subheading-icon') as HTMLImageElement).src).toContain(
-			`/assets/icon/${icon}`
-		);
-	}
-});
-
-it('redirects to correct path', () => {
-	// given
-	const subheading = 'test';
-	const icon = 'infobutton.svg';
-	const actionUrl = '/politician/1/votes';
-
-	const history = createMemoryHistory();
-
-	// when
-	act(() => {
-		render(
-			<Router history={history}>
-				<SubHeading name={subheading} icon={icon} buttonAction={actionUrl} />
-			</Router>,
-			container
-		);
+describe('unittests', () => {
+	it('renders with correct heading', () => {
+		// given
+		const subheading = 'Sub Heading';
+	
+		// when
+		act(() => {
+			render(<SubHeading name={subheading} />, container);
+		});
+	
+		// then
 		if (container !== null) {
-			getByTestId(container, 'subheading-button').click();
+			expect(container.textContent).toBe(subheading);
 		}
 	});
+	
+	it('renders with a button', () => {
+		// given
+		const subheading = 'test';
+		const buttonAction = 'votes';
+	
+		// when
+		act(() => {
+			render(<SubHeading name={subheading} buttonAction={buttonAction} />, container);
+		});
+	
+		// then
+		if (container !== null) {
+			expect(container.getElementsByTagName('ion-button').length).toBe(1);
+		}
+	});
+	
+	it('renders with an icon', () => {
+		// given
+		const subheading = 'test';
+		const icon = 'infobutton.svg';
+	
+		// when
+		act(() => {
+			render(<SubHeading name={subheading} icon={icon} />, container);
+		});
+	
+		// then
+		if (container !== null) {
+			expect((getByTestId(container, 'subheading-icon') as HTMLImageElement).src).toContain(
+				`/assets/icon/${icon}`
+			);
+		}
+	});
+})
 
-	// then
-	if (container !== null) {
-		expect(history.location.pathname).toBe(actionUrl);
-	}
-});
+describe('integration tests', () => {
+	it('[Router]: redirects to correct path', () => {
+		// given
+		const subheading = 'test';
+		const icon = 'infobutton.svg';
+		const actionUrl = '/politician/1/votes';
+	
+		const history = createMemoryHistory();
+	
+		// when
+		act(() => {
+			render(
+				<Router history={history}>
+					<SubHeading name={subheading} icon={icon} buttonAction={actionUrl} />
+				</Router>,
+				container
+			);
+			if (container !== null) {
+				getByTestId(container, 'subheading-button').click();
+			}
+		});
+	
+		// then
+		if (container !== null) {
+			expect(history.location.pathname).toBe(actionUrl);
+		}
+	});
+})
+
