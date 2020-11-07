2304d2506b52d5a90ddf0d7592c1da8b5242fc00
Fabian Volkers <fabian.volkers@gmail.com>
2020-11-07 21:18:14 +0100

    add command line option for platform

--- pytest/conftest.py
+++ pytest/conftest.py
@@ -8,13 +8,64 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 #from selenium.webdriver import SafariOptions
 
 def pytest_addoption(parser):
+    """
+    add cli parser options
+    """
     parser.addoption("--url", action="store", default="http://localhost:8100", help="url")
-"""     parser.addoption(
-        "--browser", action="store", default="chrome", help="browser: chrome or firefox"
-    ) """
+    parser.addoption(
+        "--platform",
+        action="store",
+        default="",
+        help="""platform on which to run the tests [ios,android,chrome,firefox,safari]. 
+        Specify multiple seperated by comma (e.g: chrome,firefox)"""
+        )
+
+
+def pytest_configure(config):
+    """
+    register markers for platforms
+    """
+    config.addinivalue_line("markers", "chrome: mark test to run on chrome")
+    config.addinivalue_line("markers", "firefox: mark test to run on firefox")
+    config.addinivalue_line("markers", "safari: mark test to run on safari")
+    config.addinivalue_line("markers", "ios: mark test to run on ios")
+    config.addinivalue_line("markers", "android: mark test to run on android")
+
+
+def pytest_collection_modifyitems(config, items):
+    """
+    skip platforms if specified
+    """
+    if config.getoption("--platform") == "":
+        print('platform not specified')
+        # --platform not specified in cli, run all tests
+        return
+
+    platform = config.getoption("--platform")
+
+    skip_ff = pytest.mark.skip(reason="need --platform=firefox option to run")
+    skip_chrome = pytest.mark.skip(reason="need --platform=chrome option to run")
+    skip_safari = pytest.mark.skip(reason="need --platform=safari option to run")
+    skip_ios = pytest.mark.skip(reason="need --platform=ios option to run")
+    skip_android = pytest.mark.skip(reason="need --platform=android option to run")
+
+    for item in items:
+        if 'firefox' in item.keywords and not 'firefox' in platform:
+            item.add_marker(skip_ff)
+        elif "chrome" in item.keywords and not 'chrome' in platform:
+            item.add_marker(skip_chrome)
+        elif "safari" in item.keywords and not 'safari' in platform:
+            item.add_marker(skip_safari)
+        elif "ios" in item.keywords and not 'ios' in platform:
+            item.add_marker(skip_ios)
+        elif "android" in item.keywords and not 'android' in platform:
+            item.add_marker(skip_android)
 
 @pytest.fixture(scope='class')
 def init_chrome(request):
+    """
+    initialise chromedriver
+    """
     chrome_options = ChromeOptions()
     #chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
@@ -30,6 +81,9 @@ def init_chrome(request):
 
 @pytest.fixture(scope='class')
 def init_firefox(request):
+    """
+    initialise geckodriver
+    """
     firefox_options = FirefoxOptions()
     profile = FirefoxProfile()
     profile.set_preference('media.navigator.streams.fake', True)
@@ -42,6 +96,9 @@ def init_firefox(request):
 
 @pytest.fixture(scope='class')
 def init_safari_desktop(request):
+    """
+    initialise safaridriver
+    """
     #safari_options = SafariOptions()
     """ profile = FirefoxProfile()
     profile.set_preference('media.navigator.streams.fake', True)
@@ -52,6 +109,17 @@ def init_safari_desktop(request):
     yield
     safari_driver.close()
 
+@pytest.fixture(scope='class')
+def init_android(request):
+    #safari_options = SafariOptions()
+    """ profile = FirefoxProfile()
+    profile.set_preference('media.navigator.streams.fake', True)
+    profile.set_preference('media.navigator.permission.disabled', True)
+    safari_options.profile = profile """
+    android_driver = webdriver.Remote(desired_capabilities=DesiredCapabilities.ANDROID) 
+    request.cls.driver = android_driver
+    yield
+    android_driver.close()
 """ @pytest.fixture(scope='class')
 def init_safari_mobile(request):
     #safari_options = SafariOptions()
--- pytest/test/test_discover_screen.py
+++ pytest/test/test_discover_screen.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 
 
-class AbstractTest:
+class DiscoverTest:
     # pylint: disable=no-member
     """
     This Class holds all the test cases for the Discover Page
@@ -17,7 +17,7 @@ class AbstractTest:
         self.driver.get(url)
         self.driver.find_element(By.TAG_NAME, "body")
         assert self.driver.title == "Ionic App"
-    
+
     def test_discover_redirect(self, url):
         """
         Test if / redirects to /discover
@@ -31,29 +31,46 @@ class AbstractTest:
         Test if the camera button opens the camera overlay
         """
         self.driver.get(url)
-        WebDriverWait(self.driver, timeout=10).until(expected_conditions.element_to_be_clickable((By.CLASS_NAME, 'camera-icon-background')))
-        el = self.driver.find_element_by_class_name('camera-icon-background')
-        el.click()
-        WebDriverWait(self.driver, timeout=10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, 'camera-container')))
+        WebDriverWait(self.driver, timeout=10).until(
+            expected_conditions.element_to_be_clickable((By.CLASS_NAME, 'camera-icon-background'))
+            )
+        element = self.driver.find_element_by_class_name('camera-icon-background')
+        element.click()
+        WebDriverWait(self.driver, timeout=10).until(
+            expected_conditions.presence_of_element_located((By.CLASS_NAME, 'camera-container'))
+            )
         assert self.driver.find_element(By.CLASS_NAME, 'camera-container') != 'undefined'
 
+
 @pytest.mark.usefixtures('init_firefox')
-class TestFirefox(AbstractTest):
+@pytest.mark.firefox
+class TestFirefox(DiscoverTest):
     """
     Run tests on Firefox
     """
 
 
+
 @pytest.mark.usefixtures('init_chrome')
-class TestChrome(AbstractTest):
+@pytest.mark.chrome
+class TestChrome(DiscoverTest):
     """
     Run tests on Chrome
     """
 
 @pytest.mark.usefixtures('init_safari_desktop')
-class TestSafari(AbstractTest):
+@pytest.mark.safari
+class TestSafari(DiscoverTest):
     """
     Run tests on Safari
+    """
+
+
+@pytest.mark.usefixtures('init_android')
+@pytest.mark.android
+class TestAndroid(DiscoverTest):
+    """
+    Run tests on Android
     """
 
 """ @pytest.mark.usefixtures('init_safari_mobile')
