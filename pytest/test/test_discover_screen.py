282cde96b0d7b7ef77c2c34a86cddbd31b2c2fa1
Fabian Volkers <fabian.volkers@gmail.com>
2020-11-23 13:05:16 +0100

    add screenshot test for discover screen

--- pytest/test/test_discover_screen.py
+++ pytest/test/test_discover_screen.py
@@ -1,20 +1,28 @@
+"""
+Discover Screen Tests
+"""
 import pytest
+from pytest_html import extras
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 
+INNER_HEIGHT = 926
+INNER_WIDTH = 1792
 
 class DiscoverTest:
     # pylint: disable=no-member
     """
     This Class holds all the test cases for the Discover Page
     """
+    path = '/discover'
+
     def test_open_url(self, url):
         """
         Test if the url opens and the title is correct
         """
-        self.driver.get(url)
+        self.driver.get(url + self.path)
         self.driver.find_element(By.TAG_NAME, "body")
         assert self.driver.title == "Ionic App"
 
@@ -30,7 +38,7 @@ class DiscoverTest:
         """
         Test if the camera button opens the camera overlay
         """
-        self.driver.get(url)
+        self.driver.get(url + self.path)
         WebDriverWait(self.driver, timeout=10).until(
             expected_conditions.element_to_be_clickable((By.CLASS_NAME, 'camera-icon-background'))
             )
@@ -39,7 +47,25 @@ class DiscoverTest:
         WebDriverWait(self.driver, timeout=10).until(
             expected_conditions.presence_of_element_located((By.CLASS_NAME, 'camera-container'))
             )
-        assert self.driver.find_element(By.CLASS_NAME, 'camera-container') != 'undefined'
+        assert self.driver.find_element(By.CLASS_NAME, 'camera-container') != 'undefined', "Screenshot does not match baseline"
+
+    # @pytest.mark.parameterize("window_size, inner_size", [
+    #     ([])
+    # ])
+    def test_screenshot(self, url, image_similarity):
+        """
+        take screenshot and compare to baseline
+        """
+        self.driver.get(url + self.path)
+        WebDriverWait(self.driver, timeout=10).until(
+            expected_conditions.element_to_be_clickable((By.CLASS_NAME, 'camera-icon-background'))
+            )
+        # The screenshots only work if browser has the same inner dimensions
+        assert self.driver.execute_script("return window.innerHeight") == INNER_HEIGHT
+        assert self.driver.execute_script("return window.innerWidth") == INNER_WIDTH
+
+        self.driver.get_screenshot_as_file(image_similarity['filename'])
+        #assert compare_images(store_screenshot['baseline'], store_screenshot['filename']) < 0.001
 
 
 @pytest.mark.usefixtures('init_firefox')
@@ -71,6 +97,13 @@ class TestSafari(DiscoverTest):
 class TestAndroid(DiscoverTest):
     """
     Run tests on Android
+    """
+
+@pytest.mark.usefixtures('init_ios')
+@pytest.mark.ios
+class TestIOS(DiscoverTest):
+    """
+    Run tests on iOS
     """
 
 """ @pytest.mark.usefixtures('init_safari_mobile')
