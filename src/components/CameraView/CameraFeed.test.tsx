7d47719d5fae04e480bb938806a58c133543cb57
Fabian Volkers <fabian.volkers@gmail.com>
2020-12-02 13:43:19 +0100

    add unittest for camerafeed

---
+++ src/components/CameraView/CameraFeed.test.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import CameraFeed from './CameraFeed';
+
+let container: HTMLDivElement | null = null;
+
+beforeEach(() => {
+	// setup a DOM element as a render target
+	container = document.createElement('div');
+	document.body.appendChild(container);
+});
+
+afterEach(() => {
+	// cleanup on exiting
+	if (container !== null) {
+		unmountComponentAtNode(container);
+		container.remove();
+		container = null;
+	}
+});
+
+jest.mock('./CameraFeed')
+
+describe('unittest', () => {
+    it('renders correctly', () => {
+        // given
+        const cameraOpts = {
+            x: 0,
+            y: 0,
+            width: window.innerWidth,
+            height: window.innerWidth * 4 / 3,
+            camera: 'rear',
+            tapPhoto: true,
+            previewDrag: false,
+            toBack: true,
+            alpha: 1,
+        }
+        const setCameraReady = jest.fn()
+
+        // when
+        render(
+            <CameraFeed 
+                cameraOpts={cameraOpts} 
+                setCameraReady={setCameraReady}
+            ></CameraFeed>, container)
+
+        // then
+        expect(container?.getElementsByTagName('video')[0]).toBeInTheDocument();
+    })
+})
