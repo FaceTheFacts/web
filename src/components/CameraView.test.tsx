3778ca8987b8a1efaf0ea0382540d7b87f6eecef
Fabian Volkers <fabian.volkers@gmail.com>
2020-12-03 10:58:20 +0100

    skip broken integration test

--- src/components/CameraView.test.tsx
+++ src/components/CameraView.test.tsx
@@ -6,6 +6,13 @@ import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { act } from 'react-dom/test-utils';
 import 'jest-canvas-mock';
+import Enzyme, { shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import { amthor } from '../amthor';
+
+Enzyme.configure({ adapter: new Adapter() })
+
 import CameraView from './CameraView';
 
 let container: HTMLDivElement | null = null;
@@ -57,4 +64,30 @@ describe('unittest', () => {
         
     })
 
+})
+
+describe('integration test', () => {
+    test.skip('shows popover', () => {
+
+        // given
+        const history = createMemoryHistory({
+            initialEntries: [`/discover`],
+        });
+
+        const setCandidate = jest.fn()
+        const setShowPopover = jest.fn()
+
+        // when
+        const page = mount((
+                <Router history={history}>
+                    <CameraView setShowPopover={setShowPopover} setCandidate={setCandidate} />
+                </Router>
+        ))
+        console.log(page.children().invoke('setShowPopover'))
+ 
+
+        // then
+        expect(page.find('.detected-candidate-popover').length).toEqual(1);
+
+    })
 })
