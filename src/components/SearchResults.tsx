0522827d9cecab8fc258240a0d9e1edf47dce98a
Fabian Volkers <fabian.volkers@gmail.com>
2021-02-11 15:12:43 +0100

    add search result component

---
+++ src/components/SearchResults.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle } from '@ionic/react';
+
+interface SearchResultsProps {
+	results: Array<any>;
+}
+
+const SearchResults: React.FC<SearchResultsProps> = ({ results }: SearchResultsProps) => {
+	return (
+		<div id="search-results">
+			{results.map((result, index) => {
+				return (
+					<IonCard key={`result-${index}`}>
+						<IonCardHeader>
+							<IonCardSubtitle>{result.party.label}</IonCardSubtitle>
+							<IonCardTitle>{result.label}</IonCardTitle>
+						</IonCardHeader>
+					</IonCard>
+				);
+			})}
+		</div>
+	);
+};
+
+export default SearchResults;
