8690ce04d3c0ae0a9602f0e11a9ab57222ea86cb
richardk-code <richard.kruemmel@code.berlin>
2021-02-12 15:13:59 +0100

    Renaming WahlchancenProfile Component to WahlchancenCard; Adding Toggle Toolbar, Header and Back Button

--- src/pages/Wahlchancen.tsx
+++ src/pages/Wahlchancen.tsx
@@ -1,95 +1,42 @@
-import { IonContent, IonHeader, IonPage, IonFab } from '@ionic/react';
+import { IonButtons, IonContent, IonPage, IonTitle, IonBackButton, IonText, IonToolbar, IonSegment, IonSegmentButton } from '@ionic/react';
 import React from 'react';
-import { useParams } from 'react-router';
-import './Page.css';
+import PartyMatch from '../components/PartyMatch';
 import SubHeading from '../components/SubHeading';
-import VoteCard from '../components/VoteCard';
-import MenuButton from '../components/MenuButton';
-import SideJobCard from '../components/SideJobCard';
-import KontroCard from '../components/KontroCard';
-import WahlchancenProfile from '../components/WahlchancenProfile';
-import './Profile.css';
+import WahlchancenCard from '../components/WahlchancenCard';
+import './Votes.css';
 import { Candidate } from '../Types';
 
-interface WahlchancenProps {
+interface VoteProps {
 	candidate: Candidate;
 }
-
 /* Define the React component (FC stands for Functional Components, as opposed to class-based components) */
-const Wahlchancen: React.FC<WahlchancenProps> = ({ candidate }: WahlchancenProps) => {
+const Wahlchancen: React.FC<VoteProps> = ({ candidate }: VoteProps) => {
 	/* Here we define the variable 'name' to be used as a parameter in components */
-	const { id } = useParams<{ id: string }>();
 
 	/* This is returned when using this component */
 	return (
 		<IonPage>
-			{' '}
-			{/* Page Tag */}
-			<IonHeader>
-				{' '}
-				{/* Header Tag */} {/* Toolbar tag, this is the title bar / top bar */}
-			</IonHeader>
 			{/* Here the content of our page starts */}
-			<IonContent>
-				{/* ProfileImg component that holds all the images of the politicians. 
-				Right now the name property is not being used, maybe it's an idea to dynamically pass in images 
-				to make our life easier when we add the profiles images of new politicians.
-				The politicans name is included on the ProfileImg */}
-				<div className="Wahlchancen-header">
-					<WahlchancenProfile politician={candidate} />
-
-					{/*	
-					Here we include the Fab menu button - DISABLED as part of redesign
-
-					<IonFab vertical="top" horizontal="end">
-						<MenuButton />
-					</IonFab>
-				*/}
+			<IonContent fullscreen>
+				<div className="votes-toolbar">
+					<IonButtons slot="start">
+						<IonBackButton className="back-button" defaultHref="profile" text="" />
+					</IonButtons>
+					{/* Add a page title */}
+					<IonTitle className="page-title">Wahlchancen</IonTitle>
 				</div>
 
-				{/* Subheading-button created by using a div for the background color and placing a button over part of it*/}
-
-				<div className="subheading-button-underlay" data-testid="profile-subheading-votes">
-					<SubHeading
-						name="Abstimmungsverhalten >"
-						icon="infobutton.svg"
-						buttonAction={`/politician/${id}/votes`}
-					/>
+				<div data-testid="votes-subheading-parties">
+                <IonToolbar>
+                    <IonSegment value="all">
+                        <IonSegmentButton value="all">Erststimme</IonSegmentButton>
+                        <IonSegmentButton value="favorites">Zweitstimme</IonSegmentButton>
+                    </IonSegment>
+                    </IonToolbar>
 				</div>
 
-				<div className="grey-back">
-					{/* 
-					For each vote in votes, render a VoteCard component 
-					Only the first vote for now, until we display them horizontally
-					*/}
-					{candidate.polls.map((poll, index) => {
-						if (index === 0) return <VoteCard vote={poll} key={index} />;
-						else return null;
-					})}
-				</div>
-
-				{/* Pass name to Subheading to be in control of the sub heading text */}
-
-				<div data-testid="profile-subheading-controversies">
-					<SubHeading name="Kontroversen" icon="infobutton.svg" />
-				</div>
-
-				<div className="grey-back">
-					{/* For each item in kontro, render a KontroCard component */}
-					{candidate.controversies.map((kontro, index) => {
-						return <KontroCard kontro={kontro} key={index} />;
-					})}
-				</div>
-
-				<div data-testid="profile-subheading-sidejobs">
-					<SubHeading name="Bekannte Nebentätigkeiten" />
-				</div>
-
-				<div className="last-grey-back">
-					{/* For each item in title, render a NebenCard component */}
-					{candidate.sideJobs.map((sideJob, index) => {
-						return <SideJobCard sideJob={sideJob} key={index} />;
-					})}
+				<div className="votes-title" data-testid="votes-subheading-votes">
+					<IonText>Wahlergebnis</IonText>
 				</div>
 			</IonContent>
 		</IonPage>
