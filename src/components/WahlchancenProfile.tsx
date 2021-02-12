8ab8c3470de527c480b9bca2b698e63ceb1307ef
richardk-code <richard.kruemmel@code.berlin>
2021-02-12 14:02:25 +0100

    building foundation for Wahlchancen Page and Wahlchancen Component

---
+++ src/components/WahlchancenProfile.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { IonChip } from '@ionic/react';
+import './PoliticianProfile.css';
+import { Candidate } from '../Types';
+import '../index.css';
+
+interface ContainerProps {
+	politician: Candidate;
+}
+
+const WahlchancenProfile: React.FC<ContainerProps> = ({ politician }: ContainerProps) => {
+	return (
+		<div>
+			<div className="imgContainer">
+				<img
+					src={String(politician.image)}
+					alt={politician.name}
+					data-testid="profile-img-url"
+				></img>
+			</div>
+
+			<div className="politician-name" data-testid="profile-name">
+				{politician.name}
+			</div>
+
+			{politician.chips.map((chip: string, index: number) => {
+				return (
+					<IonChip className="politician-details" key={index}>
+						{chip}
+					</IonChip>
+				);
+			})}
+		</div>
+	);
+};
+
+export default WahlchancenProfile;
