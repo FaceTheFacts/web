656b7d51454f91810b821db84362a1a97ad43034
takahiro <takahiro.mitsui@code.berlin>
2021-02-24 09:07:13 +0100

    Created an empty 'SecondVote' component.

---
+++ src/pages/SecondVote.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {Candidate} from '../Types';
+
+interface ProfileProps {
+  candidate: Candidate;
+}
+
+const SecondVote: React.FC<ProfileProps> = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+};
+export default SecondVote;
