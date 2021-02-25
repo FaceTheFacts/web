4bfe4f64924bb2bba892c90fa2ef5a54da2274be
takahiro <takahiro.mitsui@code.berlin>
2021-02-25 16:05:03 +0100

    Delete sorting function

--- src/pages/SecondVote.tsx
+++ src/pages/SecondVote.tsx
@@ -4,6 +4,9 @@ import {IonPage, IonContent} from '@ionic/react'
 import './SecondVote.css';
 import {Candidate} from '../Types';
 import SecondVoteCard from '../components/SecondVoteCard';
+import SecondVoteCardItem from '../components/SecondVoteCardItem';
+
+//Add Scroll Event listener
 
 interface ProfileProps {
   candidate: Candidate;
@@ -40,21 +43,75 @@ const secondVote = [
   },
 ]
 
-const sortCandidateHandler = (electionResult: Array<any>) => {
-  const sortedCandidates = electionResult.sort ((a,b) => {
-    return b.electionChance - a.electionChance
-  });
-  return sortedCandidates
+//Just for meeting
+const candidateOne = {
+  name:'Thomas Racheal',
+  electionChance: 56,
+  rank:0,
+}
+const candidateTwo = {
+  name:'Dr.Günter Krings',
+  electionChance: 40,
+  rank:1,
+}
+const candidateThree = {
+  name:'Hermann Gröhe',
+  electionChance: 30,
+  rank:2,
+}
+const candidateFour = {
+  name:'Oliver Wittke',
+  electionChance: 29,
+  rank:3,
+}
+const candidateFive = {
+  name:'Micheala Noll',
+  electionChance: 28.8,
+  rank:4,
+}
+const candidateSix = {
+  name:'Karl-Josef Laumann',
+  electionChance: 25.6,
+  rank:5,
+}
+const candidateSeven = {
+  name:'Elisabeth Winkelmeier-Becker',
+  electionChance: 19.0,
+  rank:6,
+}
+const candidateEight = {
+  name:'Ralph Brinkhaus',
+  electionChance: 9.4,
+  rank:7,
 }
 
-const sortedSecondVote = sortCandidateHandler (secondVote);
+// 'List will be sorted'
+// => percentage per seat; 
+
+
+// const sortCandidateHandler = (electionResult: Array<any>) => {
+//   const sortedCandidates = electionResult.sort ((a,b) => {
+//     return b.electionChance - a.electionChance
+//   });
+//   return sortedCandidates
+// }
+
+// const sortedSecondVote = sortCandidateHandler (secondVote);
 
 const SecondVote: React.FC<ProfileProps> = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
         <div className = "secondvote-black-back">
-          <SecondVoteCard results = {sortedSecondVote}/>
+          {/* <SecondVoteCard results = {sortedSecondVote}/> */}
+          <SecondVoteCardItem candidateInfo = {candidateOne} cardColor = "clear" screenPosition = "away" lastCandidate= "notlast"/>
+          <SecondVoteCardItem candidateInfo = {candidateTwo} cardColor = "clear" screenPosition = "away" lastCandidate= "notlast"/>
+          <SecondVoteCardItem candidateInfo = {candidateThree} cardColor = "clear" screenPosition = "away" lastCandidate= "notlast"/>
+          <SecondVoteCardItem candidateInfo = {candidateFour} cardColor = "clear" screenPosition = "away" lastCandidate= "notlast"/>
+          <SecondVoteCardItem candidateInfo = {candidateFive} cardColor = "clear" screenPosition = "away" lastCandidate= "last"/>
+          <SecondVoteCardItem candidateInfo = {candidateSix} cardColor = "clear" screenPosition = "touched" lastCandidate= "notlast"/>
+          <SecondVoteCardItem candidateInfo = {candidateSeven} cardColor = "faded" screenPosition = "away" lastCandidate= "notlast"/>
+          <SecondVoteCardItem candidateInfo = {candidateEight} cardColor = "faded" screenPosition = "away" lastCandidate= "notlast"/>
         </div>
       </IonContent>
     </IonPage>
