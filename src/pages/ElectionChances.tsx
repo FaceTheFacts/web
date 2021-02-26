c18fc3e1cbb514ca8cd218b6117d15b9e87f93b4
richardk-code <richard.kruemmel@code.berlin>
2021-02-26 10:07:52 +0100

    adding Segment Buttons

--- src/pages/ElectionChances.tsx
+++ src/pages/ElectionChances.tsx
@@ -1,36 +1,28 @@
 import { IonButtons, IonContent, IonPage, IonTitle, IonBackButton, IonText, IonToolbar, IonSegment, IonSegmentButton } from '@ionic/react';
 import React from 'react';
 import ElectionchancesCard from '../components/ElectionChancesCard';
+import StateListCard from '../components/StateListCard';
 import './ElectionChances.css';
 import { Candidate } from '../Types';
+import SegmentButtons from '../components/SegmentButtons';
 
 interface ElectionchancesProps {
 	candidate: Candidate;
 }
+
 /* Define the React component (FC stands for Functional Components, as opposed to class-based components) */
 const Electionchances: React.FC<ElectionchancesProps> = ({ candidate }: ElectionchancesProps) => {
 	/* Here we define the variable 'name' to be used as a parameter in components */
+
+	const [tab, setTab] = React.useState("0");
 
 	/* This is returned when using this component */
 	return (
 		<IonPage>
 			{/* Here the content of our page starts */}
 			<IonContent fullscreen>
-				<div className="votes-toolbar">
-					<IonButtons slot="start">
-						<IonBackButton className="back-button" defaultHref="profile" text="" />
-					</IonButtons>
-					{/* Add a page title */}
-					<IonTitle className="page-title">Wahlchancen</IonTitle>
-				</div>
-
 				<div data-testid="votes-subheading-parties">
-                <IonToolbar>
-                    <IonSegment /*onIonChange={e => }*/>
-                        <IonSegmentButton value="FirstVote">Erststimme</IonSegmentButton>
-                        <IonSegmentButton value="SecondVote">Zweitstimme</IonSegmentButton>
-                    </IonSegment>
-                    </IonToolbar>
+                <SegmentButtons tab={tab} setTab={setTab}/>
 				</div>
 
 				<div className="election-chances-title" data-testid="votes-subheading-votes">
@@ -38,9 +30,14 @@ const Electionchances: React.FC<ElectionchancesProps> = ({ candidate }: Election
 				</div>
 
                 <div className="last-grey-back">
-					{candidate.electionResults.map((Election, index) => {
-						return <ElectionchancesCard vote={Election} key={`electionResults-${index}`}/>;
-					})}
+					{tab=="0" ? 
+					candidate.electionResults.map((Election, index) => {
+						return <ElectionchancesCard vote={Election} key={`electionResults-${index}`}/>})
+						:
+						candidate.electionResults.map((Election, index) => {
+							return <StateListCard vote={Election} key={`electionResults-${index}`}/>})
+				}
+			
 				</div>
 			</IonContent>
 		</IonPage>
