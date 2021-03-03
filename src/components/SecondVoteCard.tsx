028d9725eb5943977225189a2fc19c6fd625193a
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-03-03 12:05:21 +0100

    added change for user's touch position

--- src/components/SecondVoteCard.tsx
+++ src/components/SecondVoteCard.tsx
@@ -1,36 +1,49 @@
 import React from 'react';
-import {IonCard, IonCardHeader, IonCardTitle} from '@ionic/react'
+import { IonCard, IonCardHeader, IonCardTitle } from '@ionic/react';
 
 import './SecondVoteCard.css';
 
 interface CandidateInfoProps {
-  candidateName: string;
-  electionChance: number;
-  rank: number;
-  cardColor: 'clear'|'faded';
-  setFixedPosition: any;
-  lastCandidate: 'notlast'|'last';
+	candidateName: string;
+	electionChance: number;
+	rank: number;
+	cardColor: 'clear' | 'faded';
+	setFixedPosition: number | undefined;
+	lastCandidate: 'notlast' | 'last';
 }
 
-const SecondVoteCard: React.FC<CandidateInfoProps> = ({candidateName,electionChance,rank, cardColor, setFixedPosition, lastCandidate}: CandidateInfoProps) => {
-  return (
-    <div className = {"secondvote-cards"} id="second-vote-card">
-      <IonCard
-        className = {["secondvote-card", lastCandidate].join(" ")}
-        key = {`rank=${rank}`}>
-        <IonCardHeader
-          className = "secondvote-card-header">
-            <IonCardTitle
-              className = {["secondvote-card-name", cardColor].join(" ")}>
-              #{rank}  {candidateName}
-            </IonCardTitle>
-            {setFixedPosition === rank ? <IonCardTitle
-                className = "secondvote-card-chance">
-                {electionChance}%
-            </IonCardTitle> :null}   
-        </IonCardHeader>
-      </IonCard>   
-    </div>
-  )
+const postionChanger = (rank: number, fixedPosition: number | undefined): string => {
+	let position = 'away';
+	if (rank === fixedPosition) {
+		position = 'touched';
+	}
+	return position;
+};
+
+const SecondVoteCard: React.FC<CandidateInfoProps> = ({
+	candidateName,
+	electionChance,
+	rank,
+	cardColor,
+	setFixedPosition,
+	lastCandidate,
+}: CandidateInfoProps) => {
+	const isTouched = postionChanger(rank, setFixedPosition);
+	return (
+		<div className={['secondvote-card', isTouched].join(' ')} id="second-vote-card">
+			<IonCard className={['secondvote-card', lastCandidate].join(' ')} key={`rank=${rank}`}>
+				<IonCardHeader className="secondvote-card-header">
+					<IonCardTitle className={['secondvote-card-name', cardColor].join(' ')}>
+						#{rank} {candidateName}
+					</IonCardTitle>
+					{setFixedPosition === rank ? (
+						<IonCardTitle className="secondvote-card-chance">
+							{electionChance}%
+						</IonCardTitle>
+					) : null}
+				</IonCardHeader>
+			</IonCard>
+		</div>
+	);
 };
 export default SecondVoteCard;
