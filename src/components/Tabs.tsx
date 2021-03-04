ab8d1d39f4e6d7c709c2d1713b6efe259ca4532b
Richard Krümmel <61158193+RichardKruemmel@users.noreply.github.com>
2021-03-04 16:41:16 +0100

    [Feature] Tabbar (#43)
    * building foundation for Wahlchancen Page and Wahlchancen Component
    
    * creating route for Wahlchancen
    
    * Renaming WahlchancenProfile Component to WahlchancenCard; Adding Toggle Toolbar, Header and Back Button
    
    * Adding electionResults to Candidate, creating election Type, hardcoding top 3 candidates from election, rendering for each candidate WahlchancenCard
    
    * added picture to ElectionChancesCard; Internationalised names; created css files
    
    * adding StateListCard, displaying StateList
    
    * Creating tab bar and matching routes; styling tab bar
    
    * creating tabbar and styling acording to desgin
    
    * adding Segment Buttons
    
    * hardcoding Amthor and responding Types necessary for Electionscreen
    
    * run eslint --fix
    
    * deleted WahlchancenCard and Wahlchancen page
    
    * run prettier and fix eslint issues
    
    * running eslint and prettier
    
    * Comment Segmentbuttons out
    
    * Fix indentation and correct Route
    
    * fix linter warnings
    
    Co-authored-by: Fabian Volkers <fabian.volkers@gmail.com>

---
+++ src/components/Tabs.tsx
@@ -0,0 +1,51 @@
+import { IonRouterOutlet, IonTabs, IonTabBar, IonTabButton, IonIcon } from '@ionic/react';
+import React from 'react';
+import { Route } from 'react-router';
+import { Candidate } from '../Types';
+import { person, home, chatbubbles, archive } from 'ionicons/icons';
+import Votes from '../pages/Votes';
+import Electionchances from '../pages/ElectionChances';
+import Profile from '../pages/Profile';
+import './Tabs.css';
+
+interface TabsProps {
+	candidate: Candidate;
+}
+
+/* Define the React component (FC stands for Functional Components, as opposed to class-based components) */
+const Tabs: React.FC<TabsProps> = ({ candidate }: TabsProps) => {
+	/* This is returned when using this component */
+	return (
+		<div>
+			<IonTabs>
+				<IonRouterOutlet>
+					<Route path="/candidate/:id/profile" exact>
+						<Profile candidate={candidate as Candidate}></Profile>
+					</Route>
+					<Route path="/candidate/:id/votes" exact>
+						<Votes candidate={candidate as Candidate}></Votes>
+					</Route>
+					<Route path="/candidate/:id/electionchances" exact>
+						<Electionchances candidate={candidate as Candidate}></Electionchances>
+					</Route>
+				</IonRouterOutlet>
+				<IonTabBar slot="top" className="tabbar" selectedTab="profile">
+					<IonTabButton className="tabbutton" tab="profile" href="profile">
+						<IonIcon icon={person} />
+					</IonTabButton>
+					<IonTabButton className="tabbutton" tab="votes" href="votes">
+						<IonIcon icon={home} />
+					</IonTabButton>
+					<IonTabButton className="tabbutton" tab="controversies">
+						<IonIcon icon={chatbubbles} />
+					</IonTabButton>
+					<IonTabButton className="tabbutton" tab="electionchances" href="electionchances">
+						<IonIcon icon={archive} />
+					</IonTabButton>
+				</IonTabBar>
+			</IonTabs>
+		</div>
+	);
+};
+
+export default Tabs;
