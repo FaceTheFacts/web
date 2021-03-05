a3ead4067968edef5bae3be8c89be75c614a104a
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-03-05 07:22:43 +0100

    added card color change logic

--- src/components/SecondVoteCard.tsx
+++ src/components/SecondVoteCard.tsx
@@ -7,7 +7,7 @@ interface CandidateInfoProps {
 	candidateName: string;
 	electionChance: number;
 	rank: number;
-	cardColor: 'clear' | 'faded';
+	// cardColor: 'clear' | 'faded';
 	setFixedPosition: number | undefined;
 	lastCandidate: 'notlast' | 'last';
 }
@@ -20,15 +20,27 @@ const postionChanger = (rank: number, fixedPosition: number | undefined): string
 	return position;
 };
 
+const cardColorChanger = (rank: number, fixedPosition: number | undefined): string => {
+	let cardColor = 'faded';
+	if (!fixedPosition) {
+		return cardColor
+	}
+	if (rank <= fixedPosition) {
+		cardColor = 'clear'
+	}
+	return cardColor
+
+}
+
 const SecondVoteCard: React.FC<CandidateInfoProps> = ({
 	candidateName,
 	electionChance,
 	rank,
-	cardColor,
 	setFixedPosition,
 	lastCandidate,
 }: CandidateInfoProps) => {
 	const isTouched = postionChanger(rank, setFixedPosition);
+	const cardColor = cardColorChanger(rank, setFixedPosition);
 	return (
 		<div className={['secondvote-card', isTouched].join(' ')} id="second-vote-card">
 			<IonCard className={['secondvote-card', lastCandidate].join(' ')} key={`rank=${rank}`}>
