0c195416ec5ff42086c0cdda59b482541b5ecaeb
Fabian Volkers <fabian.volkers@gmail.com>
2021-03-08 23:18:51 +0100

    [Feature] Scan: Camera Support (#1)
    This PR  adds camera support for the mobile browser to
    power the scanning feature.
    
    Squashed commit of the following:
    
    commit 4e625084a12c5031bb4c42c5dc5c7062b1c5ed46
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Thu Sep 24 15:15:32 2020 +0200
    
        fix obsolete import
    
    commit 9e1704fa4bff23df7e4e3e2ec9e7b141291842cb
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Thu Sep 24 15:14:07 2020 +0200
    
        fix package.json
    
    commit 993a3c2ce89725c5e98f5587361577cd75ce5836
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Thu Sep 24 14:38:48 2020 +0200
    
        add electron build files to gitignore
    
    commit e982d48eeaa9fd8c5c6614805f21d5d3007375ec
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Thu Sep 24 12:40:43 2020 +0200
    
        preliminary scan styling
    
    commit 35abb0b420a63cf7235e1b426dcd67b0c30d6e12
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Thu Sep 24 12:40:01 2020 +0200
    
        implement character recognition and redirect to profile
    
    commit 454b895dca4d742f3c762d2cf2f1da8c18e92cf6
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Thu Sep 24 12:21:33 2020 +0200
    
        refactor to use PureComponent and useHistory, so we can access this.props.history
    
    commit faa519883f074daf50635d80b3143f50a04c1456
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Thu Sep 24 12:19:52 2020 +0200
    
        add fuse.js for fuzzy search of ocr result
    
    commit a86772351a2eede89dab67ee4c054e0ea8857cf7
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Wed Sep 23 11:20:14 2020 +0200
    
        add tesseract.js
    
    commit 6064e56b7cb0140496f09eabbb49583d213ad388
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Tue Sep 22 23:00:07 2020 +0200
    
        refactor Scan component to be class based
    
    commit 00c1b7fb326b7c8b4c1e3f98ecc289cf48ff735a
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Tue Sep 22 22:41:14 2020 +0200
    
        add tensorflow and dependencies
    
    commit f435e6039c49f2a4537d059bb13e92bb32b711c0
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Tue Sep 22 22:40:51 2020 +0200
    
        add instructions for mobile camera support on dev server
    
    commit bb11c58eed489dfafee1ac7eaa5d2c95b16b7558
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Tue Sep 22 22:40:17 2020 +0200
    
        add electron build to gitignore
    
    commit 470ff2fcf913da5b0a1fb8aadb7ac7bce964b5f9
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Tue Sep 22 22:40:00 2020 +0200
    
        implement web camera support
    
    commit 552c99fba52515708b71c3b4882b318bd800183e
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Mon Sep 21 10:48:14 2020 +0200
    
        use capacitor camera preview plugin

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ cd face-the-facts
 npm install
 ionic serve
 ```
+
+If you want to try out the app on an actual phone use the `external` flag
+
+on iPhone you also need the `https` flag. You will have to ignore the certificate error.
+
+```zsh
+ionic serve --https --external
+```
---
+++ src/components/CameraView.tsx
@@ -0,0 +1,306 @@
+import React from "react";
+import { RouteComponentProps, withRouter } from 'react-router-dom';
+
+//import { CameraPreviewOptions } from "@ionic-native/camera-preview";
+import { IonButton } from "@ionic/react";
+
+import {
+	BlazeFaceModel,
+	load,
+	NormalizedFace,
+} from "@tensorflow-models/blazeface";
+
+import { createWorker, createScheduler } from "tesseract.js";
+import Fuse from 'fuse.js'
+
+import "./CameraView.css";
+
+
+// TODO: Refactor to properly make use of async functions and promises
+// DetectFace should return a promise, 
+// upon resolve we detect Characters, 
+// upon resolve we fuzzy search for the politicians name, 
+// upon resolve we redirect
+
+// Pseudo Code ideal structure component
+
+// componentDidMount(){
+//   this.startCamera()
+//	 .then(() => {
+//
+//	 this.detectFaceFromVideoFrame()
+//   .then( => {
+//	 this.detectTextFromVideoFrame()
+//})
+//})	
+// }
+
+
+class CameraView extends React.PureComponent<RouteComponentProps> {
+
+	constructor(props: RouteComponentProps) {
+		super(props);
+	}
+	state = {
+		politicianDetected: false
+	}
+
+
+	canvasRef: React.RefObject<HTMLCanvasElement> = React.createRef();
+	videoRef: React.RefObject<HTMLVideoElement> = React.createRef();
+	stream?: MediaStream;
+
+	cameraOpts = {
+		x: 0,
+		y: 0,
+		width: window.screen.width,
+		height: window.screen.height * 0.8,
+		camera: "rear",
+		tapPhoto: true,
+		previewDrag: false,
+		toBack: true,
+		alpha: 1,
+	};
+
+	scheduler = createScheduler();
+
+	async componentDidMount() {
+		await this.startCamera()
+			.then((res) => {
+				console.log("started camera");
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
+
+	detectFaceFromVideoFrame = (
+		model: BlazeFaceModel,
+		video: React.RefObject<HTMLVideoElement>
+	) => {
+		if (video.current !== null) {
+			model.estimateFaces(video.current, false).then(
+				(predictions) => {
+					this.showDetections(predictions);
+
+					requestAnimationFrame(() => {
+						this.detectFaceFromVideoFrame(model, video);
+					});
+				},
+				(error) => {
+					console.log("Couldn't start the webcam");
+					console.error(error);
+				}
+			);
+		}
+	};
+
+	initializeTesseract = async () => {
+		for (let i = 0; i < 1; i++) {
+			const worker = createWorker({
+				logger: (m) => console.log(m),
+			});
+			await worker.load();
+			await worker.loadLanguage("deu");
+			await worker.initialize("deu");
+			await worker.setParameters({
+				tessedit_char_whitelist: 'abcdefghijklmnopqrstuvwxyzäöüABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜ',
+			});
+			this.scheduler.addWorker(worker);
+		}
+	};
+
+	detectTextFromVideoFrame = async (
+		canvas: React.RefObject<HTMLCanvasElement>,
+		video: React.RefObject<HTMLVideoElement>
+	) => {
+		if (this.scheduler.getNumWorkers() === 0) {
+			await this.initializeTesseract();
+		}
+		const start = new Date();
+
+		if (video.current) {
+			canvas.current
+				?.getContext("2d")
+				?.drawImage(
+					video.current,
+					0,
+					0,
+					canvas.current.width,
+					canvas.current.height
+				);
+
+			await this.scheduler
+				.addJob("recognize", canvas.current)
+				.then((result) => {
+					console.log(result.data.text);
+					result.data.text.split("\n").forEach((line: String) => {
+						console.log(line);
+					});
+					const list = result.data.text.split("\n");
+					const options = {
+						includeScore: true
+					  }
+					  
+					  const fuse = new Fuse(list, options)
+					  
+					  const res = fuse.search('philipp amthor');
+					  console.log(res)
+					  if (res.length > 0){
+						  this.redirectToProfile();
+						  return new Promise((resolve, reject) => {
+
+						  })
+						  /* this.setState({politicianDetected: true}) */
+
+					  }
+					const end = new Date();
+					console.log(
+						`[${start.getMinutes()}:${start.getSeconds()} - ${end.getMinutes()}:${end.getSeconds()}], ${
+							(end.getTime() - start.getTime()) / 1000
+						} s`
+					);
+					requestAnimationFrame(() => {
+						this.detectTextFromVideoFrame(canvas, video);
+					});
+					
+				});
+
+			
+		}
+	};
+
+	redirectToProfile = async () => {
+		console.log('terminating workers');
+		await this.scheduler.terminate();
+
+		console.log('stopping camera');
+		const tracks = this.stream?.getTracks()
+		if (tracks !== undefined){
+			for(let track of tracks){
+				track.stop();
+			}
+		}
+
+		console.log('redirecting');
+		this.props.history.push('/politician/1')
+	}
+
+	showDetections = (predictions: NormalizedFace[]) => {
+
+		const ctx = this.canvasRef.current?.getContext(
+			"2d"
+		) as CanvasRenderingContext2D;
+
+		ctx.canvas.width = this.cameraOpts.width;
+		ctx.canvas.height = this.cameraOpts.height;
+		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+		predictions.forEach((prediction) => {
+
+			const start: [number, number] = prediction.topLeft as [
+				number,
+				number
+			];
+			const end: [number, number] = prediction.bottomRight as [
+				number,
+				number
+			];
+			var probability = prediction.probability as number;
+
+			const size = [end[0] - start[0], end[1] - start[1]];
+
+			// Render a rectangle over each detected face.
+			ctx.beginPath();
+			ctx.strokeStyle = "green";
+			ctx.lineWidth = 4;
+			ctx.rect(start[0], start[1], size[0], size[1]);
+			ctx.stroke();
+			var prob = (probability * 100).toPrecision(5).toString();
+			var text = prob + "%";
+			ctx.fillStyle = "red";
+			ctx.font = "13pt sans-serif";
+			ctx.fillText(text, start[0] + 5, start[1] + 20);
+		});
+
+	};
+
+	async startCamera() {
+		if (this.videoRef.current !== null) {
+			this.videoRef.current.style.width = String(this.cameraOpts.width);
+			this.videoRef.current.style.height = String(this.cameraOpts.height);
+			this.videoRef.current.addEventListener(
+				"play",
+				() => {
+					this.draw();
+				},
+				false
+			);
+		}
+
+		await navigator.mediaDevices
+			.getUserMedia({
+				video: { facingMode: "environment" },
+				audio: false,
+			})
+			.then((stream: MediaStream) => {
+				console.log(this.videoRef);
+				if (this.videoRef.current !== null) {
+
+					this.stream = stream
+					this.videoRef.current.srcObject = stream;
+					this.videoRef.current.onloadedmetadata = (e) => {
+						if (this.videoRef.current !== null) {
+							console.log(e);
+							console.log(this);
+							this.videoRef.current.play();
+						}
+					};
+					console.log(this.videoRef.current);
+
+				}
+			})
+			.catch((err: {}) => {
+				console.log(err);
+			});
+		return new Promise((resolve, reject) => {
+			if (
+				this.videoRef.current !== null &&
+				typeof this.videoRef.current.srcObject !== "undefined"
+			) {
+				resolve({ value: "success" });
+			} else {
+				reject("error");
+			}
+		});
+	}
+
+	async draw() {
+		console.log("Drawing");
+
+		// This broke everything, it was added because we got the error 'no backend set'
+		//console.log('Using TensorFlow backend: ', tf.getBackend());
+
+		//const context = this.canvasRef.current?.getContext("2d");
+		const model = await load();
+		console.log(model);
+		const video = this.videoRef;
+		this.detectFaceFromVideoFrame(model, video);
+		await this.detectTextFromVideoFrame(this.canvasRef, video);
+	}
+	render() {
+		
+		return (
+			<div>
+				<IonButton onClick={this.startCamera.bind(this)}>
+					Start Camera Preview
+				</IonButton>
+				{ this.state.politicianDetected ? <IonButton id='profile-button' routerLink='/politician/1' >Detected Philipp Amthor, view?</IonButton> : null }
+				
+				<video ref={this.videoRef} playsInline autoPlay></video>
+				<canvas ref={this.canvasRef}></canvas>
+			</div>
+		);
+	}
+}
+export default withRouter(CameraView);
