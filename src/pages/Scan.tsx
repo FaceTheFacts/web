f62c637c0eb6c6dee95b45100b5e9d33e552a227
Fabian Volkers <fabian.volkers@gmail.com>
2021-03-08 23:18:51 +0100

    [Feature] Scan: Camera Support (#1)
    This PR  adds camera support for the mobile browser to
    power the scanning feature.
    
    Squashed commit of the following:
    
    commit 4e625084a12c5031bb4c42c5dc5c7062b1c5ed46
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Thu Sep 24 15:15:32 2020 +0200
    
        fix obsolete import
    
    commit 9e1704fa4bff23df7e4e3e2ec9e7b141291842cb
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Thu Sep 24 15:14:07 2020 +0200
    
        fix package.json
    
    commit 993a3c2ce89725c5e98f5587361577cd75ce5836
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Thu Sep 24 14:38:48 2020 +0200
    
        add electron build files to gitignore
    
    commit e982d48eeaa9fd8c5c6614805f21d5d3007375ec
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Thu Sep 24 12:40:43 2020 +0200
    
        preliminary scan styling
    
    commit 35abb0b420a63cf7235e1b426dcd67b0c30d6e12
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Thu Sep 24 12:40:01 2020 +0200
    
        implement character recognition and redirect to profile
    
    commit 454b895dca4d742f3c762d2cf2f1da8c18e92cf6
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Thu Sep 24 12:21:33 2020 +0200
    
        refactor to use PureComponent and useHistory, so we can access this.props.history
    
    commit faa519883f074daf50635d80b3143f50a04c1456
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Thu Sep 24 12:19:52 2020 +0200
    
        add fuse.js for fuzzy search of ocr result
    
    commit a86772351a2eede89dab67ee4c054e0ea8857cf7
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Wed Sep 23 11:20:14 2020 +0200
    
        add tesseract.js
    
    commit 6064e56b7cb0140496f09eabbb49583d213ad388
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Tue Sep 22 23:00:07 2020 +0200
    
        refactor Scan component to be class based
    
    commit 00c1b7fb326b7c8b4c1e3f98ecc289cf48ff735a
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Tue Sep 22 22:41:14 2020 +0200
    
        add tensorflow and dependencies
    
    commit f435e6039c49f2a4537d059bb13e92bb32b711c0
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Tue Sep 22 22:40:51 2020 +0200
    
        add instructions for mobile camera support on dev server
    
    commit bb11c58eed489dfafee1ac7eaa5d2c95b16b7558
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Tue Sep 22 22:40:17 2020 +0200
    
        add electron build to gitignore
    
    commit 470ff2fcf913da5b0a1fb8aadb7ac7bce964b5f9
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Tue Sep 22 22:40:00 2020 +0200
    
        implement web camera support
    
    commit 552c99fba52515708b71c3b4882b318bd800183e
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Mon Sep 21 10:48:14 2020 +0200
    
        use capacitor camera preview plugin

--- src/pages/Scan.tsx
+++ src/pages/Scan.tsx
@@ -9,39 +9,51 @@ import {
 	IonToolbar,
 } from "@ionic/react";
 import React from "react";
+import { withRouter, RouteComponentProps } from 'react-router-dom';
 import {
 	useCamera,
 	availableFeatures,
 } from "@capacitor-community/react-hooks/camera";
-import { CameraResultType } from "@capacitor/core";
+import { CameraResultType, Camera } from "@capacitor/core";
 import {
 	CameraPreview,
 	CameraPreviewPictureOptions,
 	CameraPreviewOptions,
 	CameraPreviewDimensions,
 } from "@ionic-native/camera-preview";
+
+import { Plugins } from "@capacitor/core";
+//import "capacitor-plugin-camera-preview"
+
 import { useParams } from "react-router";
 import ExploreContainer from "../components/ExploreContainer";
 import "./Page.css";
+import CameraView from "../components/CameraView";
 
-const Scan: React.FC = () => {
-	const { name } = useParams<{ name: string }>();
-	const { photo, getPhoto } = useCamera();
 
-	const handleTakePhoto = () => {
+class Scan extends React.PureComponent<RouteComponentProps> {
+
+	//name = useParams<{ name: string }>();
+	name = 'Scan'
+	/* photo = useCamera();
+	getPhoto = useCamera(); */
+
+	CameraPreview = Plugins;
+
+	/* const handleTakePhoto = () => {
 		if (availableFeatures.getPhoto) {
-			getPhoto({
+			this.getPhoto({
 				quality: 100,
 				allowEditing: false,
 				resultType: CameraResultType.DataUrl,
 			});
 		}
-	};
-	const cameraPreviewOpts: CameraPreviewOptions = {
+	}; */
+	cameraPreviewOpts: CameraPreviewOptions = {
 		x: 0,
 		y: 0,
 		width: window.screen.width,
-		height: window.screen.height*0.8,
+		height: window.screen.height * 0.8,
 		camera: "rear",
 		tapPhoto: true,
 		previewDrag: false,
@@ -49,36 +61,63 @@ const Scan: React.FC = () => {
 		alpha: 1,
 	};
 
-	const startCamera = async () => {
-		await CameraPreview.startCamera(cameraPreviewOpts).then((res) => {
+	/* startCamera = async () => {
+		const video = React.createRef();
+		const canvas = React.createRef();
+		await CameraPreview.startCamera(this.cameraPreviewOpts, video, canvas)
+			.then((res: {}) => {
+				console.log("Opened Camera");
+				console.log(res);
+			})
+			.catch((err: {}) => {
+				console.log(err);
+			});
+		/* await CameraPreview.startCamera(cameraPreviewOpts).then((res) => {
 			console.log("Open Camera");
 			console.log(res);
 		}).catch((err) =>{
 			console.log(err);
-		});
-	};
-	return (
-		<IonPage>
-			<IonHeader>
-				<IonToolbar>
-					<IonButtons slot="start">
-						<IonMenuButton />
-					</IonButtons>
-					<IonTitle>{name}</IonTitle>
-				</IonToolbar>
-			</IonHeader>
+		}); */
+	//};
 
-			<IonContent fullscreen>
-				<IonHeader collapse="condense">
+	takeSnapshot = async () => {
+		await CameraPreview.takeSnapshot().then((res: {}) => console.log(res));
+	};
+
+	state = {
+		showingCamera :false
+	};
+	
+
+	showCamera = () => {
+		this.setState({showingCamera: true});
+	}
+
+	render() {
+		return (
+			<IonPage>
+				<IonHeader>
 					<IonToolbar>
-						<IonTitle size="large">{name}</IonTitle>
+						<IonButtons slot="start">
+							<IonMenuButton />
+						</IonButtons>
+						<IonTitle>{this.name}</IonTitle>
 					</IonToolbar>
 				</IonHeader>
+
+				<IonContent fullscreen>
+					<IonHeader collapse="condense">
+						<IonToolbar>
+							<IonTitle size="large">{this.name}</IonTitle>
+						</IonToolbar>
+					</IonHeader>
+					{/*
+				
 				{availableFeatures.getPhoto ? (
 					<div>
 						<div>
 							<IonButton onClick={startCamera}>
-								Take Photo
+								Start Camera Preview
 							</IonButton>
 						</div>
 						<div>{photo && <img alt="" src={photo.dataUrl} />}</div>
@@ -86,9 +125,21 @@ const Scan: React.FC = () => {
 				) : (
 					<div>Camera not available on this platform</div>
 				)}
-			</IonContent>
-		</IonPage>
-	);
-};
+				
+				<div id='camera-preview'></div>
+				<IonButton onClick={takeSnapshot}>
+								Take Snapshot
+							</IonButton>
+							*/}
+					<IonButton onClick={this.showCamera}>
+						Start Camera Preview
+					</IonButton>
 
-export default Scan;
+					{ this.state.showingCamera ? <CameraView /> : null }
+				</IonContent>
+			</IonPage>
+		);
+	}
+}
+
+export default withRouter(Scan);
