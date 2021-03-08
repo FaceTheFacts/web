df2a0f1be6a004138228a3abfcac81e8afa20481
Fabian Volkers <fabian.volkers@gmail.com>
2021-03-08 22:07:25 +0100

    [Feature] Hackathon: Profile Page
    This is the first draft of the profile page, built during
    the Slash PPP hackathon Fall 2020. This merge is a squashed
    commit of the following:
    
    commit c172581d07a849b256e2bf2a27f391226f6392cc
    Author: Kintsugi <liam_going@hotmail.com>
    Date:   Wed Sep 2 14:48:43 2020 +0200
    
        .
    
    commit e671317970848394dcd7028cdf52500f149151ba
    Author: Kintsugi <liam_going@hotmail.com>
    Date:   Wed Sep 2 14:02:36 2020 +0200
    
        .
    
    commit 57ecba61754521fb9e12c96da026b7d76081a64f
    Author: Kintsugi <liam_going@hotmail.com>
    Date:   Wed Sep 2 12:19:49 2020 +0200
    
        Profile
    
    commit bc798fee7fdebaaa507a8274079b1f75a7dcd546
    Author: Fabian Volkers <fabianvolkers@Fabians-MacBook-Pro.local>
    Date:   Wed Sep 2 11:43:34 2020 +0200
    
        add profile and votes pages

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+interface ContainerProps {
+  name: string;
+}
+
+const Header: React.FC<ContainerProps> = ({ name }) => {
+  return (
+    <div className="Header">
+      <strong>{name}</strong>
+    </div>
+  );
+};
+
+export default Header;
