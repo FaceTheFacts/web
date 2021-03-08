bf56f367b7dfea010e4e3f7867350e7151756b93
Fabian Volkers <fabian.volkers@gmail.com>
2021-03-09 00:18:51 +0100

    [Feature] ControversyCard: Add Expandability (#5)
    Make controversy card component expandable.
    
    Squashed commit of the following:
    
    commit 7017ffc42dd0582f13d49cff8339a54c86414a4a
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Thu Oct 1 13:49:53 2020 +0200
    
        add horizontal scroll
    
    commit d8f91648dd365f72b352784e0e5df9293bc4f2bc
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Thu Oct 1 13:48:52 2020 +0200
    
        remove obsolete code and imports
    
    commit a869e0443b7a795ce9e12c8726a2e4b0c1a4a2ba
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Thu Oct 1 11:57:30 2020 +0200
    
        add expandable card and example articles
    
    commit aca05d246dd951551f544198b04cd4b9bf757061
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Thu Oct 1 11:56:44 2020 +0200
    
        add newspaper logos
    
    commit 00b00d2ebdbee350b83ba9bb724240d6cf3ebed9
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Thu Oct 1 11:56:30 2020 +0200
    
        add expand icon
    
    commit aa18768384d03adeab5f37a0d4891b3f5aab4d18
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Thu Oct 1 10:12:30 2020 +0200
    
        add image and url to kontro props
    
    commit 6787c871847ebd8ada7928e5f6fdb167244d0369
    Author: Fabian Volkers <fabian.volkers@gmail.com>
    Date:   Thu Oct 1 10:12:00 2020 +0200
    
        refactor into article component

--- src/components/KontroCard.tsx
+++ src/components/KontroCard.tsx
@@ -8,25 +8,60 @@ import {
 	IonCardTitle,
 } from "@ionic/react";
 
+import KontroArticle from "./KontroArticle";
 import "./KontroCard.css";
+import { imag } from "@tensorflow/tfjs-core";
 
+interface Article {
+	image: string;
+	url: string;
+	label: string;
+	publisher: string;
+}
 interface ContainerProps {
-	kontro: string;
+	kontro: {
+		label: string;
+		articles: Array<Article>;
+	};
 }
 
 const KontroCard: React.FC<ContainerProps> = ({ kontro }) => {
 	//State Hook to alter state when clicked
 	const [isOpen, setIsOpen] = React.useState(false);
 
+	const kontroClassName = `kontro-layout ${
+		isOpen ? "kontro-open" : "kontro-closed"
+	}`;
 	return (
-		<IonCard className="kontro-layout">
+		<IonCard className={kontroClassName}>
 			<IonCardHeader
 				className="accordion"
 				onClick={() => setIsOpen(!isOpen)}
 			>
-				{kontro} {isOpen ? "open" : "closed"}
+				<IonCardTitle className="kontro-title">
+					{kontro.label}
+				</IonCardTitle>
+
+				{
+					<img
+						className="kontro-expand-icon"
+						src={
+							isOpen
+								? "../assets/icon/ausgeklappt.png"
+								: "../assets/icon/eingeklappt.png"
+						}
+					></img>
+				}
 			</IonCardHeader>
-			<IonCard></IonCard>
+			<IonCardContent className="kontro-card-content">
+				<div className="article-row">
+					{kontro.articles.map((article, index) => {
+						return (
+							<KontroArticle article={article}></KontroArticle>
+						);
+					})}
+				</div>
+			</IonCardContent>
 		</IonCard>
 	);
 };
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -38,12 +38,57 @@ import KontroCard from "../components/KontroCard";
 import "./Profile.css";
 
 // Hardcoded Kontroversen until we connect to our API
-const kontros =["Lobbyismus-Affäre",'"Hey Rezo, du alter Zerstörer!"'];
+const kontros = [
+	{
+		label: "Lobbyismus-Affäre",
+		articles: [
+			{
+				label:
+					"Geschäftsführer von Augustus Intelligence lässt Amt ruhen",
+				image:
+					"https://cdn.prod.www.spiegel.de/images/e4f16506-cc40-4023-a068-8c33a57bf098_w948_r1.77_fpx39.81_fpy49.98.jpg",
+				url:
+					"https://www.spiegel.de/wirtschaft/unternehmen/affaere-um-philipp-amthor-geschaeftsfuehrer-von-augustus-intelligence-laesst-amt-ruhen-a-6577f286-2dc0-4f74-a08c-a2536e86a87d",
+				publisher: "spiegel",
+			},
+			{
+				label:
+					"Für diese merkwürdige Firma hat sich Philipp Amthor engagiert",
+				image:
+					"https://www.handelsblatt.com/images/philipp-amthor/25914394/3-format2020.jpg",
+				url:
+					"https://www.handelsblatt.com/politik/deutschland/augustus-intelligence-fuer-diese-merkwuerdige-firma-hat-sich-philipp-amthor-engagiert/25914246.html?ticket=ST-630502-PMl5Lfyb2kItmkRSq1QJ-ap6",
+				publisher: "handelsblatt",
+			},
+			{
+				label: "Verfahren gegen Philipp Amthor eingestellt",
+				image:
+					"https://img.zeit.de/politik/deutschland/2020-07/philipp-amthor-staatsanwaltschaft-lobbyismus-bestechlichkeit-ermittlungen-eingestellt-bild/wide__820x461__desktop",
+				url:
+					"https://www.zeit.de/politik/deutschland/2020-07/philipp-amthor-lobby-affaere-staatsanwaltschaft-bestechlichkeit-ermittlungen-eingestellt",
+				publisher: "zeit-online",
+			},
+		],
+	},
+	{
+		label: '"Hey Rezo, du alter Zerstörer!"',
+		articles: [
+			{
+				label: "Article 1",
+				image: "Image url",
+				url: "article url",
+				publisher: "spiegel",
+			},
+		],
+	},
+];
 
 // Hardcoded chips until we connect to our API
 const chips = ["CDU/CSU", "Mitglied des Bundestags"];
 
-{/* Hardcoded for now until passed in from the API */}
+{
+	/* Hardcoded for now until passed in from the API */
+}
 const nebens = [
 	{
 		subtitle: "Mitglied des Verwaltungsrates",
@@ -63,13 +108,8 @@ const votes = [
 			abstain: 21,
 			none: 173,
 		},
-	}
-	
+	},
 ];
-
-
-
-	
 
 /* Define the React component (FC stands for Functional Components, as opposed to class-based components) */
 const Profile: React.FC = () => {
@@ -81,80 +121,68 @@ const Profile: React.FC = () => {
 		<IonPage>
 			{" "}
 			{/* Page Tag */}
-				<IonHeader>
-					{" "}
-					{/* Header Tag */}
-					
-						{" "}
-						{/* Toolbar tag, this is the title bar / top bar */}					
-
-				</IonHeader>
-
+			<IonHeader>
+				{" "}
+				{/* Header Tag */}{" "}
+				{/* Toolbar tag, this is the title bar / top bar */}
+			</IonHeader>
 			{/* Here the content of our page starts */}
 			<IonContent>
-
 				{/* ProfileImg component that holds all the images of the politicians. 
 				Right now the name property is not being used, maybe it's an idea to dynamically pass in images 
 				to make our life easier when we add the profiles images of new politicians.
 				The politicans name is included on the ProfileImg */}
-				<div className ="profile-header">
+				<div className="profile-header">
 					{/* ProfileImage needs a gradient overlay added! */}
 					<ProfileImage name={name} />
 					<ProfileName name={name} />
 					{/* For each vote in votes, render a VoteCard component */}
 					{chips.map((chip, index) => {
 						return <ProfileChip chip={chip} />;
-					})}	
-				{/* Here we include the Fab menu button */}
+					})}
+					{/* Here we include the Fab menu button */}
 					<IonFab vertical="top" horizontal="end">
-						<MenuButton/>
+						<MenuButton />
 					</IonFab>
 				</div>
 
-
 				{/* Subheading-button created by using a div for the background color and placing a button over part of it*/}
-				
+
 				<div className="subheading-button-underlay">
-					<SubHeadingButtonInfo name ="Abstimmungsverhalten >" />
+					<SubHeadingButtonInfo name="Abstimmungsverhalten >" />
 				</div>
 
-					<div className="grey-back">
-						{/* For each vote in votes, render a VoteCard component */}
-						{votes.map((vote, index) => {
-							return <VoteCard vote={vote} />;
-						})}
-					</div>		
-					
+				<div className="grey-back">
+					{/* For each vote in votes, render a VoteCard component */}
+					{votes.map((vote, index) => {
+						return <VoteCard vote={vote} />;
+					})}
+				</div>
 
 				{/* Pass name to Subheading to be in control of the sub heading text */}
-				
+
 				<div>
 					<SubHeadingInfo name="Kontroversen" />
 				</div>
-				
-					
-					<div className="grey-back">
-						{/* For each item in kontro, render a KontroCard component */}
-						{kontros.map((kontro, index) => {
-								return <KontroCard kontro={kontro} />;
-						})}	
-					</div>
-					
-				
+
+				<div className="grey-back">
+					{/* For each item in kontro, render a KontroCard component */}
+					{kontros.map((kontro, index) => {
+						return <KontroCard kontro={kontro} />;
+					})}
+				</div>
+
 				<div>
 					<SubHeadingInfo name="Bekannte Nebentätigkeiten" />
 				</div>
-				
-					<div className="last-grey-back">
-						{/* For each item in title, render a NebenCard component */}
-						{nebens.map((neben, index) => {
-								return <NebenCard neben={neben} />;
-						})}	
-						
-					</div>
-					
+
+				<div className="last-grey-back">
+					{/* For each item in title, render a NebenCard component */}
+					{nebens.map((neben, index) => {
+						return <NebenCard neben={neben} />;
+					})}
+				</div>
 			</IonContent>
-			
 		</IonPage>
 	);
 };
