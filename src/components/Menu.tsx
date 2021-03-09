461eabaa2b9968701439af282e8a3aea5b73a415
Fabian Volkers <fabian.volkers@gmail.com>
2021-03-09 02:15:11 +0100

    [Chore] Vote details: Clean up obsolete code (#12)
    Squashed commit of the following:
    
    commit ffe3b648f5548b478fbf480eedd5f39284ca5d11
    Author: KintsugiCode <liam.going@code.berlin>
    Date:   Wed Oct 14 17:52:20 2020 +0200
    
        Removed menu access by swipe from all screens
    
    commit f70694dbf46cf42c3982b5d63b4cc12d3e5d77f0
    Author: KintsugiCode <liam.going@code.berlin>
    Date:   Wed Oct 14 17:31:31 2020 +0200
    
        Linked Abstimmungsverhalten button to Votes Screen
    
    commit 9697f2b6fabfca91ba33fe02d51487753aba41e1
    Author: KintsugiCode <liam.going@code.berlin>
    Date:   Wed Oct 14 16:05:42 2020 +0200
    
        Dynamic centering for candidate vote when multiple lines
    
    Co-authored-by: KintsugiCode <liam.going@code.berlin>

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -57,52 +57,15 @@ const appPages: AppPage[] = [
 		iosIcon: homeOutline,
 		mdIcon: homeSharp,
 	},
-	/* {
-    title: 'Inbox',
-    url: '/page/Inbox',
-    iosIcon: mailOutline,
-    mdIcon: mailSharp
-  },
-  {
-    title: 'Outbox',
-    url: '/page/Outbox',
-    iosIcon: paperPlaneOutline,
-    mdIcon: paperPlaneSharp
-  },
-  {
-    title: 'Favorites',
-    url: '/page/Favorites',
-    iosIcon: heartOutline,
-    mdIcon: heartSharp
-  },
-  {
-    title: 'Archived',
-    url: '/page/Archived',
-    iosIcon: archiveOutline,
-    mdIcon: archiveSharp
-  },
-  {
-    title: 'Trash',
-    url: '/page/Trash',
-    iosIcon: trashOutline,
-    mdIcon: trashSharp
-  },
-  {
-    title: 'Spam',
-    url: '/page/Spam',
-    iosIcon: warningOutline,
-    mdIcon: warningSharp
-  } */
 ];
 
-const labels = ["Family", "Friends", "Notes", "Work", "Travel", "Reminders"];
 
 /* Menu component. */
 const Menu: React.FC = () => {
 	const location = useLocation();
 
 	return (
-		<IonMenu side="end" contentId="main" type="overlay">
+		<IonMenu side="end" contentId="main" type="overlay" swipe-gesture="false">
 			<IonContent>
 				<IonList id="inbox-list">
 					<IonListHeader>Face the Facts</IonListHeader>
@@ -138,16 +101,6 @@ const Menu: React.FC = () => {
 						);
 					})}
 				</IonList>
-
-				{/*  <IonList id="labels-list">
-          <IonListHeader>Labels</IonListHeader>
-          {labels.map((label, index) => (
-            <IonItem lines="none" key={index}>
-              <IonIcon slot="start" icon={bookmarkOutline} />
-              <IonLabel>{label}</IonLabel>
-            </IonItem>
-          ))}
-        </IonList> */}
 			</IonContent>
 		</IonMenu>
 	);
