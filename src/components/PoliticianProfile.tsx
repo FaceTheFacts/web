a99802800c737ebbaa7a1bc4f755f51413f86719
Richard Krümmel <61158193+RichardKruemmel@users.noreply.github.com>
2021-03-09 12:06:41 +0100

    [Feature] Profile: Header (#50)
    * Create IonGrid and change backgroundcolor
    
    * Add politicians age and style chips to match Design
    
    * Create round profile image
    
    * Add line for politician job and fix eslint warnings
    
    * Update package-lock
    
    * Import loglevel

--- src/components/PoliticianProfile.tsx
+++ src/components/PoliticianProfile.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonChip } from '@ionic/react';
+import { IonGrid, IonRow, IonCol, IonChip, IonCardTitle, IonCardSubtitle } from '@ionic/react';
 import './PoliticianProfile.css';
 import { Candidate } from '../Types';
 import '../index.css';
@@ -10,26 +10,33 @@ interface ContainerProps {
 
 const PoliticianProfile: React.FC<ContainerProps> = ({ politician }: ContainerProps) => {
 	return (
-		<div>
-			<div className="imgContainer">
-				<img
-					src={String(politician.image)}
-					alt={politician.name}
-					data-testid="profile-img-url"
-				></img>
-			</div>
+		<div className="header">
+			<IonGrid>
+				<IonRow>
+					<IonCol size="3">
+						<img className="politician-image"
+							src={String(politician.image)}
+							alt={politician.name}
+							data-testid="profile-img-url"
+						></img>
+					</IonCol>
+					<IonCol size="9">
+						<div className="politician-name"><IonCardTitle>{politician.name} <span className="politician-age">29</span></IonCardTitle></div>
+						<div className="politician-job"><IonCardSubtitle>Jurist</IonCardSubtitle></div>
+						<div className="politican-details">
 
-			<div className="politician-name" data-testid="profile-name">
-				{politician.name}
-			</div>
-
-			{politician.chips.map((chip: string, index: number) => {
-				return (
-					<IonChip className="politician-details" key={index}>
-						{chip}
-					</IonChip>
-				);
-			})}
+							{politician.chips.map((chip: string, index: number) => {
+								return (
+									<IonChip className="politician-detail" key={index}>
+										{chip}
+									</IonChip>
+								
+								);
+							})}
+						</div>
+					</IonCol>
+				</IonRow>
+			</IonGrid>
 		</div>
 	);
 };
