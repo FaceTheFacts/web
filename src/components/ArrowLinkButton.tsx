ba6fbe1b829cbed9f98a68e10ecbe0adc3a3200b
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-03-10 17:23:11 +0100

    changed background color

--- src/components/ArrowLinkButton.tsx
+++ src/components/ArrowLinkButton.tsx
@@ -3,13 +3,15 @@ import { arrowForwardOutline } from 'ionicons/icons';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './ArrowLinkButton.css';
+
 interface ArrowLinkButtonProps {
 	linkTo: string;
 }
 
 const ArrowLinkButton: React.FC<ArrowLinkButtonProps> = ({ linkTo }: ArrowLinkButtonProps) => (
 	<Link to={linkTo}>
-		<IonButton>
+		<IonButton size="small">
 			<IonIcon slot="icon-only" icon={arrowForwardOutline} />
 		</IonButton>
 	</Link>
