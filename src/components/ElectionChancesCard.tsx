01d5012661aefa13c4d1925337a0fd11abb0f4bd
RichardKruemmel <richard.kruemmel@code.berlin>
2021-03-11 15:25:41 +0100

    Change first ElectionchancesCard to be stuck to top

--- src/components/ElectionChancesCard.tsx
+++ src/components/ElectionChancesCard.tsx
@@ -41,16 +41,15 @@ const ElectionchancesCard: React.FC<ContainerProps> = ({ vote, firstCandidate }:
 				It's inside a css grid so we can more easily arrange the items.
 				*/}
 				<IonCardHeader className="election-chances-card-header">
-					<IonGrid>
-						<IonRow>
-							{firstCandidate ? 
+							{firstCandidate ?
+							<IonGrid>
+							<IonRow>
 								<IonCol size="3">
 									<div className="election-chances-card-image">
 										<img id="img" src={String(vote.image)}></img>
 									</div>
 								</IonCol>
-							: null}
-							<IonCol size="6">
+								<IonCol size="6">
 								<div id="center-column">
 								<IonCardTitle className="election-chances-card-title">
 									{vote.candidate}
@@ -67,8 +66,32 @@ const ElectionchancesCard: React.FC<ContainerProps> = ({ vote, firstCandidate }:
 								</IonCardTitle>
 								</div>
 							</IonCol>
-						</IonRow>
+							</IonRow>
 					</IonGrid>
+							:
+						<IonGrid>
+						<IonRow> 
+							<IonCol size="9">
+								<div id="center-column">
+								<IonCardTitle className="election-chances-card-title">
+									{vote.candidate}
+								</IonCardTitle>
+								<IonCardSubtitle className={["election-chances-card-subtitle", partyClassName].join(" ")}>
+									<span>{vote.party}</span>
+								</IonCardSubtitle>
+								</div>
+							</IonCol>
+							<IonCol size="3">
+								<div id="center-column">
+							<IonCardTitle className="election-chances-card-title">
+									{vote.percentage}%
+								</IonCardTitle>
+								</div>
+							</IonCol>
+							</IonRow>
+					</IonGrid>
+					}
+						
 				</IonCardHeader>
 			</IonCard>
 	);
