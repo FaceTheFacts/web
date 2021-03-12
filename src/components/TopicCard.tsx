9ef99922a46ea8efcae44a8b638ac7374b0becd4
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-03-12 11:43:34 +0100

    added a map function to show topics

--- src/components/TopicCard.tsx
+++ src/components/TopicCard.tsx
@@ -73,31 +73,20 @@ const topics = [
 
 const TopicCard: React.FC = () => {
 	return (
-    <IonChip className = "topic-card">
-     <IonIcon src = "assets/icon/sack-dollar-light.svg" color="light" />
-      <IonLabel>
-        Finanzen
-      </IonLabel>
-    </IonChip>
-    // <IonCard className = "topic-card">
-    //   <IonItem>
-    //    <IonIcon src = "assets/icon/sack-dollar-light.svg" slot="start"> 
-    //    </IonIcon>
-    //     Finanzen
-    //   </IonItem>
-    // </IonCard>
-		// <div className="topic-filters">
-		// 	<div className="topic-filter-row">
-		// 		{topicFilters.row0.map((topicFilter, index) => {
-		// 			return (
-		// 				<IonButton fill="clear" className="topic-filter-button" key={`0-${index}`}>
-		// 					<IonIcon slot="start" src={topicFilter.icon} />
-		// 					{topicFilter.name}
-		// 				</IonButton>
-		// 			);
-		// 		})}
-		// 	</div>
-		// </div>
+    <React.Fragment>
+      {
+        topics.map((topic, index)=>{
+          return(
+          <IonChip className = "topic-card" key={`topic-${index}`}>
+            <IonIcon src = {topic.icon} color="light" />
+            <IonLabel>
+              {topic.name}
+            </IonLabel>
+          </IonChip>
+          )
+        })
+      }
+    </React.Fragment>
 	);
 };
 
