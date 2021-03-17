857196e14789cd1e3a497744018d6aaeebc4f67e
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-03-17 21:55:40 +0100

    added KontroCard to News component

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-
+import { Candidate } from '../Types';
 import TitleHeader from './TitleHeader';
+import KontroCard from './KontroCard';
 
-
-const News: React.FC = () => (
+interface NewsProps {
+	candidate: Candidate;
+}
+const News: React.FC<NewsProps> = ({ candidate }: NewsProps) => (
 	<React.Fragment>
 		<div className="black-back">
 			<TitleHeader title="kontroversen" />
+			{candidate.controversies.map((kontro, index) => {
+				return <KontroCard kontro={kontro} key={index} />;
+			})}
 		</div>
 	</React.Fragment>
 );
