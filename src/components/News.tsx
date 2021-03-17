90e2bdf72d981664b91dc1f94e1169f40757dd9f
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-03-17 21:34:57 +0100

    created a news component

---
+++ src/components/News.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import TitleHeader from './TitleHeader';
+
+
+const News: React.FC = () => (
+	<React.Fragment>
+		<div className="black-back">
+			<TitleHeader title="kontroversen" />
+		</div>
+	</React.Fragment>
+);
+export default News;
