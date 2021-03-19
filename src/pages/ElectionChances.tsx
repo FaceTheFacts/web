1e3cd50f305c9aaa568c60c95eee9f36ee057ee4
RichardKruemmel <richard.kruemmel@code.berlin>
2021-03-19 11:55:47 +0100

    Clean up SecondVote and SecondVoteCard

--- src/pages/ElectionChances.tsx
+++ src/pages/ElectionChances.tsx
@@ -16,81 +16,9 @@ interface ElectionchancesProps {
 const Electionchances: React.FC<ElectionchancesProps> = ({ candidate }: ElectionchancesProps) => {
 	/* Here we define the variable 'name' to be used as a parameter in components */
 	const { id } = useParams<{ id: string }>();
-
-	const [tab, setTab] = React.useState('0'); // eslint-disable-line @typescript-eslint/no-unused-vars
+	const [tab, setTab] = React.useState(0)
+	const [segment, setSegment] = React.useState('0'); // eslint-disable-line @typescript-eslint/no-unused-vars
 /* Second Vote Code */
-
-const [fixedPosition, setFixedPosition] = useState<number | undefined>();
-	const [previousScrollHeight, setpreviousScrollHeight] = useState<number>(0);
-	const [lastCandidate, setLastcandidate] = useState<number>(0);
-
-	const initialFixedPositionHandler = (): undefined | number => {
-		const windowHeight: number | undefined = document.getElementById('second-vote-results')
-			?.clientHeight;
-		const cardHeight: number | undefined = document.getElementById('second-vote-card')
-			?.clientHeight;
-		console.log(cardHeight, windowHeight);
-		if (typeof windowHeight === 'number' && typeof cardHeight === 'number') {
-			const halfWindowHeight = windowHeight / 2;
-			const cardNums = Math.round(halfWindowHeight / cardHeight);
-			return cardNums + 1;
-		}
-	};
-
-	useIonViewDidEnter(() =>
-		setTimeout(function () {
-			setFixedPosition(initialFixedPositionHandler());
-			setLastCandidateHandler();
-		}, 100)
-	);
-
-	const cardNumsHandler = (voteRes: { name: string; electionChance: number }[]): number => {
-		const cardNums = voteRes.length;
-		return cardNums;
-	};
-
-	const setFixedPositionHandler = (): void => {
-		const cardNums = cardNumsHandler(candidate.secondVote);
-		const voteResults = document.getElementById('second-vote-results');
-		const initialPos = initialFixedPositionHandler();
-		const cardHeight = 57;
-		const cardblank = 10;
-		const oneScroll = cardHeight + cardblank;
-		if (!fixedPosition || !voteResults || !initialPos) {
-			return;
-		}
-		//margin top 10px
-		const scrollHeight = voteResults.scrollTop - 10;
-		const scrollDistance = Math.abs(scrollHeight - previousScrollHeight);
-
-		let direction;
-		if (previousScrollHeight < scrollHeight) {
-			direction = 1;
-		} else {
-			direction = -1;
-		}
-		//If a user scroll at the bottom of screen
-		if (fixedPosition === cardNums && scrollDistance >= oneScroll) {
-			setFixedPosition(fixedPosition - 1);
-		}
-
-		if (fixedPosition < cardNums && scrollDistance >= oneScroll) {
-			setpreviousScrollHeight(scrollHeight);
-			const scrollNums = (scrollDistance / oneScroll) >> 0;
-			setFixedPosition(fixedPosition + scrollNums * direction);
-		}
-		//If a user try to scroll up above the initial position
-		if (fixedPosition < initialPos) {
-			setFixedPosition(fixedPosition + 1);
-		}
-	};
-
-	const setLastCandidateHandler = (): void => {
-		//if we get deta from backend, we should write calculation logic here
-		//But now I detemine last candidate is rank #4
-		//this function should excute after rendering this page
-		setLastcandidate(4);
-	};
 
 
 	/* This is returned when using this component */
@@ -98,10 +26,10 @@ const [fixedPosition, setFixedPosition] = useState<number | undefined>();
 		<IonPage className="black-back">
 			{/* Here the content of our page starts */}
 				<PoliticianProfile candidate={candidate} />
-				<Tabs candidate={candidate}></Tabs>
-				<SegmentButtons tab={tab} setTab={setTab}/>
+				<Tabs></Tabs>
+				<SegmentButtons tab={segment} setTab={setSegment}/>
 				
-				{tab=='0' ? 
+				{segment=='0' ? 
 					<div className="election-chances-title">
 						<IonCardSubtitle>Wahlergebnis 2017</IonCardSubtitle>
 					</div>
@@ -119,9 +47,24 @@ const [fixedPosition, setFixedPosition] = useState<number | undefined>();
 					</div>
 				}
 							
-				{tab=="0" ?
+				{segment=="0" ?
 					<ElectionchancesCard firstCandidate={true} vote={candidate.electionResults[0]} />
-					: null
+				: null}
+				<IonContent>
+					<div className="blue-back">
+					{segment=="1" ?
+					candidate.secondVote.slice(1).map((StateList, index) => {
+						return <SecondVoteCard
+						secondVote={StateList}
+						key={`secondvote-${index}`}
+						rank={index + 1}
+					/>;
+					})
+					:
+					candidate.electionResults.slice(1).map((Election, index) => {
+						
+						return <ElectionchancesCard firstCandidate={false} vote={Election} key={`electionResults-${index}`}/>;
+					})	
 					}
 
 				<div className="last-grey-back">
