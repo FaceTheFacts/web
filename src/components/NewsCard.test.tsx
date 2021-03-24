67a0ffaf1750fd01fe7d45d226e84f6cb291b57f
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-03-24 14:15:44 +0100

    Write a test for NewsCard component

---
+++ src/components/NewsCard.test.tsx
@@ -0,0 +1,31 @@
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import { amotherNews } from '../amother_news';
+let container: HTMLDivElement | null = null;
+import NewsCard from './NewsCard';
+
+beforeEach(() => {
+	// setup a DOM element as a render target
+	container = document.createElement('div');
+	document.body.appendChild(container);
+});
+
+afterEach(() => {
+	// cleanup on exiting
+	if (container !== null) {
+		unmountComponentAtNode(container);
+		container.remove();
+		container = null;
+	}
+});
+
+it('renders with correct title', () => {
+	act(() => {
+		render(<NewsCard news={amotherNews} />, container);
+	});
+	if (container !== null) {
+		expect(container.getElementsByClassName('news-card-title')[0]).toBe(
+			amotherNews[0].newsTitle
+		)
+	}
+});
