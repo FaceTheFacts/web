c92118c027ff207990a57569a2b19bac87acd539
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-03-24 11:38:06 +0100

    Create News page

---
+++ src/pages/News.tsx
@@ -0,0 +1,31 @@
+import { IonContent, IonPage } from '@ionic/react';
+import React from 'react';
+import { useParams } from 'react-router';
+import './Page.css';
+import Tabs from '../components/Tabs';
+import PoliticianProfile from '../components/PoliticianProfile';
+import { Candidate } from '../Types';
+interface ProfileProps {
+	candidate: Candidate;
+}
+
+/* Define the React component (FC stands for Functional Components, as opposed to class-based components) */
+const News: React.FC<ProfileProps> = ({ candidate }: ProfileProps) => {
+	/* Here we define the variable 'name' to be used as a parameter in components */
+	const { id } = useParams<{ id: string }>();
+
+	/* This is returned when using this component */
+	return (
+		<IonPage>
+			{' '}
+			{/* Page Tag */}
+			<PoliticianProfile candidate={candidate} />
+			<Tabs />
+			<IonContent>
+			
+			</IonContent>
+		</IonPage>
+	);
+};
+
+export default News;
