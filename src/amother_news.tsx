aa0d0448bcf9585b01307d7cbec97d30a796a1ea
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-03-24 14:06:44 +0100

    Change into array data

--- src/amother_news.tsx
+++ src/amother_news.tsx
@@ -1,53 +1,21 @@
-export const amotherNews = {
-	news: [
-		{
-			newsTitle: 'Lobbyismus-Affäre',
-			articles: [
-				{
-					url: new URL(
-						'https://www.spiegel.de/wirtschaft/unternehmen/affaere-um-philipp-amthor-geschaeftsfuehrer-von-augustus-intelligence-laesst-amt-ruhen-a-6577f286-2dc0-4f74-a08c-a2536e86a87d'
-					),
-				},
-				{
-					url: new URL(
-						'https://www.handelsblatt.com/politik/deutschland/augustus-intelligence-fuer-diese-merkwuerdige-firma-hat-sich-philipp-amthor-engagiert/25914246.html?ticket=ST-630502-PMl5Lfyb2kItmkRSq1QJ-ap6'
-					),
-					publisher: 'handelsblatt',
-				},
-				{
-					url: new URL(
-						'https://www.zeit.de/politik/deutschland/2020-07/philipp-amthor-lobby-affaere-staatsanwaltschaft-bestechlichkeit-ermittlungen-eingestellt'
-					),
-				},
-			],
-		},
-		{
-			newsTitle: '"Hey Rezo, du alter Zerstörer!"',
-			articles: [
-				{
-					url: new URL('https://www.youtube.com/watch?v=DEKzmztBZNI'),
-				},
-				{
-					url: new URL(
-						'https://www.tagesspiegel.de/politik/amthor-bleibt-video-schuldig-cdu-antwort-an-youtuber-rezo-doch-nicht-im-netz/25035964.html'
-					),
-				},
-				{
-					url: new URL(
-						'https://www.zdf.de/nachrichten/heute-sendungen/videos/philipp-amthor-verraet-ersten-satz-aus-antwort-video-an-rezo-bei-markus-lanz-100.html'
-					),
-				},
-				{
-					url: new URL(
-						'https://www.bild.de/politik/inland/politik-inland/philipp-amthor-cdu-kuendigt-bei-markus-lanz-antwort-auf-rezo-video-an-aber-64827894.bild.html'
-					),
-				},
-				{
-					url: new URL(
-						'https://www.wz.de/politik/cdu-antwort-an-rezo-doch-nicht-im-netz-amthor-bleibt-video-schuldig_aid-45960339'
-					),
-				},
-			],
-		},
-	],
-};
+export const amotherNews = [
+	{
+		newsTitle: 'Lobbyismus-Affäre',
+		articles: [
+			'https://www.spiegel.de/wirtschaft/unternehmen/affaere-um-philipp-amthor-geschaeftsfuehrer-von-augustus-intelligence-laesst-amt-ruhen-a-6577f286-2dc0-4f74-a08c-a2536e86a87d',
+
+			'https://www.handelsblatt.com/politik/deutschland/augustus-intelligence-fuer-diese-merkwuerdige-firma-hat-sich-philipp-amthor-engagiert/25914246.html?ticket=ST-630502-PMl5Lfyb2kItmkRSq1QJ-ap6',
+			'https://www.zeit.de/politik/deutschland/2020-07/philipp-amthor-lobby-affaere-staatsanwaltschaft-bestechlichkeit-ermittlungen-eingestellt',
+		],
+	},
+	{
+		newsTitle: '"Hey Rezo, du alter Zerstörer!"',
+		articles: [
+			'https://www.youtube.com/watch?v=DEKzmztBZNI',
+			'https://www.tagesspiegel.de/politik/amthor-bleibt-video-schuldig-cdu-antwort-an-youtuber-rezo-doch-nicht-im-netz/25035964.html',
+			'https://www.zdf.de/nachrichten/heute-sendungen/videos/philipp-amthor-verraet-ersten-satz-aus-antwort-video-an-rezo-bei-markus-lanz-100.html',
+			'https://www.bild.de/politik/inland/politik-inland/philipp-amthor-cdu-kuendigt-bei-markus-lanz-antwort-auf-rezo-video-an-aber-64827894.bild.html',
+			'https://www.wz.de/politik/cdu-antwort-an-rezo-doch-nicht-im-netz-amthor-bleibt-video-schuldig_aid-45960339',
+		],
+	},
+];
