21689a7c16eb428e3aa7a5f4c6be01a29d8e678c
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-03-25 16:39:16 +0100

    Map data in News component instead of in NewsCard

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import { Candidate } from '../Types';
+import { Candidate, NewsInput } from '../Types';
 import TitleHeader from './TitleHeader';
-// import KontroCard from './KontroCard';
+
+import NewsCard from './NewsCard';
+
 
 interface NewsProps {
 	candidate: Candidate;
+	news: Array<NewsInput>;
 }
-const News: React.FC<NewsProps> = ({ candidate }: NewsProps) => (
+
+const News: React.FC<NewsProps> = ({ candidate, news }: NewsProps) => (
 	<React.Fragment>
 		<div className="black-back">
 			<TitleHeader title="kontroversen" />
 			{/* {candidate.controversies.map((kontro, index) => {
 				return <KontroCard kontro={kontro} key={index} />;
 			})} */}
+			{news.map((newsItem, index) => {
+				const key = `${newsItem.newsTitle}-${index}`;
+				return <NewsCard key={key} newsData={newsItem} />;
+			})}
 		</div>
 	</React.Fragment>
 );
