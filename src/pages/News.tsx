993a78c8615de3df9275e37bd01bf5eba4251d39
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-03-30 12:04:25 +0200

    Delete unnecessal components

--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -1,10 +1,8 @@
 import { IonContent, IonPage } from '@ionic/react';
 import React from 'react';
-//
 import './Page.css';
 import Tabs from '../components/Tabs';
 import PoliticianProfile from '../components/PoliticianProfile';
-// import NewsComponent from '../components/News';
 import { amthor } from '../amthor';
 import { Candidate } from '../Types';
 import KontroCard from '../components/KontroCard';
@@ -14,10 +12,6 @@ interface ProfileProps {
 
 /* Define the React component (FC stands for Functional Components, as opposed to class-based components) */
 const News: React.FC<ProfileProps> = ({ candidate }: ProfileProps) => {
-	/* Here we define the variable 'name' to be used as a parameter in components */
-	// const { id } = useParams<{ id: string }>();
-
-	/* This is returned when using this component */
 	return (
 		<IonPage>
 			{' '}
@@ -25,7 +19,6 @@ const News: React.FC<ProfileProps> = ({ candidate }: ProfileProps) => {
 			<PoliticianProfile candidate={amthor} />
 			<Tabs />
 			<IonContent>
-				{/* <NewsComponent candidate={candidate} news={amotherNews} /> */}
 				{candidate.controversies.map((kontro, index) => {
 					return <KontroCard kontro={kontro} key={index} />;
 				})}
