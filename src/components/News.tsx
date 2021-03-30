1a486076fcd40d91716ef6593c96b86d239010fc
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-03-30 09:15:42 +0200

    Remove scraping logic to use it on backend server

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Candidate, NewsInput } from '../Types';
 import TitleHeader from './TitleHeader';
-import axios from 'axios';
-import cheerio from 'cheerio';
 import NewsCard from './NewsCard';
 
 interface NewsProps {
@@ -15,27 +13,7 @@ interface ArticleData {
 	title: string | undefined;
 	imageUrl: string | undefined;
 }
-const ArticleScraper = async (url: string) => {
-	const AxiosInstance = axios.create();
-	const response = await AxiosInstance.get(url);
-	const html = await response.data;
-	const $ = cheerio.load(html);
-	const title = $('meta[property="og:title"]').attr('content'); //meta-tag//title
-	const imageUrl = $('meta[property="og:image"]').attr('content'); //meta-tag//image
-	const ScrapedArticles: ArticleData[] = [];
-	ScrapedArticles.push({
-		url,
-		title,
-		imageUrl,
-	});
-	return ScrapedArticles;
-};
-const ScrapeHandler = async (url: string) => {
-	// const x = myserver.com/article/url
-	//We need backend server
-	const data = await ArticleScraper(url);
-	console.log(data);
-};
+
 
 const News: React.FC<NewsProps> = ({ candidate, news }: NewsProps) => {
 	return (
@@ -49,7 +27,6 @@ const News: React.FC<NewsProps> = ({ candidate, news }: NewsProps) => {
 					const key = `${newsItem.newsTitle}-${index}`;
 					return <NewsCard key={key} newsData={newsItem} />;
 				})}
-				<button onClick={() => ScrapeHandler(news[1].articles[1])}>Test</button>
 			</div>
 		</React.Fragment>
 	);
