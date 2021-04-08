759b7c9fd87548e957660866d72679804ea1b727
RichardKruemmel <richard.kruemmel@code.berlin>
2021-04-08 19:20:42 +0200

    Fetch Data for PoliticianProfile with ReactQuery and Axios

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -4,8 +4,16 @@ import { useParams } from 'react-router';
 import './Page.css';
 import Tabs from '../components/Tabs';
 import PoliticianProfile from '../components/PoliticianProfile';
+<<<<<<< HEAD
 import { Candidate } from '../Types';
 import ProfileComponent from '../components/Profile/Profile';
+=======
+import { Candidate, Politician } from '../Types';
+import ProfileComponent from '../components/Profile';
+import { useQuery } from 'react-query';
+import axios, { AxiosResponse } from 'axios';
+
+>>>>>>> Fetch Data for PoliticianProfile with ReactQuery and Axios
 interface ProfileProps {
 	candidate: Candidate;
 }
@@ -13,14 +21,16 @@ interface ProfileProps {
 /* Define the React component (FC stands for Functional Components, as opposed to class-based components) */
 const Profile: React.FC<ProfileProps> = ({ candidate }: ProfileProps) => {
 	/* Here we define the variable 'name' to be used as a parameter in components */
+	const data = useQuery('politicianProfile', () => axios(`https://www.abgeordnetenwatch.de/api/v2/politicians/${id}?related_data=show_information`))
 	const { id } = useParams<{ id: string }>();
+	const politicianProfileData: AxiosResponse<any> | undefined = data.data
 
 	/* This is returned when using this component */
 	return (
 		<IonPage className="Profile-Mobile">
 			{' '}
 			{/* Page Tag */}
-			<PoliticianProfile candidate={candidate} />
+			{politicianProfileData !== undefined ? <PoliticianProfile candidate={politicianProfileData.data.data} /> : null}
 			<Tabs />
 			<IonContent>
 				<ProfileComponent candidate={candidate} profileId={id} />
