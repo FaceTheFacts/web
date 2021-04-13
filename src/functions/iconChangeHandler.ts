6af737b18431b2d5c8eee64ea60a819a5ce0b7eb
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-04-13 15:41:02 +0200

    Write iconChangeHandler and check the test passes.

--- src/functions/iconChangeHandler.ts
+++ src/functions/iconChangeHandler.ts
@@ -1,4 +1,4 @@
-import { arrowForwardOutline, chevronBackOutline } from 'ionicons/icons';
+import { arrowForwardOutline, chevronBackOutline, closeCircle, closeOutline, searchOutline } from 'ionicons/icons';
 import { iconEnum } from '../enums/icon.enum';
 export const iconChangeHandler = (icon:iconEnum) => {
   let iconName;
@@ -9,7 +9,15 @@ export const iconChangeHandler = (icon:iconEnum) => {
     case iconEnum.CHEVRON_BACK:
       iconName = chevronBackOutline
       break
-      
+    case iconEnum.SEARCH:
+      iconName = searchOutline
+      break
+    case iconEnum.CLOSE:
+      iconName = closeOutline
+      break
+    case iconEnum.CLOSE_CIRCLE:
+      iconName = closeCircle
+      break
     default:
       iconName = arrowForwardOutline
   }
