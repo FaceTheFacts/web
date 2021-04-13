b099ad65f1a82c1e44585bd44692bad1b4c8c4ed
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-04-13 15:48:03 +0200

    Run prettier.

--- src/functions/iconChangeHandler.ts
+++ src/functions/iconChangeHandler.ts
@@ -1,25 +1,31 @@
-import { arrowForwardOutline, chevronBackOutline, closeCircle, closeOutline, searchOutline } from 'ionicons/icons';
+import {
+	arrowForwardOutline,
+	chevronBackOutline,
+	closeCircle,
+	closeOutline,
+	searchOutline,
+} from 'ionicons/icons';
 import { iconEnum } from '../enums/icon.enum';
-export const iconChangeHandler = (icon:iconEnum) => {
-  let iconName;
-  switch(icon){
-    case iconEnum.ARROW_FORWARD:
-      iconName = arrowForwardOutline
-      break
-    case iconEnum.CHEVRON_BACK:
-      iconName = chevronBackOutline
-      break
-    case iconEnum.SEARCH:
-      iconName = searchOutline
-      break
-    case iconEnum.CLOSE:
-      iconName = closeOutline
-      break
-    case iconEnum.CLOSE_CIRCLE:
-      iconName = closeCircle
-      break
-    default:
-      iconName = arrowForwardOutline
-  }
-  return iconName
-}
+export const iconChangeHandler = (icon: iconEnum) => {
+	let iconName;
+	switch (icon) {
+		case iconEnum.ARROW_FORWARD:
+			iconName = arrowForwardOutline;
+			break;
+		case iconEnum.CHEVRON_BACK:
+			iconName = chevronBackOutline;
+			break;
+		case iconEnum.SEARCH:
+			iconName = searchOutline;
+			break;
+		case iconEnum.CLOSE:
+			iconName = closeOutline;
+			break;
+		case iconEnum.CLOSE_CIRCLE:
+			iconName = closeCircle;
+			break;
+		default:
+			iconName = arrowForwardOutline;
+	}
+	return iconName;
+};
