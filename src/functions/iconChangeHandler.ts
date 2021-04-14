df5feaf2b779c1599d658116997288cd118be450
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-04-14 21:48:06 +0200

    Fix the eslint issue.

--- src/functions/iconChangeHandler.ts
+++ src/functions/iconChangeHandler.ts
@@ -9,23 +9,23 @@ import { iconEnum } from '../enums/icon.enum';
 export const iconChangeHandler = (icon: iconEnum): string => {
 	let iconName: string;
 	switch (icon) {
-		case iconEnum.ARROW_FORWARD:
-			iconName = arrowForwardOutline;
-			break;
-		case iconEnum.CHEVRON_BACK:
-			iconName = chevronBackOutline;
-			break;
-		case iconEnum.SEARCH:
-			iconName = searchOutline;
-			break;
-		case iconEnum.CLOSE:
-			iconName = closeOutline;
-			break;
-		case iconEnum.CLOSE_CIRCLE:
-			iconName = closeCircle;
-			break;
-		default:
-			iconName = arrowForwardOutline;
+	case iconEnum.ARROW_FORWARD:
+		iconName = arrowForwardOutline;
+		break;
+	case iconEnum.CHEVRON_BACK:
+		iconName = chevronBackOutline;
+		break;
+	case iconEnum.SEARCH:
+		iconName = searchOutline;
+		break;
+	case iconEnum.CLOSE:
+		iconName = closeOutline;
+		break;
+	case iconEnum.CLOSE_CIRCLE:
+		iconName = closeCircle;
+		break;
+	default:
+		iconName = arrowForwardOutline;
 	}
 	return iconName;
 };
