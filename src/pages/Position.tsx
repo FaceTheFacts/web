c44cee5158e83452fe723e3e08abc038b74a5281
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-04-22 12:57:49 +0200

    Refactor with the new component

--- src/pages/Position.tsx
+++ src/pages/Position.tsx
@@ -4,7 +4,7 @@ import './Page.css';
 import Tabs from '../components/Tabs';
 import PoliticianProfile from '../components/PoliticianProfile';
 import { Candidate } from '../Types';
-import PositionCard from '../components/PositionCard';
+import PositionCards from '../components/PositionCards';
 
 interface PositionProps {
 	candidate: Candidate;
@@ -12,18 +12,18 @@ interface PositionProps {
 
 const examplePositionData = [
 	{
-		'title':'Pfleger:innen aus dem Ausland',
-		'answer':'yes'
+		title: 'Pfleger:innen aus dem Ausland',
+		answer: 'yes',
 	},
 	{
-		'title':'Lobbyismusgesetz verschärfen',
-		'answer':'no'
+		title: 'Lobbyismusgesetz verschärfen',
+		answer: 'no',
 	},
 	{
-		'title':'Abitur aussetzen',
-		'answer':'yes'
+		title: 'Abitur aussetzen',
+		answer: 'yes',
 	},
-]
+];
 
 const Position: React.FC<PositionProps> = ({ candidate }: PositionProps) => {
 	return (
@@ -31,11 +31,7 @@ const Position: React.FC<PositionProps> = ({ candidate }: PositionProps) => {
 			<PoliticianProfile candidate={candidate} />
 			<Tabs />
 			<IonContent>
-				{
-					examplePositionData.map((datum, index) => (
-						<PositionCard key = {`position-${index}`} title = {datum.title}/>
-					))
-				}
+				<PositionCards positions={examplePositionData} />
 			</IonContent>
 		</IonPage>
 	);
