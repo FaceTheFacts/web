81129ee5cef05cee68a09ff8c37a32a890a36095
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-04-22 18:34:49 +0200

    Add a new property

--- src/components/PositionCard.tsx
+++ src/components/PositionCard.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { IonCard, IonCardHeader, IonCardTitle } from '@ionic/react';
+import Positioning from './Positioning';
 
 interface PostionCardProps {
 	title: string;
+	answer: string;
 }
 
-const PositionCard: React.FC<PostionCardProps> = ({ title }: PostionCardProps) => {
+const PositionCard: React.FC<PostionCardProps> = ({ title, answer }: PostionCardProps) => {
 	return (
 		<IonCard>
 			<IonCardHeader>
 				<IonCardTitle data-testid="position-card-title">{title}</IonCardTitle>
+				<Positioning positioning={answer} />
 			</IonCardHeader>
 		</IonCard>
 	);
