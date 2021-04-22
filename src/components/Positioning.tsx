6e441a98f6f11b73356c587faba4dd6ca72ae20e
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-04-22 14:13:19 +0200

    Create a new component

---
+++ src/components/Positioning.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Positioning.css';
+
+interface PositioningProps {
+	positioning: string;
+}
+
+const Positioning: React.FC<PositioningProps> = ({ positioning }: PositioningProps) => (
+	<div className={`positioning-${positioning}`}>{positioning}</div>
+);
+
+export default Positioning;
