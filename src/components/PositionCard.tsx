6b1788aa2c2efb33daeaa8281ac96f5c17d9f748
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-04-22 21:00:23 +0200

    Add a LinkButton

--- src/components/PositionCard.tsx
+++ src/components/PositionCard.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import { IonCard, IonCardHeader, IonCardTitle } from '@ionic/react';
 import Positioning from './Positioning';
+import LinkButton from './LinkButton';
+import { iconEnum } from '../enums/icon.enum';
 
 interface PostionCardProps {
+	candidateId: number;
 	title: string;
 	answer: string;
 }
 
-const PositionCard: React.FC<PostionCardProps> = ({ title, answer }: PostionCardProps) => {
+const PositionCard: React.FC<PostionCardProps> = ({
+	candidateId,
+	title,
+	answer,
+}: PostionCardProps) => {
 	return (
 		<IonCard>
 			<IonCardHeader>
 				<IonCardTitle data-testid="position-card-title">{title}</IonCardTitle>
 				<Positioning positioning={answer} />
+				<LinkButton
+					linkTo={`/politician/${candidateId}/position`}
+					icon={iconEnum.CHEVRON_FORWARD}
+				/>
 			</IonCardHeader>
 		</IonCard>
 	);
