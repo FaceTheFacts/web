cb553543d1ea7fecc217618305d222b912378056
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-04-22 14:46:19 +0200

    Fix typo and import a function

--- src/functions/germanTranslator.test.ts
+++ src/functions/germanTranslator.test.ts
@@ -1,3 +1,4 @@
+import { germanTranslator } from './germanTranslator';
 test("If the input is 'yes', it should return 'ja'.", () => {
 	expect(germanTranslator('yes')).toBe('ja');
 });
@@ -7,8 +8,8 @@ test("If the input is 'no', it should return 'nein'.", () => {
 test("If the input is 'abstain', it should return 'enthalten'.", () => {
 	expect(germanTranslator('abstain')).toBe('enthalten');
 });
-test("If the input is 'no', it should return 'nicht abg'.", () => {
-	expect(germanTranslator('no')).toBe('nicht abg');
+test("If the input is 'none', it should return 'nicht abg'.", () => {
+	expect(germanTranslator('none')).toBe('nicht abg');
 });
 test("If the input is 'something', it should return 'undefined'.", () => {
 	expect(germanTranslator('something')).toBe('undefined');
