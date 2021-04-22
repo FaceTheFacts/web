1d2bfd5830a7805c72e0c0da32ebba76fcf9daba
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-04-22 14:36:04 +0200

    Write a test

---
+++ src/functions/germanTranslator.test.ts
@@ -0,0 +1,15 @@
+test("If the input is 'yes', it should return 'ja'.", () => {
+	expect(germanTranslator('yes')).toBe('ja');
+});
+test("If the input is 'no', it should return 'nein'.", () => {
+	expect(germanTranslator('no')).toBe('nein');
+});
+test("If the input is 'abstain', it should return 'enthalten'.", () => {
+	expect(germanTranslator('abstain')).toBe('enthalten');
+});
+test("If the input is 'no', it should return 'nicht abg'.", () => {
+	expect(germanTranslator('no')).toBe('nicht abg');
+});
+test("If the input is 'something', it should return 'undefined'.", () => {
+	expect(germanTranslator('something')).toBe('undefined');
+});
