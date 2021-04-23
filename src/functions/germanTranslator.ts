9f4f9aad8837d6768adacc33a7f1c2668c9c7307
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-04-23 10:20:03 +0200

    Fix eslint issue

--- src/functions/germanTranslator.ts
+++ src/functions/germanTranslator.ts
@@ -1,21 +1,20 @@
 export const germanTranslator = (word: string): string => {
 	let translated: string;
 	switch (word) {
-		// eslint-disable-next-line
-		case 'yes':
-			translated = 'ja';
-			break;
-		case 'no':
-			translated = 'nein';
-			break;
-		case 'abstain':
-			translated = 'enthalten';
-			break;
-		case 'none':
-			translated = 'nicht abg';
-			break;
-		default:
-			translated = 'undefined';
+	case 'yes':
+		translated = 'ja';
+		break;
+	case 'no':
+		translated = 'nein';
+		break;
+	case 'abstain':
+		translated = 'enthalten';
+		break;
+	case 'none':
+		translated = 'nicht abg';
+		break;
+	default:
+		translated = 'undefined';
 	}
 	return translated;
 };
