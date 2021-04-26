457b22b46dec6f2c0d254e8f74988358294b7fac
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-04-26 16:55:06 +0200

    Add more topics

--- src/pages/Position.tsx
+++ src/pages/Position.tsx
@@ -23,6 +23,34 @@ const examplePositionData = [
 		title: 'Abitur aussetzen',
 		answer: 'yes',
 	},
+	{
+		title: 'Bedigungsloses Grundeinkommen',
+		answer: 'no',
+	},
+	{
+		title: 'Spitzensteuersatz senken',
+		answer: 'yes',
+	},
+	{
+		title: 'Mehr Subventionen für erneuerbare Energien',
+		answer: 'no',
+	},
+	{
+		title: 'Mehr Geld für Pflegekräfte',
+		answer: 'yes',
+	},
+	{
+		title: 'Soziale Start-ups fördern',
+		answer: 'yes',
+	},
+	{
+		title: 'Amazon und Co. angemessen besteuern',
+		answer: 'yes',
+	},
+	{
+		title: 'Mietpreise deckeln',
+		answer: 'no',
+	},
 ];
 
 const Position: React.FC<PositionProps> = ({ candidate }: PositionProps) => {
