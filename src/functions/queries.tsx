87472469f400d7e762d747818376640127e0cd0e
RichardKruemmel <richard.kruemmel@code.berlin>
2021-05-03 10:31:17 +0200

    Add Base_URL

--- src/functions/queries.tsx
+++ src/functions/queries.tsx
@@ -1,6 +1,7 @@
 import axios, { AxiosResponse } from 'axios';
 
 export default async function fetch(url: string) {
-	const res: AxiosResponse = await axios.get(url);
+	const BASE_URL = 'https://www.abgeordnetenwatch.de/api/v2/';
+	const res: AxiosResponse = await axios.get(BASE_URL + url);
 	return await res.data;
 }
