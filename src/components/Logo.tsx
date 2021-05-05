29bed320aab31e7a4309eca43d420088d9546534
RichardKruemmel <richard.kruemmel@code.berlin>
2021-05-05 18:07:46 +0200

    Import Logo and create own Component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import faceTheFactsLogo from '../assets/images/Logo.png';
+const Logo: React.FC = () => (
+	<div>
+		<img src={faceTheFactsLogo} alt="logo" />
+	</div>
+);
+export default Logo;
