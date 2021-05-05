19ea2484b4fd9760237c72e99b524b6f19983a3f
RichardKruemmel <richard.kruemmel@code.berlin>
2021-05-05 18:08:33 +0200

    Add general structure of LandingPage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,60 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Logo from '../components/Logo'
 import './Home.css';
+
 
 /* Define the React component (FC stands for Functional Components, as opposed to class-based components) */
 const Home: React.FC = () => {
 	/* This is returned when using this component */
 
 	return (
-		<div className="tabbar">
-			<NavLink
+        <div>
+		<div className="nav-header">
+            <NavLink
+				id="home"
+				to={'/'}
+			>
+				<Logo />
+			</NavLink>
+			<div className="links"><NavLink
 				id="legal-notice"
 				to={'/legal-notice'}
 				className="navbar"
-				activeClassName="navbar activated"
+				activeClassName="navbar activ"
 			>
-				<div className="navtext">Impressum</div>
+				Impressum
 			</NavLink>
 			<NavLink
 				id="privacy"
 				to={`/privacy`}
 				className="navbar"
-				activeClassName="navbar activated"
+				activeClassName="navbar activ"
 			>
-				<div className="navtext">Datenschutz</div>
+				Datenschutz
 			</NavLink>
+            </div>
+        </div>
+        <div className="content">Content</div>
+
+        <div className="links-bottom">
+            <NavLink
+                id="legal-notice-bottom"
+                to={'/legal-notice'}
+                className="navbar"
+                activeClassName="navbar activ"
+                >
+                Impressum
+            </NavLink>
+            <NavLink
+                id="privacy-bottom"
+                to={`/privacy`}
+                className="navbar"
+                activeClassName="navbar activ"
+            >
+            Datenschutz
+            </NavLink>
+        </div> 
         </div>
 	);
 };
