1613e366b0907f4bcd042c7256d16b0d7c289c8e
RichardKruemmel <richard.kruemmel@code.berlin>
2021-05-06 11:17:56 +0200

    Add Mockup

---
+++ src/components/Mockup.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import faceTheFactsMockUp from '../assets/images/Mockup.png';
+const Mockup: React.FC = () => (
+	<div>
+		<img src={faceTheFactsMockUp} alt="logo" />
+	</div>
+);
+export default Mockup;
