9c51860c989392b7132e8b9d3c6adad003beb1a7
RichardKruemmel <richard.kruemmel@code.berlin>
2021-05-07 18:23:15 +0200

    Run prettier and fix eslint errors

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,50 +1,53 @@
-import { IonApp, IonButton, IonCardSubtitle, IonCardTitle, IonContent, IonTitle } from '@ionic/react';
+import { IonApp, IonButton, IonCardSubtitle, IonCardTitle, IonContent } from '@ionic/react';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import HomeNavBar from '../components/HomeNavBar'
-import Logo from '../components/Logo'
-import Mockup from '../components/Mockup'
+import HomeNavBar from '../components/HomeNavBar';
+import Mockup from '../components/Mockup';
 import './Home.css';
-
 
 /* Define the React component (FC stands for Functional Components, as opposed to class-based components) */
 const Home: React.FC = () => {
 	/* This is returned when using this component */
 
 	return (
-        <IonApp>
-		<HomeNavBar />
-		<IonContent>
-			<div className="content">
-				<div className="text">
-					<IonCardTitle className="home-title">Wähl nicht irgendwen.</IonCardTitle>
-					<IonCardSubtitle className="home-subtitle">Mit Face the Facts Wahlplakate scannen und <br />herausfinden wofür Politiker:innen wirklich stehen.</IonCardSubtitle>
-					<IonButton href="/politician/1/position" className="home-button">Zur Demo mit Philipp Amthor</IonButton>
+		<IonApp>
+			<HomeNavBar />
+			<IonContent>
+				<div className="content">
+					<div className="text">
+						<IonCardTitle className="home-title">Wähl nicht irgendwen.</IonCardTitle>
+						<IonCardSubtitle className="home-subtitle">
+							Mit Face the Facts Wahlplakate scannen und <br />
+							herausfinden wofür Politiker:innen wirklich stehen.
+						</IonCardSubtitle>
+						<IonButton href="/politician/1/position" className="home-button">
+							Zur Demo mit Philipp Amthor
+						</IonButton>
+					</div>
+					<div className="mockup">
+						<Mockup />
+					</div>
 				</div>
-				<div className="mockup">
-					<Mockup />
+				<div className="links-bottom">
+					<NavLink
+						id="legal-notice-bottom"
+						to={'/legal-notice'}
+						className="navbar"
+						activeClassName="navbar activ"
+					>
+						Impressum
+					</NavLink>
+					<NavLink
+						id="privacy-bottom"
+						to={'/privacy'}
+						className="navbar"
+						activeClassName="navbar activ"
+					>
+						Datenschutz
+					</NavLink>
 				</div>
-			</div>
-			<div className="links-bottom">
-				<NavLink
-				id="legal-notice-bottom"
-				to={'/legal-notice'}
-				className="navbar"
-                activeClassName="navbar activ"
-                >
-					Impressum
-				</NavLink>
-				<NavLink
-					id="privacy-bottom"
-					to={`/privacy`}
-					className="navbar"
-					activeClassName="navbar activ"
-				>
-					Datenschutz
-				</NavLink>
-			</div>
-		</IonContent> 
-        </IonApp>
+			</IonContent>
+		</IonApp>
 	);
 };
 
