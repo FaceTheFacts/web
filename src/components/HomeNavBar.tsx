ebf877bc78f5654f93ac279b23595d2daf3a1729
RichardKruemmel <richard.kruemmel@code.berlin>
2021-05-07 12:32:24 +0200

    Add NavBar for Landing page as own Component

---
+++ src/components/HomeNavBar.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Logo from '../components/Logo'
+import './HomeNavBar.css';
+
+
+/* Define the React component (FC stands for Functional Components, as opposed to class-based components) */
+const HomeNavBar: React.FC = () => {
+	/* This is returned when using this component */
+
+	return (
+
+        <div className="nav-header">
+            <NavLink
+                id="home"
+                to={'/'}
+            >
+                <Logo />
+            </NavLink>
+        <div className="links"><NavLink
+            id="legal-notice"
+            to={'/legal-notice'}
+            className="navbar"
+            activeClassName="navbar activ"
+        >
+            Impressum
+        </NavLink>
+        <NavLink
+            id="privacy"
+            to={`/privacy`}
+            className="navbar"
+            activeClassName="navbar activ"
+        >
+            Datenschutz
+        </NavLink>
+        </div>
+        </div>
+
+);
+};
+
+export default HomeNavBar;
