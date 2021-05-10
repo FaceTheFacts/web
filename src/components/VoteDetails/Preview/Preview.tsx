4a4078c9fa995db503c2ad9ae424d881fd4534bb
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-10 14:04:43 +0200

    Add styling

--- src/components/VoteDetails/Preview/Preview.tsx
+++ src/components/VoteDetails/Preview/Preview.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
 
+import './Preview.css';
+
 interface PreviewProps {
   title: string;
   content: string;
 }
 const Preview: React.FC <PreviewProps> = (props: PreviewProps) => (
-  <div>
-    <div>{props.title}</div>
-    <div>{props.content}</div>
+  <div className='preview-container'>
+    <div className='preview-title'>{props.title}</div>
+    <div className='preview-content'>{props.content}</div>
   </div>
 )
 export default Preview;
