f8cfc42dda252938bbe160443a7da48a7a841c70
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-10 14:37:45 +0200

    Add styling to the button

--- src/components/VoteDetails/Preview/Preview.tsx
+++ src/components/VoteDetails/Preview/Preview.tsx
@@ -10,6 +10,7 @@ const Preview: React.FC <PreviewProps> = (props: PreviewProps) => (
   <div className='preview-container'>
     <div className='preview-title'>{props.title}</div>
     <div className='preview-content'>{props.content}</div>
+    <button className='preview-button'>mehr lesen</button>
   </div>
 )
 export default Preview;
