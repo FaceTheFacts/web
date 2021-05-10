c4ae3edf6c4f5260a37c42cfc895ce707bf1a202
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-10 10:05:35 +0200

    Write a test

--- src/components/VoteDetails/VotesDetailsToolbar/VoteDetailsToolbar.tsx
+++ src/components/VoteDetails/VotesDetailsToolbar/VoteDetailsToolbar.tsx
@@ -4,7 +4,7 @@ import CrossImage from '../../../assets/images/cross.svg';
 import './VoteDetailsToolbar.css'
 const VoteDetailsToolbar: React.FC = () => (
   <div className='details-toolbar-container'>
-    <div>Gesetzesentwurf</div>
+    <div data-testid='votes-details-toolbar-title' >Gesetzesentwurf</div>
     <div><img src = {CrossImage} alt = 'cross'/></div>
   </div>
 
