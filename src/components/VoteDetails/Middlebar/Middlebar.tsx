d13cfde243f719235d3caf30f650a7f02b9d833b
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-10 15:50:07 +0200

    Create a component and add a styling for it

---
+++ src/components/VoteDetails/Middlebar/Middlebar.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import Positioning from '../../Positioning';
+
+import './Middlebar.css';
+
+interface MiddlebarProps {
+  postioning: string;
+  result: string;
+}
+
+const Middlebar: React.FC<MiddlebarProps> = (props: MiddlebarProps) => (
+  <div className='middlebar-container'>
+    <div className='middlebar-vote-container'>
+      <div className='middlebar-vote-item'>Kandidat:in
+  stimmte mit </div>
+      <Positioning positioning={props.postioning}/>
+    </div>
+    <div className='middlebar-border'>
+    </div>
+    <div className='middlebar-result'>
+      {props.result}
+    </div>
+  </div>
+);
+
+export default Middlebar;
