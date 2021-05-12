36675051204785dbc20392405dcb3ba0e03cf9c8
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-12 11:15:43 +0200

    Create a party chip and add style

---
+++ src/components/VoteDetails/Table/Table/PartyChips/PartyChip/PartyChip.tsx
@@ -0,0 +1,14 @@
+import { IonChip } from '@ionic/react';
+import * as React from 'react';
+import './PartyChip.css';
+interface PartyChipProps {
+  party: string;
+}
+
+const PartyChip: React.FC<PartyChipProps> = (props: PartyChipProps) => (
+  <IonChip className={['party-chip',props.party].join(' ')}>
+  {props.party}
+  </IonChip>
+);
+
+export default PartyChip;
