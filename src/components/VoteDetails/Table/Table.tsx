6eccef7bb4b0cef8b194ca7a5e22c788fd6aad18
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-13 11:49:54 +0200

    Create a new class

--- src/components/VoteDetails/Table/Table.tsx
+++ src/components/VoteDetails/Table/Table.tsx
@@ -2,21 +2,27 @@ import React from 'react';
 import { VoteResult } from '../../../Types';
 import Positioning from '../../Positioning';
 import './Table.css';
+import PartyChip from './Table/PartyChips/PartyChip/PartyChip';
+import PartyChips from './Table/PartyChips/PartyChips';
 
 interface TableProps {
   result: VoteResult;
 }
 
 const Table: React.FC = () => (
-  <div className='table-frame'>
-    <div className='table-column-container'>
-      <Positioning positioning = 'yes'/>
-      <Positioning positioning = 'no'/>
-      <Positioning positioning = 'abstain'/>
-      <Positioning positioning = 'absent'/>
+  <div className='table'>
+    <div>
+      <PartyChips parties = {['cdu', 'spd','fdp', 'grüne']}/>
+    </div>
+    <div className='table-content-frame'>
+      <div className='table-column-container'>
+        <Positioning positioning = 'yes'/>
+        <Positioning positioning = 'no'/>
+        <Positioning positioning = 'abstain'/>
+        <Positioning positioning = 'absent'/>
+      </div>
     </div>
   </div>
-
 );
 
 export default Table;
