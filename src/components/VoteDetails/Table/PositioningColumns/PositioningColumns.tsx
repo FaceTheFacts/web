b406fff063c8213d79670b1f1300c30e0109a8e3
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-14 14:06:44 +0200

    Create a PositioningColumns comonent and add style

---
+++ src/components/VoteDetails/Table/PositioningColumns/PositioningColumns.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import Positioning from '../../../Positioning';
+import './PositioningColumns.css';
+
+const positionings = ['yes', 'no', 'abstain','absent']
+
+const PositioningColumns: React.FC = () => (
+  <div className='positioning-columns-container'>
+    {
+      positionings.map((position, index)=>(
+        <div key={`positioning-column-${index}`} className='positioning-columns'>
+          <Positioning positioning = {position}/>
+        </div>
+      ))
+    }
+  </div>
+);
+export default PositioningColumns;
