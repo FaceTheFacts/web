465340ca11fb96ca77f357654b1c45f446e6502a
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-14 14:20:21 +0200

    Refactoring

--- src/components/VoteDetails/Table/Table.tsx
+++ src/components/VoteDetails/Table/Table.tsx
@@ -1,41 +1,66 @@
 import React from 'react';
-import { VoteResult } from '../../../Types';
-import Positioning from '../../Positioning';
 import './Table.css';
-import PartyChips from './PartyChips/PartyChips';
-import NumberChip from './NumberChips/NumberChip/NumberChip';
-import NumberChips from './NumberChips/NumberChips';
+import Content from './Content/Content';
+import PositioningColumns from './PositioningColumns/PositioningColumns';
 
-interface TableProps {
-  result: VoteResult;
-}
-
-const temporalInput = {
+const totalVote = {
   votes:{
     ja: 432,
     nein: 176,
     enthalten: 21,
     abwesend: 79,
   },
-  majority:'ja'
-}
+  majority:'ja',
+};
 
+const partyVoteInputs = [
+  {
+    party: 'cdu',
+    votes:{
+    ja: 244,
+    nein: 0,
+    enthalten: 0,
+    abwesend: 20,
+  },
+  majority:'ja'},
+  {
+    party: 'spd',
+    votes:{
+    ja: 126,
+    nein: 7,
+    enthalten: 2,
+    abwesend: 17,
+  },
+  majority:'ja'},
+  {
+    party: 'fdp',
+    votes:{
+    ja: 68,
+    nein: 2,
+    enthalten: 5,
+    abwesend: 5,
+  },
+  majority:'ja'},
+  {
+    party: 'grüne',
+    votes:{
+    ja: 14,
+    nein: 32,
+    enthalten: 14,
+    abwesend: 7,
+  },
+  majority:'nein'},
+]
+const tempInput = {
+  totalVote,
+  partyVoteInputs
+}
 const Table: React.FC = () => (
   <div className='table'>
-    <div className='table-row'>
-      <PartyChips parties = {['cdu', 'spd','fdp', 'grüne']}/>
+    <div className='table-positioning'>
+      <PositioningColumns/>
     </div>
-    <div className='table-content-frame'>
-      <div className='table-column-container'>
-        <Positioning positioning = 'yes'/>
-        <Positioning positioning = 'no'/>
-        <Positioning positioning = 'abstain'/>
-        <Positioning positioning = 'absent'/>
-      </div>
-      <div className='table-column-total'>
-        <NumberChips votes = {temporalInput.votes} majority={temporalInput.majority}/>
-      </div>
-    </div>
+    <Content totalVote = {tempInput.totalVote} partyVote={tempInput.partyVoteInputs}/>
   </div>
 );
 
