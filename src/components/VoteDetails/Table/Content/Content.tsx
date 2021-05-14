766dfdfa288c0cb6a870475c617da6fb9ea47396
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-14 13:42:03 +0200

    Create a Content comonent and add style for it

---
+++ src/components/VoteDetails/Table/Content/Content.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ColumnType } from './Columns/Column/Column';
+import Columns from './Columns/Columns';
+
+import TotalColumn, {TotalColumnType } from './Columns/TotalColumn/TotalColumn';
+
+import './Content.css';
+
+interface ContentProps {
+  totalVote: TotalColumnType;
+  partyVote: Array<ColumnType>;
+}
+
+const Content: React.FC<ContentProps> = (props: ContentProps)=> (
+  <div className='content-container'>
+    <TotalColumn votes = {props.totalVote.votes} majority = {props.totalVote.majority}/>
+    <div className = 'content-line'></div>
+    <Columns votes = {props.partyVote}/>
+  </div>
+);
+
+export default Content;
