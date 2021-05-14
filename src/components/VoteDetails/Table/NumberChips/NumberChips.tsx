7945b8ec720f68ed33c52bd50781263ea0f28e43
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-14 07:42:22 +0200

    Create a component and add styling for it

---
+++ src/components/VoteDetails/Table/NumberChips/NumberChips.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import NumberChip from './NumberChip/NumberChip';
+import './NumberChips.css';
+
+type VoteNumbers = {
+  ja: number;
+  nein: number;
+  enthalten: number;
+  abwesend: number;
+}
+
+interface NumberChipsProps {
+  votes: VoteNumbers;
+  majority: string;
+}
+
+
+const NumberChips: React.FC<NumberChipsProps> = (props: NumberChipsProps) => (
+  <div className='number-chips-container'>
+    <div className='number-chips-item'>
+      <NumberChip voteNumber={props.votes.ja} majority={props.majority==='ja'?'ja':''}/>
+    </div>
+    <div className='number-chips-item'>
+      <NumberChip voteNumber={props.votes.nein} majority={props.majority==='nein'?'nein':''}/>
+    </div>
+    <div className='number-chips-item'>
+      <NumberChip voteNumber={props.votes.enthalten} majority={props.majority==='enthalten'?'enthalten':''}/>
+    </div>
+    <div className='number-chips-item'>
+      <NumberChip voteNumber={props.votes.abwesend} majority={props.majority==='abwesend'?'abwesend':''}/>
+    </div>
+  </div>
+)
+
+export default NumberChips
