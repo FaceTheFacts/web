b92a007783fcd1c405bd7d269cbc33f69369b29e
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-17 21:46:06 +0200

    Add ReadMore function

--- src/components/VoteCard/VoteDetails/Preview/Preview.tsx
+++ src/components/VoteCard/VoteDetails/Preview/Preview.tsx
@@ -6,11 +6,28 @@ interface PreviewProps {
 	title: string;
 	content: string;
 }
-const Preview: React.FC<PreviewProps> = (props: PreviewProps) => (
-	<div className="preview-container">
-		<div className="preview-title">{props.title}</div>
-		<div className="preview-content">{props.content}</div>
-		<button className="preview-button">mehr lesen</button>
-	</div>
-);
+const Preview: React.FC<PreviewProps> = (props: PreviewProps) => {
+	const [isReadMore, setIsReadMore] = React.useState(true);
+	const toggleReadMore = () => {
+		setIsReadMore(!isReadMore);
+	};
+
+	const wordLimit = (text: string, limit: number) => {
+		const split = text.split(' ');
+		return split.slice(0, limit).join(' ');
+	};
+
+	return (
+		<div className="preview-container">
+			<div className="preview-title">{props.title}</div>
+			<div className="preview-content">
+				{isReadMore ? wordLimit(props.content, 40) : props.content}
+			</div>
+			<button className="preview-button" onClick={toggleReadMore}>
+				{isReadMore ? 'mehr lesen' : 'weniger lesen'}
+			</button>
+		</div>
+	);
+};
+
 export default Preview;
