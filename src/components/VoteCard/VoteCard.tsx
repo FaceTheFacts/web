5a42e20326c655308ce48b94f83bc9ecc3949895
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-18 10:32:57 +0200

    Remove comments

--- src/components/VoteCard/VoteCard.tsx
+++ src/components/VoteCard/VoteCard.tsx
@@ -105,68 +105,6 @@ const VoteCard: React.FC<ContainerProps> = ({ vote }: ContainerProps) => {
 		);
 	});
 
-	//Temporal Input
-	// const preview = {
-	// 	title: 'Verlängerung des Bundeswehreinsatzes in Südsudan (Friedensmission UNMISS)',
-	// 	content:
-	// 		'Die Bundesregierung beruft sich bei ihrem Antrag auf das Schutzmandat der Vereinten Nationen und das System gegenseitiger kollektiver Sicherheit. Die Zivilbevölkerung müsse vor der bewaffneten Auseinandersetzung zwischen Regierungs- und Oppositionsgruppen geschützt werden.',
-	// };
-	// const totalVote = {
-	// 	votes: {
-	// 		ja: 432,
-	// 		nein: 176,
-	// 		enthalten: 21,
-	// 		abwesend: 79,
-	// 	},
-	// 	majority: 'ja',
-	// };
-
-	// const partyVoteInputs = [
-	// 	{
-	// 		party: 'cdu',
-	// 		votes: {
-	// 			ja: 244,
-	// 			nein: 0,
-	// 			enthalten: 0,
-	// 			abwesend: 20,
-	// 		},
-	// 		majority: 'ja',
-	// 	},
-	// 	{
-	// 		party: 'spd',
-	// 		votes: {
-	// 			ja: 126,
-	// 			nein: 7,
-	// 			enthalten: 2,
-	// 			abwesend: 17,
-	// 		},
-	// 		majority: 'ja',
-	// 	},
-	// 	{
-	// 		party: 'fdp',
-	// 		votes: {
-	// 			ja: 68,
-	// 			nein: 2,
-	// 			enthalten: 5,
-	// 			abwesend: 5,
-	// 		},
-	// 		majority: 'ja',
-	// 	},
-	// 	{
-	// 		party: 'grüne',
-	// 		votes: {
-	// 			ja: 14,
-	// 			nein: 32,
-	// 			enthalten: 14,
-	// 			abwesend: 7,
-	// 		},
-	// 		majority: 'nein',
-	// 	},
-	// ];
-	// const tempInput = {
-	// 	totalVote,
-	// 	partyVoteInputs,
-	// };
 	return (
 		<React.Fragment>
 			<IonCard className="vote-card" onClick={(): void => setShowDetails(!showDetails)}>
