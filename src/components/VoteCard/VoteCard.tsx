d7488da1ce2611f60c2e43f98e3ed81fd1664251
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-19 07:41:45 +0200

    Add backdrop

--- src/components/VoteCard/VoteCard.tsx
+++ src/components/VoteCard/VoteCard.tsx
@@ -168,6 +168,7 @@ const VoteCard: React.FC<ContainerProps> = ({ vote }: ContainerProps) => {
 					/>
 				</IonModal>
 			</IonContent>
+			{showDetails?<div className='overlay'></div>:undefined}
 		</React.Fragment>
 	);
 };
