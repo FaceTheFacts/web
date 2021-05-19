0698301651e1fa0d20f3662849115e6989db4c5d
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-19 10:19:08 +0200

    Add types

--- src/components/VoteCard/VoteDetails/Table/PartyChips/PartyChip/PartyChip.tsx
+++ src/components/VoteCard/VoteDetails/Table/PartyChips/PartyChip/PartyChip.tsx
@@ -2,7 +2,7 @@ import { IonChip } from '@ionic/react';
 import * as React from 'react';
 import './PartyChip.css';
 interface PartyChipProps {
-	party: string;
+	party?: string;
 }
 
 const PartyChip: React.FC<PartyChipProps> = (props: PartyChipProps) => (
