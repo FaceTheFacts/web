7cb62a57efe45183ae0038aaa3f4bea3a1484fd4
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-20 15:22:29 +0200

    Change styling and add state for a modal

--- src/components/VoteExplainerCard.tsx
+++ src/components/VoteExplainerCard.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './VoteExplainerCard.css';
-import { IonCardSubtitle, IonBackButton } from '@ionic/react';
+import { IonCardSubtitle, IonBackButton, IonButton, IonIcon } from '@ionic/react';
+import { chevronForward } from 'ionicons/icons';
 interface ContainerProps {
 	vote: string;
 }
 // eslint-disable-next-line
 const VoteExplainerCard: React.FC<ContainerProps> = ({ vote }: ContainerProps) => {
+	const [showModal, setShowModal] = useState(false)
 	return (
 		<div className="vote-explainer-card">
 			<IonCardSubtitle>Wie funktioniert die {vote}?</IonCardSubtitle>
-			<IonBackButton className="forward-button" defaultHref="profile" text="" />
+			<IonButton className="forward-button" fill = "clear" onClick={(): void=>setShowModal(!showModal)}>
+				<IonIcon slot="icon-only" icon={chevronForward} />
+			</IonButton>
 		</div>
 	);
 };
