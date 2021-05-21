6c02fbf914433579a9a70543b5314733bf7bbf20
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-21 09:59:22 +0200

    Add modal

--- src/components/VoteExplainerCard.tsx
+++ src/components/VoteExplainerCard.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import './VoteExplainerCard.css';
-import { IonCardSubtitle, IonBackButton, IonButton, IonIcon } from '@ionic/react';
+import { IonCardSubtitle, IonButton, IonIcon, IonContent, IonModal, IonPopover, IonBackdrop} from '@ionic/react';
 import { chevronForward } from 'ionicons/icons';
+import FirstVotePopup from './PopupCard/FirstVotePopup/FirstVotePopup';
 interface ContainerProps {
 	vote: string;
 }
@@ -9,12 +10,21 @@ interface ContainerProps {
 const VoteExplainerCard: React.FC<ContainerProps> = ({ vote }: ContainerProps) => {
 	const [showModal, setShowModal] = useState(false)
 	return (
-		<div className="vote-explainer-card">
-			<IonCardSubtitle>Wie funktioniert die {vote}?</IonCardSubtitle>
-			<IonButton className="forward-button" fill = "clear" onClick={(): void=>setShowModal(!showModal)}>
-				<IonIcon slot="icon-only" icon={chevronForward} />
-			</IonButton>
-		</div>
+		<React.Fragment>
+			<div className="vote-explainer-card">
+				<IonCardSubtitle>Wie funktioniert die {vote}?</IonCardSubtitle>
+				<IonButton className="forward-button" fill = "clear" onClick={(): void=>setShowModal(!showModal)}>
+					<IonIcon slot="icon-only" icon={chevronForward} />
+				</IonButton>
+			</div>
+			{showModal ?
+			<button className='explainer-modal' onClick={(): void=>setShowModal(!showModal)}>
+				<div className='explainer-modalcard-container'>
+					<FirstVotePopup/>
+				</div>
+			</button> 	
+			: undefined}
+		</React.Fragment>
 	);
 };
 
