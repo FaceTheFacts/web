5a62b3c65f1283a6a0a10aec5be7894a1a68f72d
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-21 10:08:20 +0200

    Run prettier

--- src/components/PopupCard/FirstVotePopup/FirstVotePopup.tsx
+++ src/components/PopupCard/FirstVotePopup/FirstVotePopup.tsx
@@ -6,13 +6,25 @@ import firstVote from '../../../assets/images/first-vote.svg';
 import ImageAndSentence from '../ImageAndSentence/ImageAndSentence';
 
 const FirstVotePopup: React.FC = () => (
-  <PopupCard title='Wie funktioniert die Erststimme?' bottomContent={['Gewählte Direktkandidat:innen ziehen auf jeden Fall in den Bundestag ein, selbst wenn ihre Partei nicht über die 5% der Stimmen gekommen ist.']}>
-    <div>
-      <ImageAndSentence image = {personLogo} imageAlt='person' sentence = 'Mit der Erststimme wählst du eine Kandidat:in deines Wahlkreises direkt in den Bundestag.'/>
-      <ImageAndSentence image = {firstVote} imageAlt='first-vote' sentence = 'Es gewinnt die Person mit den meisten Stimmen im jeweiligen Wahlkreis.'/>
-    </div>
-  </PopupCard>
-
+	<PopupCard
+		title="Wie funktioniert die Erststimme?"
+		bottomContent={[
+			'Gewählte Direktkandidat:innen ziehen auf jeden Fall in den Bundestag ein, selbst wenn ihre Partei nicht über die 5% der Stimmen gekommen ist.',
+		]}
+	>
+		<div>
+			<ImageAndSentence
+				image={personLogo}
+				imageAlt="person"
+				sentence="Mit der Erststimme wählst du eine Kandidat:in deines Wahlkreises direkt in den Bundestag."
+			/>
+			<ImageAndSentence
+				image={firstVote}
+				imageAlt="first-vote"
+				sentence="Es gewinnt die Person mit den meisten Stimmen im jeweiligen Wahlkreis."
+			/>
+		</div>
+	</PopupCard>
 );
 
 export default FirstVotePopup;
--- src/components/PopupCard/ImageAndSentence/ImageAndSentence.tsx
+++ src/components/PopupCard/ImageAndSentence/ImageAndSentence.tsx
@@ -3,20 +3,18 @@ import React from 'react';
 import './ImageAndSentence.css';
 
 interface ImageAndSentenceProps {
-  image: string;
-  imageAlt: string;
-  sentence: string;
+	image: string;
+	imageAlt: string;
+	sentence: string;
 }
 
-const ImageAndSentence: React.FC <ImageAndSentenceProps> = (props: ImageAndSentenceProps)=> (
-  <div className='img-sentence-container'>
-    <div className='img-sentence-img'>
-      <img src ={props.image} alt = {props.imageAlt}/>
-    </div>
-    <div className='img-sentence'>
-      {props.sentence}
-    </div>
-  </div>
+const ImageAndSentence: React.FC<ImageAndSentenceProps> = (props: ImageAndSentenceProps) => (
+	<div className="img-sentence-container">
+		<div className="img-sentence-img">
+			<img src={props.image} alt={props.imageAlt} />
+		</div>
+		<div className="img-sentence">{props.sentence}</div>
+	</div>
 );
 
 export default ImageAndSentence;
