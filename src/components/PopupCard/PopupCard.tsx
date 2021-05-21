741eaecbca4644b9b163981865e13e2bd1e07ac2
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-21 10:08:20 +0200

    Run prettier

--- src/components/PopupCard/PopupCard.tsx
+++ src/components/PopupCard/PopupCard.tsx
@@ -4,30 +4,26 @@ import React from 'react';
 import './PopupCard.css';
 
 interface PopupCardProps {
-  title: string;
-  children?: JSX.Element;
-  bottomContent?: Array<string>|undefined;
+	title: string;
+	children?: JSX.Element;
+	bottomContent?: Array<string> | undefined;
 }
-const PopupCard: React.FC<PopupCardProps>= (props: PopupCardProps)=>(
-  <IonCard className='PopupCard'>
-    <IonCardHeader className='popup-header'>
-      <IonCardTitle className='popup-title'>
-        {props.title}
-      </IonCardTitle>
-    </IonCardHeader>
-    <div className='popup-border'></div>
-    <IonCardContent className='popup-content'>
-      {props.children}
-    </IonCardContent>
-    <div className='popup-border'></div>
-    <IonCardContent className='popup-content'>
-      {props.bottomContent?.map((sentence, index)=>(
-        <div className='bottom-sentence' key={`bottom-sentence-${index}`}>
-          {sentence}
-        </div>
-      ))}
-    </IonCardContent>
-  </IonCard>
+const PopupCard: React.FC<PopupCardProps> = (props: PopupCardProps) => (
+	<IonCard className="PopupCard">
+		<IonCardHeader className="popup-header">
+			<IonCardTitle className="popup-title">{props.title}</IonCardTitle>
+		</IonCardHeader>
+		<div className="popup-border"></div>
+		<IonCardContent className="popup-content">{props.children}</IonCardContent>
+		<div className="popup-border"></div>
+		<IonCardContent className="popup-content">
+			{props.bottomContent?.map((sentence, index) => (
+				<div className="bottom-sentence" key={`bottom-sentence-${index}`}>
+					{sentence}
+				</div>
+			))}
+		</IonCardContent>
+	</IonCard>
 );
 
 export default PopupCard;
--- src/components/VoteExplainerCard.tsx
+++ src/components/VoteExplainerCard.tsx
@@ -1,30 +1,31 @@
 import React, { useState } from 'react';
 import './VoteExplainerCard.css';
-import { IonCardSubtitle, IonButton, IonIcon} from '@ionic/react';
+import { IonCardSubtitle, IonButton, IonIcon } from '@ionic/react';
 import { chevronForward } from 'ionicons/icons';
-import FirstVotePopup from './PopupCard/FirstVotePopup/FirstVotePopup';
 interface ContainerProps {
 	vote: string;
 	children?: JSX.Element;
 }
-// eslint-disable-next-line
-const VoteExplainerCard: React.FC<ContainerProps> = (props:  ContainerProps) => {
-	const [showModal, setShowModal] = useState(false)
+
+const VoteExplainerCard: React.FC<ContainerProps> = (props: ContainerProps) => {
+	const [showModal, setShowModal] = useState(false);
 	return (
 		<React.Fragment>
 			<div className="vote-explainer-card">
 				<IonCardSubtitle>Wie funktioniert die {props.vote}?</IonCardSubtitle>
-				<IonButton className="forward-button" fill = "clear" onClick={(): void=>setShowModal(!showModal)}>
+				<IonButton
+					className="forward-button"
+					fill="clear"
+					onClick={(): void => setShowModal(!showModal)}
+				>
 					<IonIcon slot="icon-only" icon={chevronForward} />
 				</IonButton>
 			</div>
-			{showModal ?
-			<button className='explainer-modal' onClick={(): void=>setShowModal(!showModal)}>
-				<div className='explainer-modalcard-container'>
-					{props.children}
-				</div>
-			</button> 	
-			: undefined}
+			{showModal ? (
+				<button className="explainer-modal" onClick={(): void => setShowModal(!showModal)}>
+					<div className="explainer-modalcard-container">{props.children}</div>
+				</button>
+			) : undefined}
 		</React.Fragment>
 	);
 };
