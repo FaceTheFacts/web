1b3f90870e87d52d883c18bf8e9b20c3da32df57
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-21 16:44:47 +0200

    Use modal

--- src/components/VoteExplainerCard/VoteExplainerCard.tsx
+++ src/components/VoteExplainerCard/VoteExplainerCard.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './VoteExplainerCard.css';
-import { IonCardSubtitle, IonButton, IonIcon } from '@ionic/react';
+import { IonCardSubtitle, IonButton, IonIcon, IonModal, IonContent } from '@ionic/react';
 import { chevronForward } from 'ionicons/icons';
 interface ContainerProps {
 	vote: string;
@@ -21,11 +21,19 @@ const VoteExplainerCard: React.FC<ContainerProps> = (props: ContainerProps) => {
 					<IonIcon slot="icon-only" icon={chevronForward} />
 				</IonButton>
 			</div>
-			{showModal ? (
-				<button className="explainer-modal" onClick={(): void => setShowModal(!showModal)}>
-					<div className="explainer-modalcard-container">{props.children}</div>
-				</button>
-			) : undefined}
+			<IonContent>
+				<IonModal
+					isOpen={showModal}
+					swipeToClose={true}
+					onDidDismiss={(): void => setShowModal(false)}
+					cssClass= 'explainer-modal'
+
+				>
+				<div className="explainer-modalcard-container">
+					{props.children}
+				</div>
+				</IonModal>
+			</IonContent>
 		</React.Fragment>
 	);
 };
