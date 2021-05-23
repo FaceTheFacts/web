ba445c4effe97e5f4ac431edfd147300bf073c8f
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-23 21:13:56 +0200

    Add a modal

--- src/components/PositionCards/PositionCard/PositionCard.tsx
+++ src/components/PositionCards/PositionCard/PositionCard.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { IonCard, IonCardHeader, IonCardTitle, IonIcon } from '@ionic/react';
+import { IonCard, IonCardHeader, IonCardTitle, IonContent, IonIcon, IonModal } from '@ionic/react';
 import Positioning from '../Positioning/Positioning';
 
 import './PositionCard.css';
 import { chevronForwardOutline } from 'ionicons/icons';
+import PositionPopup from '../../PopupCard/PositionPopup/PositionPopup';
 
 interface PostionCardProps {
 	candidateId: number;
@@ -11,28 +12,47 @@ interface PostionCardProps {
 	answer: string;
 }
 
-const PositionCard: React.FC<PostionCardProps> = ({ title, answer }: PostionCardProps) => {
+const PositionCard: React.FC<PostionCardProps> = (props: PostionCardProps) => {
+	const [showModal, setShowModal] = React.useState(false);
 	return (
-		<IonCard className="position-card">
-			<IonCardHeader className="position-card-header">
-				<div className="position-card-container">
-					<div className="position-card-title">
-						<IonCardTitle
-							className="position-card-title"
-							data-testid="position-card-title"
-						>
-							{title}
-						</IonCardTitle>
-					</div>
-					<div className="position-card-sub-content">
-						<div className="position-positioning">
-							<Positioning positioning={answer} />
+		<React.Fragment>
+			<IonCard className="position-card" onClick={(): void => setShowModal(!showModal)}>
+				<IonCardHeader className="position-card-header">
+					<div className="position-card-container">
+						<div className="position-card-title">
+							<IonCardTitle
+								className="position-card-title"
+								data-testid="position-card-title"
+							>
+								{props.title}
+							</IonCardTitle>
 						</div>
-						<IonIcon slot="icon-only" icon={chevronForwardOutline} />
+						<div className="position-card-sub-content">
+							<div className="position-positioning">
+								<Positioning positioning={props.answer} />
+							</div>
+							<IonIcon slot="icon-only" icon={chevronForwardOutline} />
+						</div>
 					</div>
-				</div>
-			</IonCardHeader>
-		</IonCard>
+				</IonCardHeader>
+				<IonContent>
+					<IonModal
+						isOpen={showModal}
+						swipeToClose={true}
+						onDidDismiss={(): void => setShowModal(false)}
+						cssClass="explainer-modal"
+					>
+						<div className="position-card-popup">
+							<PositionPopup
+								title={props.title}
+								positioning={props.answer}
+								reason="Reasons for the position"
+							/>
+						</div>
+					</IonModal>
+				</IonContent>
+			</IonCard>
+		</React.Fragment>
 	);
 };
 
