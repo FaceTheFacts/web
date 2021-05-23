9c18e2fffe672e3d5b84d098f5e9e1d2a111199f
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-23 21:02:30 +0200

    Create a new pop up

---
+++ src/components/PopupCard/PositionPopup/PositionPopup.tsx
@@ -0,0 +1,27 @@
+import { IonCard, IonCardContent, IonCardHeader, IonCardTitle } from '@ionic/react';
+import React from 'react';
+import Positioning from '../../PositionCards/Positioning/Positioning';
+
+import './PositionPopup.css';
+
+interface PositionPopupProps {
+	title: string;
+	positioning: string;
+	reason: string;
+}
+const PositionPopup: React.FC<PositionPopupProps> = (props: PositionPopupProps) => (
+	<IonCard className="PopupCard">
+		<IonCardHeader className="popup-header">
+			<IonCardTitle className="popup-title">{props.title}</IonCardTitle>
+		</IonCardHeader>
+		<div className="popup-border"></div>
+		<div className="position-popup-positioning">
+			<Positioning positioning={props.positioning} />
+		</div>
+		<IonCardContent className="popup-content">
+			<div className="position-popup-bottom">{props.reason}</div>
+		</IonCardContent>
+	</IonCard>
+);
+
+export default PositionPopup;
