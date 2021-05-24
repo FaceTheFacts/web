45972b47d2f718cd09a35e73a27fad32ce13238b
RichardKruemmel <richard.kruemmel@code.berlin>
2021-05-24 21:21:43 +0200

    Add content to SignUp

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import { IonIcon } from '@ionic/react';
-import { chevronForward } from 'ionicons/icons';
 import './SignUp.css';
+import SignUpButton from './SignUpButton';
 
 /* Define the React component (FC stands for Functional Components, as opposed to class-based components) */
 const SignUp: React.FC = () => {
 	/* This is returned when using this component */
 
 	return (
-		<div>
+		<div className="signup">
             <div className="signup-text">Als Erste:r erfahren, sobald<br className="signup-break"/> Face The Facts verfügbar ist</div>
             <div>
                 <div></div>
                 <div>
-                    <div>
+                    <div className="signup-form">
                         <form>
                             <input type="text" id="email" name="email" placeholder="Deine Mail-Adresse"></input>
-                            <button type="submit"><IonIcon className="forward-button" slot="icon-only" icon={chevronForward} /></button><br/>
+                            <button type="submit"><SignUpButton /></button><br/>
                             <input type="checkbox" id="privacy-box" name="privacy-box" value="privacy-box"></input>
-                            <label htmlFor="privacy-box">Ich bin einverstanden</label>
+                            <label htmlFor="privacy-box">Ich bin einverstanden mit der <a className="signup-link">Datenschutzerklärung</a>.</label>
                         </form>
                     </div>
                 </div>
