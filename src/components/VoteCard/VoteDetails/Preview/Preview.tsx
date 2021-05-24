dabb78b9ee219a6794f8bee73029f3b7d1be459e
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-24 21:40:24 +0200

    Fix readmore button

--- src/components/VoteCard/VoteDetails/Preview/Preview.tsx
+++ src/components/VoteCard/VoteDetails/Preview/Preview.tsx
@@ -7,24 +7,22 @@ interface PreviewProps {
 	content: string;
 }
 const Preview: React.FC<PreviewProps> = (props: PreviewProps) => {
-	// As a default, 'Read More === mehr lesen' will show up
-	const [isReadMore, setIsReadMore] = React.useState(true);
-	//isReadMore === true => sentence should be short
-	const [isShort, setIsShort] = React.useState('readmore-short');
+	const [isReadMore, setIsReadMore] = React.useState('readmore-short');
 
-	//If the button clicked
 	const readmoreChange = (): void => {
-		setIsReadMore(!isReadMore);
-		//change class
-		isReadMore ? setIsShort('readmore-short') : setIsShort('readmore-long');
+		if (isReadMore === 'readmore-short') {
+			setIsReadMore('readmore-long');
+		} else {
+			setIsReadMore('readmore-short');
+		}
 	};
 
 	return (
 		<div className="preview-container">
 			<div className="preview-title">{props.title}</div>
-			<div className={['preview-content', isShort].join(' ')}>{props.content}</div>
+			<div className={['preview-content', isReadMore].join(' ')}>{props.content}</div>
 			<button className="preview-button" onClick={readmoreChange}>
-				{isReadMore ? 'mehr lesen' : 'weniger lesen'}
+				{isReadMore === 'readmore-short' ? 'mehr lesen' : 'weniger lesen'}
 			</button>
 		</div>
 	);
