5da057fa3c4e4eb471b51a3b87d77e9f70f391b8
RichardKruemmel <richard.kruemmel@code.berlin>
2021-05-24 17:06:12 +0200

    Replace old mockup with new design

--- src/components/Mockup.tsx
+++ src/components/Mockup.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import faceTheFactsMockUp from '../assets/images/Mockup.png';
+import faceTheFactsMockUp from '../assets/images/mockup.png';
 const Mockup: React.FC = () => (
 	<div>
 		<img src={faceTheFactsMockUp} alt="logo" />
