522338634a61ce2bfa6bb7e6812a610f131f821f
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-26 18:29:06 +0200

    Add different stylings for iOS and Android

--- src/components/VoteCard/VoteDetails/Middlebar/Middlebar.tsx
+++ src/components/VoteCard/VoteDetails/Middlebar/Middlebar.tsx
@@ -10,21 +10,45 @@ interface MiddlebarProps {
 }
 
 const Middlebar: React.FC<MiddlebarProps> = (props: MiddlebarProps) => {
-	return (
-		<div className="middlebar-container">
-			{/* left */}
-			<div className="middlebar-left">
-				<div className="middlebar-left-sentence">Kandidat:in stimmte mit</div>
-				<div className="middlebar-left-positioning">
-					<Positioning positioning={props.postioning} />
+	const [middlebar, setMiddlebar] = React.useState<JSX.Element | null>(null);
+	const operatingSystem = React.useRef('');
+	React.useEffect(() => {
+		operatingSystem.current = getMobileOperatingSystem();
+		if (operatingSystem.current === 'iOS') {
+			setMiddlebar(
+				<div className="middlebar-container">
+					{/* left */}
+					<div className="middlebar-left">
+						<div className="middlebar-left-sentence">Kandidat:in stimmte mit</div>
+						<div className="middlebar-left-positioning">
+							<Positioning positioning={props.postioning} />
+						</div>
+					</div>
+					{/* border bar */}
+					<div className="middlebar-border"></div>
+					{/* right */}
+					<div className="middlebar-right">{props.result}</div>
 				</div>
-			</div>
-			{/* border bar */}
-			<div className="middlebar-border"></div>
-			{/* right */}
-			<div className="middlebar-right">{props.result}</div>
-		</div>
-	);
+			);
+		} else {
+			setMiddlebar(
+				<div className="middlebar-container">
+					{/* left */}
+					<div className="middlebar-left">
+						<div className="middlebar-left-sentence">Kandidat:in stimmte mit</div>
+						<div className="middlebar-left-positioning">
+							<Positioning positioning={props.postioning} />
+						</div>
+					</div>
+					{/* border bar */}
+					<div className="middlebar-border-android"></div>
+					{/* right */}
+					<div className="middlebar-right-android">{props.result}</div>
+				</div>
+			);
+		}
+	}, [props]);
+	return <React.Fragment>{middlebar}</React.Fragment>;
 };
 
 export default Middlebar;
