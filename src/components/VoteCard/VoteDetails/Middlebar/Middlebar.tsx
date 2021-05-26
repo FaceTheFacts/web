9598291dc755ea98d1c23090f76f2ee4a85a29f3
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-26 18:10:56 +0200

    Add styling for iOS

--- src/components/VoteCard/VoteDetails/Middlebar/Middlebar.tsx
+++ src/components/VoteCard/VoteDetails/Middlebar/Middlebar.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { getMobileOperatingSystem } from '../../../../functions/getMobileOperatingSystem/getMobileOperatingSystem';
 import Positioning from '../../../PositionCards/Positioning/Positioning';
 
 import './Middlebar.css';
@@ -8,20 +9,22 @@ interface MiddlebarProps {
 	result: string;
 }
 
-const Middlebar: React.FC<MiddlebarProps> = (props: MiddlebarProps) => (
-	<div className="middlebar-container">
-		{/* left */}
-		<div className="middlebar-left">
-			<div className="middlebar-left-sentence">Kandidat:in stimmte mit</div>
-			<div className="middlebar-left-positioning">
-				<Positioning positioning={props.postioning} />
+const Middlebar: React.FC<MiddlebarProps> = (props: MiddlebarProps) => {
+	return (
+		<div className="middlebar-container">
+			{/* left */}
+			<div className="middlebar-left">
+				<div className="middlebar-left-sentence">Kandidat:in stimmte mit</div>
+				<div className="middlebar-left-positioning">
+					<Positioning positioning={props.postioning} />
+				</div>
 			</div>
+			{/* border bar */}
+			<div className="middlebar-border"></div>
+			{/* right */}
+			<div className="middlebar-right">{props.result}</div>
 		</div>
-		{/* border bar */}
-		<div className="middlebar-border"></div>
-		{/* right */}
-		<div className="middlebar-right">{props.result}</div>
-	</div>
-);
+	);
+};
 
 export default Middlebar;
