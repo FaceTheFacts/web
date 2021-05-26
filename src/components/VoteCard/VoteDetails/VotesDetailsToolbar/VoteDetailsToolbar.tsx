d7da710947b6b7134e90daaa95d4315d475ce7b7
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-05-26 13:58:39 +0200

    Add different component based on the type of OS

--- src/components/VoteCard/VoteDetails/VotesDetailsToolbar/VoteDetailsToolbar.tsx
+++ src/components/VoteCard/VoteDetails/VotesDetailsToolbar/VoteDetailsToolbar.tsx
@@ -1,24 +1,46 @@
 import { IonButton, IonIcon, IonToolbar } from '@ionic/react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { closeCircle } from 'ionicons/icons';
 import './VoteDetailsToolbar.css';
+import CrossImage from '../../../../assets/images/cross.svg';
+import { getMobileOperatingSystem } from '../../../../functions/getMobileOperatingSystem/getMobileOperatingSystem';
 
 interface VoteDetailsToolbarProps {
 	clicked: Function;
 }
 
-const VoteDetailsToolbar: React.FC<VoteDetailsToolbarProps> = (props: VoteDetailsToolbarProps) => (
-	<IonToolbar className="details-toolbar-container">
-		<div data-testid="votes-details-toolbar-title">Gesetzesentwurf</div>
-		<IonButton
-			className="details-toolbar-button"
-			onClick={(): void => props.clicked()}
-			slot="end"
-			fill="clear"
-		>
-			<IonIcon slot="icon-only" icon={closeCircle} />
-		</IonButton>
-	</IonToolbar>
-);
+const VoteDetailsToolbar: React.FC<VoteDetailsToolbarProps> = (props: VoteDetailsToolbarProps) => {
+	const [toolbar, setToolbar] = useState<JSX.Element | null>(null);
+	let operatingSystem;
+	useEffect(() => {
+		operatingSystem = getMobileOperatingSystem();
+		if (operatingSystem === 'iOS') {
+			setToolbar(
+				<IonToolbar className="details-toolbar-container">
+					<div data-testid="votes-details-toolbar-title">Gesetzesentwurf</div>
+					<IonButton
+						className="details-toolbar-button"
+						onClick={(): void => props.clicked()}
+						slot="end"
+						fill="clear"
+					>
+						<IonIcon slot="icon-only" icon={closeCircle} />
+					</IonButton>
+				</IonToolbar>
+			);
+		} else {
+			setToolbar(
+				<div className="details-toolbar">
+					<div data-testid="votes-details-toolbar-title">Gesetzesentwurf</div>
+					<IonButton onClick={(): void => props.clicked()} fill="clear">
+						<IonIcon slot="icon-only" icon={CrossImage} />
+					</IonButton>
+				</div>
+			);
+		}
+	}, []);
+
+	return <React.Fragment>{toolbar}</React.Fragment>;
+};
 
 export default VoteDetailsToolbar;
