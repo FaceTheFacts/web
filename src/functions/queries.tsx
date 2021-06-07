ef6216bfa63e37adc529e909351868beba4af8c3
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-06-07 09:13:26 +0200

    Run prettier

--- src/functions/queries.tsx
+++ src/functions/queries.tsx
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from 'axios';
 
-export default async function fetch(url: string)  {
+export default async function fetch(url: string) {
 	const BASE_URL = 'https://www.abgeordnetenwatch.de/api/v2/';
 	const res: AxiosResponse = await axios.get(BASE_URL + url);
 	return await res.data;
