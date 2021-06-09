865384495b6899606198f3cccefc25c70fd27bc6
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-06-09 10:38:11 +0200

    Use PartyChip component

--- src/components/ElectionChancesCard/ElectionChancesCard.tsx
+++ src/components/ElectionChancesCard/ElectionChancesCard.tsx
@@ -1,38 +1,23 @@
 import React from 'react';
-import className from 'classnames';
 import './ElectionChancesCard.css';
 import {
 	IonCard,
 	IonCardHeader,
-	IonCardSubtitle,
 	IonCardTitle,
 	IonGrid,
 	IonCol,
 	IonRow,
 } from '@ionic/react';
 import { ElectionResult } from '../../Types';
+import PartyChip from '../VoteCard/VoteDetails/Table/PartyChips/PartyChip/PartyChip';
 
 interface ContainerProps {
 	vote: ElectionResult;
 }
 
 const ElectionchancesCard: React.FC<ContainerProps> = ({ vote }: ContainerProps) => {
-	/* 
-	Internationalisation to keep the code in English but print the national language
-	*/
-
-	const partyClassName = vote.party.label.toLowerCase().replace(/\s/g, '');
-	const electionChancesCardSubtitleClass = className(
-		'election-chances-card-subtitle',
-		partyClassName
-	);
-
 	return (
 		<IonCard className="election-chances-card">
-			{/* 
-			Card header with Vote name, vote subtitle which holds the type of vote it was and the candidate's vote.
-			It's inside a css grid so we can more easily arrange the items.
-			*/}
 			<IonCardHeader className="election-chances-card-header">
 				<IonGrid>
 					<IonRow>
@@ -44,12 +29,7 @@ const ElectionchancesCard: React.FC<ContainerProps> = ({ vote }: ContainerProps)
 								>
 									{vote.politician.label}
 								</IonCardTitle>
-								<IonCardSubtitle
-									className={electionChancesCardSubtitleClass}
-									data-testid="election-chances-card-party"
-								>
-									<span>{vote.party.label}</span>
-								</IonCardSubtitle>
+								<PartyChip party={vote.party.label}/>
 							</div>
 						</IonCol>
 						<IonCol className="percentage" size="3">
