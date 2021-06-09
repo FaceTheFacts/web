aa41ffe8b1c77e19c607aabc9c947366f0f15b67
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-06-09 10:47:31 +0200

    Use classnames library

--- src/components/VoteCard/VoteDetails/Table/PartyChips/PartyChip/PartyChip.tsx
+++ src/components/VoteCard/VoteDetails/Table/PartyChips/PartyChip/PartyChip.tsx
@@ -1,12 +1,16 @@
 import { IonChip } from '@ionic/react';
 import * as React from 'react';
+import className from 'classnames';
 import './PartyChip.css';
 interface PartyChipProps {
 	party?: string;
 }
 
-const PartyChip: React.FC<PartyChipProps> = (props: PartyChipProps) => (
-	<IonChip className={['party-chip', props.party + '-1'].join(' ')}>{props.party}</IonChip>
-);
+const PartyChip: React.FC<PartyChipProps> = (props: PartyChipProps) => {
+	const partyClassName = props.party?.toLowerCase().replace(/\s/g, '');
+	const partyChipClass = className('party-chip', partyClassName, '-1');
+
+	return <IonChip className={partyChipClass}>{props.party}</IonChip>;
+};
 
 export default PartyChip;
