5446ccfd70a63e044ece019e6950e2c9148b0d79
RichardKruemmel <richard.kruemmel@code.berlin>
2021-06-17 13:36:05 +0200

    Refactoring switch statement

--- src/functions/occupationHandler.tsx
+++ src/functions/occupationHandler.tsx
@@ -11,73 +11,60 @@ export const occupationHandler = (occupation: string): JSX.Element => {
 
 	let firstPart: string;
 	let secondPart = '';
-	const stringHandler = (secondPart: string): string => {
-		switch (secondPart) {
-		case 'Fraktionsvorsitzender DIE LINKE im Bundestag':
-			return 'Fraktionsvorsitzender';
-		case 'Fraktionsvorsitzende DIE LINKE im Bundestag':
-			return 'Fraktionsvorsitzende';
-		case 'Fraktionsvorsitzender SPD im Bundestag':
-			return 'Fraktionsvorsitzender';
-		case 'Fraktionsvorsitzende BÜNDNIS 90/DIE GRÜNE im Bundestag':
-			return 'Fraktionsvorsitzende';
-		case 'Fraktionsvorsitzender BÜNDNIS 90/DIE GRÜNE im Bundestag':
-			return 'Fraktionsvorsitzender';
-		case 'Fraktionsvorsitzender FDP im Bundestag':
-			return 'Fraktionsvorsitzender';
-		case 'Fraktionsvorsitzender Afd im Bundestag':
-			return 'Fraktionsvorsitzender';
-		case 'Fraktionsvorsitzende Afd im Bundestag':
-			return 'Fraktionsvorsitzende';
-		case 'Fraktionsvorsitzender CDU/CSU im Bundestag':
-			return 'Fraktionsvorsitzender';
-			/* case 'CSU-Landesgruppenchef':
-                secondPart = 'Landesgruppenchef'; */
-		default:
-			return secondPart;
-		}
-	};
+	const inputHandler = (input: string): string => {
+		switch (input) {
+			// Fractions
+			case 'Fraktionsvorsitzender DIE LINKE im Bundestag':
+				return 'Fraktionsvorsitzender';
+			case 'Fraktionsvorsitzende DIE LINKE im Bundestag':
+				return 'Fraktionsvorsitzende';
+			case 'Fraktionsvorsitzender SPD im Bundestag':
+				return 'Fraktionsvorsitzender';
+			case 'Fraktionsvorsitzende BÜNDNIS 90/DIE GRÜNE im Bundestag':
+				return 'Fraktionsvorsitzende';
+			case 'Fraktionsvorsitzender BÜNDNIS 90/DIE GRÜNE im Bundestag':
+				return 'Fraktionsvorsitzender';
+			case 'Fraktionsvorsitzender FDP im Bundestag':
+				return 'Fraktionsvorsitzender';
+			case 'Fraktionsvorsitzender Afd im Bundestag':
+				return 'Fraktionsvorsitzender';
+			case 'Fraktionsvorsitzende Afd im Bundestag':
+				return 'Fraktionsvorsitzende';
+			case 'Fraktionsvorsitzender CDU/CSU im Bundestag':
+				return 'Fraktionsvorsitzender';
+			case 'CSU-Landesgruppenchef':
+                return 'Landesgruppenchef';
 
-	const ministryHandler = (secondPart: string): string => {
-		switch (secondPart) {
-		case 'Bundesminister des Auswärtigen':
-			return 'Außenminister';
-		case 'Bundesminister für Wirtschaft und Energie':
-			return 'Bundeswirtschaftsminister';
-		case 'Bundesministerin für Justiz und Verbraucherschutz':
-			return 'Bundesjustizministerin';
-		case 'Bundesminister für Arbeit und Soziales':
-			return 'Bundesarbeitsminister';
-		case 'Bundesminister für Gesundheit':
-			return 'Bundesgesundheitsminister';
-		case 'Bundesminister für Verkehr und digitale Infrastruktur':
-			return 'Bundesverkehrsminister';
-		case 'Bundesministerin für Umwelt, Naturschutz und nukleare Sicherheit':
-			return 'Bundesumweltministerin';
-		case 'Bundesministerin für Bildung und Forschung':
-			return 'Bundesbildungsministerin';
-		case 'Bundesminister für wirtschaftliche Zusammenarbeit und Entwicklung':
-			return 'Bundesentwicklungshilfeminister';
-		case 'Bundesminister für besondere Aufgaben und Chef des Bundeskanzleramts':
-			return 'Chef des Bundeskanzleramts';
-		default:
-			return secondPart;
-		}
-	};
+			// Ministries
+			case 'Bundesminister des Auswärtigen':
+				return 'Außenminister';
+			case 'Bundesminister für Wirtschaft und Energie':
+				return 'Bundeswirtschaftsminister';
+			case 'Bundesministerin für Justiz und Verbraucherschutz':
+				return 'Bundesjustizministerin';
+			case 'Bundesminister für Arbeit und Soziales':
+				return 'Bundesarbeitsminister';
+			case 'Bundesminister für Gesundheit':
+				return 'Bundesgesundheitsminister';
+			case 'Bundesminister für Verkehr und digitale Infrastruktur':
+				return 'Bundesverkehrsminister';
+			case 'Bundesministerin für Umwelt, Naturschutz und nukleare Sicherheit':
+				return 'Bundesumweltministerin';
+			case 'Bundesministerin für Bildung und Forschung':
+				return 'Bundesbildungsministerin';
+			case 'Bundesminister für wirtschaftliche Zusammenarbeit und Entwicklung':
+				return 'Bundesentwicklungshilfeminister';
+			case 'Bundesminister für besondere Aufgaben und Chef des Bundeskanzleramts':
+				return 'Chef des Bundeskanzleramts';
 
-	const stateHandler = (secondPart: string): string => {
-		switch (secondPart) {
-		case 'Ministerpräsident NRW':
-			return 'Ministerpräsident';
-		case 'Regierender Bürgermeister von Berlin':
-			return 'Bürgermeister von Berlin';
-		default:
-			return secondPart;
+			// Misc
+			case 'Ministerpräsident NRW':
+				return 'Ministerpräsident';
+			case 'Regierender Bürgermeister von Berlin':
+				return 'Bürgermeister von Berlin';
+			default:
+				return input;
 		}
-	};
-
-	const inputHandler = (part: string): string => {
-		return stringHandler(ministryHandler(stateHandler(part)));
 	};
 
 	if (firstPartIndex !== -1) {
