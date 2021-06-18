4cc0105d492db358c09d4a1e36c71f4bb64bfb06
Johann Hemmann <johann.hemmann@code.berlin>
2021-06-18 15:37:51 +0200

    Adapt `Position` to backend

--- src/pages/Position.tsx
+++ src/pages/Position.tsx
@@ -5,7 +5,8 @@ import PoliticianProfile from '../components/PoliticianProfile/PoliticianProfile
 import PositionCards from '../components/PositionCards/PositionCards';
 import { useParams } from 'react-router';
 import { useQuery } from 'react-query';
-import fetch from '../functions/queries';
+import axios from 'axios';
+
 const reason =
 	'"Das ist nur ein Platzhalter. Für die Bundestagswahl haben Kandidat:innen  hier die Möglichkeit ihre Position zu begründen"';
 const examplePositionData = [
@@ -69,7 +70,7 @@ const Position: React.FC = () => {
 	const { id } = useParams<{ id: string }>();
 	const { data, status, error } = useQuery(
 		`politicianProfile-${id}`,
-		() => fetch(`politicians/${id}?related_data=show_information`),
+		async () => await (await axios.get(`/politicians/${id}`)).data,
 		{
 			staleTime: 60 * 10000000, // 10000 minute = around 1 week
 			cacheTime: 60 * 10000000,
@@ -86,10 +87,10 @@ const Position: React.FC = () => {
 	}
 	return (
 		<IonPage className="Profile-Mobile">
-			{data !== undefined ? <PoliticianProfile candidate={data.data} /> : null}
+			{data !== undefined ? <PoliticianProfile candidate={data} /> : null}
 			<Tabs />
 			<IonContent>
-				<PositionCards candidateId={data.data.id} positions={examplePositionData} />
+				<PositionCards candidateId={data.id} positions={examplePositionData} />
 			</IonContent>
 		</IonPage>
 	);
