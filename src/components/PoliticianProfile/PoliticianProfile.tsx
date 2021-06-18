d03e564c23c03f44d46d6b1633ef056d157d6178
Johann Hemmann <johann.hemmann@code.berlin>
2021-06-18 16:02:24 +0200

    Simplify `PoliticianProfile`

--- src/components/PoliticianProfile/PoliticianProfile.tsx
+++ src/components/PoliticianProfile/PoliticianProfile.tsx
@@ -10,31 +10,27 @@ interface ContainerProps {
 	candidate: Politician;
 }
 
-const PoliticianProfile: React.FC<ContainerProps> = ({ candidate }: ContainerProps) => {
-	occupationHandler(candidate.occupation);
-
-	return (
-		<div className="header">
-			<IonGrid>
-				<IonRow>
-					<IonCol className="profile-image">
-						<img className="politician-image" src={Placeholder} alt="placeholder" />
-					</IonCol>
-					<IonCol className="profile-details">
-						<div className="politician-name">
-							<IonCardTitle>{candidate.label}</IonCardTitle>
-						</div>
-						<div className="politican-details">
-							<PartyChip party={candidate.party.label} />
-							{candidate.occupation !== null
-								? occupationHandler(candidate.occupation)
-								: null}
-						</div>
-					</IonCol>
-				</IonRow>
-			</IonGrid>
-		</div>
-	);
-};
+const PoliticianProfile: React.FC<ContainerProps> = ({ candidate }: ContainerProps) => (
+	<div className="header">
+		<IonGrid>
+			<IonRow>
+				<IonCol className="profile-image">
+					<img className="politician-image" src={Placeholder} alt="placeholder" />
+				</IonCol>
+				<IonCol className="profile-details">
+					<div className="politician-name">
+						<IonCardTitle>{candidate.label}</IonCardTitle>
+					</div>
+					<div className="politican-details">
+						<PartyChip party={candidate.party.label} />
+						{candidate.occupation !== null
+							? occupationHandler(candidate.occupation)
+							: null}
+					</div>
+				</IonCol>
+			</IonRow>
+		</IonGrid>
+	</div>
+);
 
 export default PoliticianProfile;
