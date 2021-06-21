ea638cf6cda8c6516e20ae22a3f43605260fef8a
RichardKruemmel <richard.kruemmel@code.berlin>
2021-06-21 18:01:59 +0200

    Add funtion to return month and year of income

---
+++ src/functions/incomeMonthHandler.tsx
@@ -0,0 +1,9 @@
+export const incomeMonthHandler = (date: string): string => {
+	const yearIndex = date.indexOf('-');
+    const year = date.slice(0, yearIndex)
+    const newDate = date.slice(yearIndex+1)
+    const monthIndex = newDate.indexOf("-")
+    const month = newDate.slice(0, monthIndex)
+    return (month + "/" + year)
+
+};
