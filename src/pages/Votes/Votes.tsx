f1e26ba4d20efad9656615d3b8757041b34f6aea
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-06-22 14:19:29 +0200

    Remove unnecessary profile logic

--- src/pages/Votes/Votes.tsx
+++ src/pages/Votes/Votes.tsx
@@ -4,21 +4,13 @@ import './Votes.css';
 import LinkButton from '../../components/LinkButton';
 import { iconEnum } from '../../enums/icon.enum';
 import { useParams } from 'react-router';
-import { useQueries, useQuery } from 'react-query';
+import { useQueries } from 'react-query';
 import fetch from '../../functions/queries';
 import VoteCard from '../../components/VoteCard/VoteCard';
 
 const Votes: React.FC = () => {
 	const pollIds = [1584, 1604, 1639, 1758, 3602, 3936, 4088, 4098];
 	const { id } = useParams<{ id: string }>();
-	const { data, status, error } = useQuery(
-		`politicianProfile-${id}`,
-		() => fetch(`politicians/${id}?related_data=show_information`),
-		{
-			staleTime: 60 * 10000000, // 10000 minute = around 1 week
-			cacheTime: 60 * 10000000,
-		}
-	);
 	const polls = useQueries(
 		pollIds.map((pollId) => {
 			return {
@@ -30,45 +22,29 @@ const Votes: React.FC = () => {
 			};
 		})
 	);
-
-	switch (status) {
-	case 'loading':
-		// eslint-disable-next-line
-			return <iframe src="https://lottiefiles.com/iframe/58266-quad-cube-shifter-1"></iframe>;
-	case 'error':
-		return <p>Error: {error}</p>;
-	default:
-		/* This is returned when using this component */
-		return (
-			<IonPage className="votes-black-back">
-				<div className="votes-header-container">
-					<LinkButton
-						linkTo={`/politician/${id}/profile`}
-						icon={iconEnum.CHEVRON_BACK}
-					/>
+	return (
+		<IonPage className="votes-black-back">
+			<div className="votes-header-container">
+				<LinkButton linkTo={`/politician/${id}/profile`} icon={iconEnum.CHEVRON_BACK} />
+			</div>
+			<IonContent>
+				<div className="votes-black-back">
+					{
+						// eslint-disable-next-line
+						polls.map((poll: any, index: number): JSX.Element | undefined => {
+							if (poll.data?.data !== undefined) {
+								return (
+									<li key={index}>
+										<VoteCard vote={poll.data?.data} name={poll.data?.label} />
+									</li>
+								);
+							}
+						})
+					}
 				</div>
-				<IonContent>
-					<div className="votes-black-back">
-						{
-							// eslint-disable-next-line
-								polls.map((poll: any, index: number): JSX.Element | undefined => {
-								if (poll.data?.data !== undefined) {
-									return (
-										<li key={index}>
-											<VoteCard
-												vote={poll.data?.data}
-												name={poll.data?.label}
-											/>
-										</li>
-									);
-								}
-							})
-						}
-					</div>
-				</IonContent>
-			</IonPage>
-		);
-	}
+			</IonContent>
+		</IonPage>
+	);
 };
 
 export default Votes;
