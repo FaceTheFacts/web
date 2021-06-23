d721a2ce41cc913440177d87e0404f33edc89c1d
RichardKruemmel <richard.kruemmel@code.berlin>
2021-06-23 17:56:53 +0200

    Fix error when date of income is null

--- src/functions/incomeMonthHandler.tsx
+++ src/functions/incomeMonthHandler.tsx
@@ -1,8 +1,10 @@
 export const incomeMonthHandler = (date: string): string => {
-	const yearIndex = date.indexOf('-');
-	const year = date.slice(0, yearIndex);
-	const newDate = date.slice(yearIndex + 1);
-	const monthIndex = newDate.indexOf('-');
-	const month = newDate.slice(0, monthIndex);
-	return month + '/' + year;
+	if (date !== null) {
+		const yearIndex = date.indexOf('-');
+		const year = date.slice(0, yearIndex);
+		const newDate = date.slice(yearIndex + 1);
+		const monthIndex = newDate.indexOf('-');
+		const month = newDate.slice(0, monthIndex);
+		return month + '/' + year;}
+	else {return ''}
 };
