9966251d4554e5f092a8cd561d2775d41864df82
RichardKruemmel <richard.kruemmel@code.berlin>
2021-06-23 17:56:34 +0200

    Add empty function to component

--- src/pages/Votes/Votes.tsx
+++ src/pages/Votes/Votes.tsx
@@ -35,7 +35,7 @@ const Votes: React.FC = () => {
 							if (poll.data?.data !== undefined) {
 								return (
 									<div className="votes-vote-card" key={`poll-${index}`}>
-										<VoteCard vote={poll.data?.data} name={poll.data?.label} />
+										<VoteCard vote={poll.data?.data} name={poll.data?.label} setArrow={(): null => null}/>
 									</div>
 								);
 							}
