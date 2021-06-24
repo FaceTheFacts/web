6a93f049e3d87b0c1a4f01e014cd9c6055fd6986
Johann Hemmann <johann.hemmann@code.berlin>
2021-06-24 12:45:16 +0200

    Revert "Simplify `PoliticianProfile`"
    This reverts commit 4f7f9aa6808c5149e9886b203ffffd3d53ae3d64.

--- src/components/PoliticianProfile/PoliticianProfile.tsx
+++ src/components/PoliticianProfile/PoliticianProfile.tsx
@@ -10,35 +10,39 @@ interface ContainerProps {
 	candidate: Politician;
 }
 
-const PoliticianProfile: React.FC<ContainerProps> = ({ candidate }: ContainerProps) => (
-	<div className="header">
-		<IonGrid>
-			<IonRow>
-				<IonCol className="profile-image">
-					<img className="politician-image" src={Placeholder} alt="placeholder" />
-				</IonCol>
-				<IonCol className="profile-details">
-					<div className="politician-name">
-						<IonCardTitle>{candidate.label}</IonCardTitle>
-					</div>
-					<div className="politician-details">
-						<PartyChip party={candidate.party.label} />
-						{candidate.occupation !== null
-							? occupationHandler(candidate.occupation).map(
-								(occupation: string, index: number) => {
-									return (
-										<IonChip key={index} className="politician-detail">
-											{occupation}
-										</IonChip>
-									);
-								}
-							)
-							: null}
-					</div>
-				</IonCol>
-			</IonRow>
-		</IonGrid>
-	</div>
-);
+const PoliticianProfile: React.FC<ContainerProps> = ({ candidate }: ContainerProps) => {
+	occupationHandler(candidate.occupation);
+
+	return (
+		<div className="header">
+			<IonGrid>
+				<IonRow>
+					<IonCol className="profile-image">
+						<img className="politician-image" src={Placeholder} alt="placeholder" />
+					</IonCol>
+					<IonCol className="profile-details">
+						<div className="politician-name">
+							<IonCardTitle>{candidate.label}</IonCardTitle>
+						</div>
+						<div className="politician-details">
+							<PartyChip party={candidate.party.label} />
+							{candidate.occupation !== null
+								? occupationHandler(candidate.occupation).map(
+									(occupation: string, index: number) => {
+										return (
+											<IonChip key={index} className="politician-detail">
+												{occupation}
+											</IonChip>
+										);
+									}
+								)
+								: null}
+						</div>
+					</IonCol>
+				</IonRow>
+			</IonGrid>
+		</div>
+	);
+};
 
 export default PoliticianProfile;
