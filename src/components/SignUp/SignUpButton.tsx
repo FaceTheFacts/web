cf78ba1d985d6576786f943c9526a49104374874
RichardKruemmel <richard.kruemmel@code.berlin>
2021-06-30 12:32:22 +0200

    Delete SignupButton

--- src/components/SignUp/SignUpButton.tsx
+++ src/components/SignUp/SignUpButton.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import button from '../../assets/images/long-arrow-right-light.svg'
+
 import './SignUpButton.css';
 
 
 const SignUpButton: React.FC = () => (
 		<div className="sign-up-button">
-			<img src={button} alt="submit-button" />
+			
 		</div>
 );
 
