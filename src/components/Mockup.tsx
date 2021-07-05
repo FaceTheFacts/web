2f2b763ad1b6bc3d02db398617ec15e1f9237515
RichardKruemmel <richard.kruemmel@code.berlin>
2021-07-05 14:03:59 +0200

    Update new MockUp

--- src/components/Mockup.tsx
+++ src/components/Mockup.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import faceTheFactsMockUp from '../assets/images/mockup.png';
+import faceTheFactsMockUp from '../assets/images/Mockup.png';
 const Mockup: React.FC = () => (
 	<div>
 		<img src={faceTheFactsMockUp} alt="logo" />
