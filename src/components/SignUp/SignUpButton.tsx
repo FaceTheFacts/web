6f72fdf10e4cd6ebf4624ad07216af6ff9eb26c3
RichardKruemmel <richard.kruemmel@code.berlin>
2021-07-05 14:03:33 +0200

    Implement SignUp Button as arrow

--- src/components/SignUp/SignUpButton.tsx
+++ src/components/SignUp/SignUpButton.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import longArrow from '../../assets/images/long-arrow-alt-right-regular.svg'
 import './SignUpButton.css';
 
 
 const SignUpButton: React.FC = () => (
 		<div className="sign-up-button">
-			
+			<img src={longArrow} alt="Submit"></img>
 		</div>
 );
 
