caf1175544da7b6dbeab693309f1ae740ef16767
RichardKruemmel <richard.kruemmel@code.berlin>
2021-07-05 14:06:22 +0200

    Cleaning embedded form up

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -9,81 +9,43 @@ const SignUp: React.FC = () => {
 	return (
 		<div className="signup">
 			<div className="signup-text">
-				Als Erste:r erfahren, sobald
-				<br className="signup-break" /> Face The Facts verfügbar ist
+				Bleib auf dem Laufenden:
 			</div>
-			<div>
-				<div id="mc_embed_signup">
+			<div className="signup-form">
 					<form
 						action="https://app.us6.list-manage.com/subscribe/post?u=7d218321c3d9896cf653785d4&amp;id=6f8827b5cc"
 						method="post"
-						id="mc-embedded-subscribe-form"
 						name="mc-embedded-subscribe-form"
 						className="validate"
 						target="_blank"
 						noValidate
 					>
-						<div id="mc_embed_signup_scroll">
-							<h2>Subscribe</h2>
-							<div className="indicates-required">
-								<span className="asterisk">*</span> indicates required
-							</div>
-							<div className="mc-field-group">
-								<label htmlFor="mce-EMAIL">
-									Email Address <span className="asterisk">*</span>
-								</label>
+							<div className="email-field">
 								<input
 									type="email"
 									name="EMAIL"
 									className="required email"
-									id="mce-EMAIL"
+									id="email"
+                                    placeholder="Deine Mail-Adresse"
 								/>
+								<button
+									type="submit"
+									name="subscribe"
+									className="signup-button"
+                                ><SignUpButton /> </button>
 							</div>
-							<div
-								id="mergeRow-gdpr"
-								className="mergeRow gdpr-mergeRow content__gdprBlock mc-field-group"
-							>
 								<div className="content__gdpr">
-									<p>
-										Please select all the ways you would like to hear from Face
-										the Facts:
-									</p>
-									<fieldset
-										className="mc_fieldset gdprRequired mc-field-group"
-										name="interestgroup_field"
-									>
 										<label className="checkbox subfield" htmlFor="gdpr_67758">
 											<input
 												type="checkbox"
 												id="gdpr_67758"
 												name="gdpr[67758]"
 												value="Y"
-												className="av-checkbox gdpr"
+												className="checkbox-gdpr"
 											/>
-											<span>Email</span>
+											<span>Ich bin einverstanden mit der <a className="signup-link" href="/privacy">Datenschutzerklärung</a></span>
 										</label>
-									</fieldset>
-									<p>
-										You can unsubscribe at any time by clicking the link in the
-										footer of our emails. For information about our privacy
-										practices, please visit our website.
-									</p>
 								</div>
-								<div className="content__gdprLegal">
-									<p>
-										We use Mailchimp as our marketing platform. By clicking
-										below to subscribe, you acknowledge that your information
-										will be transferred to Mailchimp for processing.
-										<a href="https://mailchimp.com/legal/" target="_blank">
-											Learn more about Mailchimp&#39;s privacy practices here.
-										</a>
-									</p>
-								</div>
-							</div>
-							<div id="mce-responses" className="clear">
-								<div className="response" id="mce-error-response"></div>
-								<div className="response" id="mce-success-response"></div>
-							</div>
 							<div className="hidden-form" aria-hidden={true}>
 								<input
 									type="text"
@@ -91,18 +53,7 @@ const SignUp: React.FC = () => {
 									tabIndex={-1}
 								/>
 							</div>
-							<div className="clear">
-								<input
-									type="submit"
-									value="Subscribe"
-									name="subscribe"
-									id="mc-embedded-subscribe"
-									className="button"
-								/>
-							</div>
-						</div>
 					</form>
-				</div>
 			</div>
 		</div>
 	);
