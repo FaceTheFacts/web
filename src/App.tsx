7a7325c94c6e1e259e1a313a265a745480a907ee
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-07-06 16:04:21 +0200

    Delete an unnecessary CSS

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,6 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
-import './pages/mobile-view.css';
 import Position from './pages/Position';
 import Votes from './pages/Votes/Votes';
 
