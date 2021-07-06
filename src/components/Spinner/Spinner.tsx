b6cac276bea46d49e93c847309a80d01597a8e69
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-07-06 16:37:01 +0200

    Run prettier

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -2,6 +2,6 @@ import * as React from 'react';
 
 import './Spinner.css';
 
-const Spinner: React.FC = () => <div className="Spinner"/>;
+const Spinner: React.FC = () => <div className="Spinner" />;
 
 export default Spinner;
