f94f4395a519b5ce70ce403f9a73d2422e2d0b2d
RichardKruemmel <richard.kruemmel@code.berlin>
2021-07-09 16:03:48 +0200

    Integrate Politician Profile route

--- src/functions/queries.tsx
+++ src/functions/queries.tsx
@@ -5,3 +5,10 @@ export default async function fetch(url: string) {
 	const res: AxiosResponse = await axios.get(BASE_URL + url);
 	return await res.data;
 }
+
+// eslint-disable-next-line
+export async function newfetch(url: string) {
+	const BASE_URL = 'https://backend-aoa59.ondigitalocean.app/';
+	const res = await axios(BASE_URL + url);
+	return res.data;
+}
