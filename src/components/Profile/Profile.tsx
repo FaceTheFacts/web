1e8c55cc35aa2bf29432db1706df070bdb1fe57e
RichardKruemmel <richard.kruemmel@code.berlin>
2021-07-30 18:21:29 +0200

    Implement logic to differentiate between Profile and ElectionChances

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -11,6 +11,7 @@ import './Profile.css';
 import { iconEnum } from '../../enums/icon.enum';
 import fetch, { newfetch } from '../../functions/queries';
 import NoDataCard from '../NoDataCard/NoDataCard';
+import SegmentButtons from '../SegmentButtons';
 
 interface ProfileProps {
 	candidate: Politician;
@@ -20,6 +21,76 @@ interface ProfileProps {
 const Profile: React.FC<ProfileProps> = ({ candidate, profileId }: ProfileProps) => {
 	const pollIds = [1584, 1604, 1639, 1758, 3602, 3936, 4088, 4098];
 	const [showArrow, setArrow] = React.useState(true);
+	const [tab, setTab] = React.useState('0');
+	
+	const cvData= {
+		"Biography": {
+		"Raw": "2011 Abitur am Greifen-Gymnasium Ueckermünde. 2012/2017 Studium der Rechtswissenschaften an der Ernst-Moritz-Arndt Universität Greifswald (Studienabschluss mit Prädikat), Stipendiat der Konrad-Adenauer-Stiftung; als Nebenbeschäftigung u.a. Mitarbeiter verschiedener Abgeordneter des Deutschen Bundestages und des Landtages Mecklenburg-Vorpommern. 2017 wissenschaftlicher Mitarbeiter an der Ernst-Moritz-Arndt-Universität Greifswald und Mitarbeiter einer Wirtschaftskanzlei in Berlin. Seit 2017 Doktorand. Seit 2008 Mitglied der CDU und der Jungen Union, 2012/2018 Kreisvorsitzender der Jungen Union Vorpommern-Greifswald, seit 2017 Vorsitzender des CDU-Stadtverbandes Ueckermünde, seit 2018 Bundesschatzmeister der Jungen Union Deutschlands, seit 2019 Mitglied des Kreistages Vorpommern-Greifswald. Mitglied des Bundestages seit Oktober 2017 (Mitglied im Innenausschuss und im Europaausschuss, stellv. Mitglied im Rechtsausschuss). Seit 2018 Vorstandsmitglied im Parlamentskreis Mittelstand der CDU/CSU-Bundestagsfraktion. Mitgliedschaften (Auswahl): Atlantik-Brücke, Diözesanrat der Katholiken im Erzbistum Berlin, Landesjagdverband Mecklenburg-Vorpommern e.V., AMEOS-Regionalbeirat Vorpommern, Verwaltungsrat Sparkasse Uecker-Randow, Europa-Union, BdV, OMV (Bundesvorstand). ",
+		"ShortDescription": "Geboren am 10. November 1992 in Ueckermünde; römisch-katholisch.",
+		"Steps": [
+		{
+			"Raw": "2011 Abitur am Greifen-Gymnasium Ueckermünde",
+			"Label": "Abitur am Greifen-Gymnasium Ueckermünde",
+			"Date": "2011"
+		},
+		{
+			"Raw": "2012/2017 Studium der Rechtswissenschaften an der Ernst-Moritz-Arndt Universität Greifswald (Studienabschluss mit Prädikat)",
+			"Label": "Studium der Rechtswissenschaften an der Ernst-Moritz-Arndt Universität Greifswald (Studienabschluss mit Prädikat)",
+			"Date": "2012/2017"
+		},
+		{
+			"Raw": "2017 wissenschaftlicher Mitarbeiter an der Ernst-Moritz-Arndt-Universität Greifswald und Mitarbeiter einer Wirtschaftskanzlei in Berlin",
+			"Label": "wissenschaftlicher Mitarbeiter an der Ernst-Moritz-Arndt-Universität Greifswald und Mitarbeiter einer Wirtschaftskanzlei in Berlin",
+			"Date": "2017"
+		},
+		{
+			"Raw": "Seit 2017 Doktorand",
+			"Label": "Doktorand",
+			"Date": "seit 2017"
+		},
+		{
+			"Raw": "Seit 2008 Mitglied der CDU und der Jungen Union",
+			"Label": "Mitglied der CDU und der Jungen Union",
+			"Date": "seit 2008"
+		},
+		{
+			"Raw": "2012/2018 Kreisvorsitzender der Jungen Union Vorpommern-Greifswald",
+			"Label": "Kreisvorsitzender der Jungen Union Vorpommern-Greifswald",
+			"Date": "2012/2018"
+		},
+		{
+			"Raw": "seit 2017 Vorsitzender des CDU-Stadtverbandes Ueckermünde",
+			"Label": "Vorsitzender des CDU-Stadtverbandes Ueckermünde",
+			"Date": "seit 2017"
+		},
+		{
+			"Raw": "seit 2018 Bundesschatzmeister der Jungen Union Deutschlands",
+			"Label": "Bundesschatzmeister der Jungen Union Deutschlands",
+			"Date": "seit 2018"
+		},
+		{
+			"Raw": "seit 2019 Mitglied des Kreistages Vorpommern-Greifswald",
+			"Label": "Mitglied des Kreistages Vorpommern-Greifswald",
+			"Date": "seit 2019"
+		},
+		{
+			"Raw": "Mitglied des Bundestages seit Oktober 2017 (Mitglied im Innenausschuss und im Europaausschuss",
+			"Label": "Mitglied des Bundestages  (Mitglied im Innenausschuss und im Europaausschuss",
+			"Date": "seit Oktober 2017"
+		},
+		{
+			"Raw": "Seit 2018 Vorstandsmitglied im Parlamentskreis Mittelstand der CDU/CSU-Bundestagsfraktion",
+			"Label": "Vorstandsmitglied im Parlamentskreis Mittelstand der CDU/CSU-Bundestagsfraktion",
+			"Date": "seit 2018"
+		}]
+		}
+	}
+
+	const webLinksData = [
+		"http://www.philipp-amthor.de/",
+		"https://www.facebook.com/amthor.philipp/"
+	]
+
 	const { data, status } = useQuery(
 		`politicalFocus-${candidate.label}`,
 		() =>
@@ -60,7 +131,8 @@ const Profile: React.FC<ProfileProps> = ({ candidate, profileId }: ProfileProps)
 	}
 	return (
 		<React.Fragment>
-			<div className="profile-black-back">
+			<SegmentButtons tab={tab} setTab={setTab} type={1}/>
+			{tab==='0' ? (<div className="profile-black-back">
 				{data.data.length !== 0 ? (
 					<div>
 						<TitleHeader title="Politische Schwerpunkte" />
@@ -109,7 +181,13 @@ const Profile: React.FC<ProfileProps> = ({ candidate, profileId }: ProfileProps)
 				) : (
 					<NoDataCard type="sideJob" />
 				)}
-			</div>
+			</div>)
+			: tab === '1' ? (
+				<div>Lebenslauf</div>
+
+			) : (
+				<div>WebLinks</div>
+			)}
 		</React.Fragment>
 	);
 };
