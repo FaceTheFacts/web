3ec19169a231699b8dc93d4da2a6fcee0d61c738
RichardKruemmel <richard.kruemmel@code.berlin>
2021-07-30 19:11:07 +0200

    Create WebLinkComponent and Card

---
+++ src/components/WebLinkComponent/WebLinkComponent.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import WebLinkComponentCard from './WebLinkComponentCard';
+
+interface WebLinkComponentProps {
+	data: Array<string>;
+}
+
+const WebLinkComponent: React.FC<WebLinkComponentProps> = ({ data }: WebLinkComponentProps) => {
+    return <React.Fragment>
+        {data.map((Link: any, index: number) => (<WebLinkComponentCard link={Link} key={index} />)
+        )}
+    </React.Fragment>;
+};
+
+export default WebLinkComponent;
