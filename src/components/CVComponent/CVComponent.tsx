ace356c96164098ce348e8be10cbc61c516700d1
RichardKruemmel <richard.kruemmel@code.berlin>
2021-07-30 19:10:35 +0200

    Create CVComponent and Cards

---
+++ src/components/CVComponent/CVComponent.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './CVComponent.css';
+import { CVData } from '../../Types';
+import CVComponentCard from './CVComponentCard';
+
+interface CVComponentProps {
+	data: CVData;
+}
+
+const CVComponent: React.FC<CVComponentProps> = ({ data }: CVComponentProps) => {
+    return <React.Fragment>
+        {data.Biography.Steps.map((Step: any, index: number) => (<CVComponentCard step={Step} key={index} />)
+        )}
+    </React.Fragment>;
+};
+
+export default CVComponent;
