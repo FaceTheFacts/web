8447f932998886606add87c73cc8964aadbaf4ce
Takahiro Mitsui <78789212+takahiromitsui@users.noreply.github.com>
2021-07-30 14:11:31 +0200

    Revert "Rename and add Spinner"
    This reverts commit 3b3c67f009416d1ab4766c97f2be6a6d200514fe.

--- src/pages/Votes/Votes.tsx
+++ src/pages/Votes/Votes.tsx
@@ -10,18 +10,17 @@ import { IonContent, IonImg, IonItem, IonLabel, IonPage } from '@ionic/react';
 import React, { useState } from 'react';
 import { useQueries, useQuery } from 'react-query';
 import { useParams } from 'react-router';
-import Spinner from '../../components/Spinner/Spinner';
 
 const Votes: React.FC = () => {
 	const [filter, setFilter] = useState(false);
 	const pollIds = [1584, 1604, 1639, 1758, 3602, 3936, 4088, 4098];
 	const { id } = useParams<{ id: string }>();
-	const { data, status } = useQuery(`politician-${id}`, () => newfetch(`politicians/${id}`), {
+	const { data } = useQuery(`politician-${id}`, () => newfetch(`politicians/${id}`), {
 		staleTime: 60 * 10000000, // 10000 minute = around 1 week
 		cacheTime: 60 * 10000000,
 	});
 
-	const polls = useQueries(
+	const testPolls = useQueries(
 		pollIds.map((pollId) => {
 			return {
 				queryKey: ['profile-page-polls', pollId, data?.label],
@@ -32,14 +31,6 @@ const Votes: React.FC = () => {
 			};
 		})
 	);
-	if (status === 'loading' || polls[0].status === 'loading') {
-		return <Spinner/>;
-	}
-
-	if (status === 'error' || polls[0].status === 'error') {
-		return <Spinner/>;
-	}
-
 	return (
 		<IonPage className="votes-black-back">
 			<div className="votes-header-container">
@@ -72,7 +63,7 @@ const Votes: React.FC = () => {
 				<div className="votes-black-back">
 					{
 						// eslint-disable-next-line
-						polls.map((poll: any, index: number): JSX.Element | undefined => {
+						testPolls.map((poll: any, index: number): JSX.Element | undefined => {
 							return (
 								<div className="votes-vote-card" key={`poll-${index}`}>
 									<VoteCard
