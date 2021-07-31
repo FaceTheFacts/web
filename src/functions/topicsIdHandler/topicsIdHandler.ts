f3d7ccab69a81dd1ec455ef3abdea00b34ebea6c
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-07-31 16:12:20 +0200

    Change into array

--- src/functions/topicsIdHandler/topicsIdHandler.ts
+++ src/functions/topicsIdHandler/topicsIdHandler.ts
@@ -1,54 +1,54 @@
-export const topicsIdHandler = (topic: string): number|Array<number> => {
+export const topicsIdHandler = (topic: string): Array<number> => {
   switch(topic){
     case 'Digitale Agenda':
-      return 39
+      return [39, 1, 45]
     case 'Arbeit und Soziales':
-      return 14
+      return [14, 2, 25]
     case 'Bau und Wohnen':
-      return 18
+      return [18]
     case 'Bildung und Forschung':
-      return 3
+      return [3, 49, 15, 51]
     case 'Europäische Union':
-      return 4
+      return [4]
     case 'Ernährung und Landwirtschaft':
-      return 5
+      return [5]
     case 'Gesundheit':
-      return 28
+      return [28]
     case 'Inneres':
-      return 23
+      return [23, 38]
     case 'Kultur und Medien':
-      return 7
+      return [7, 45]
     case 'Menschenrechte':
-      return 25
+      return [25, 11, 43]
     case 'Recht und Verbraucherschutz':
-      return 44
+      return [44, 8]
     case 'Tourismus':
-      return 47
+      return [47, 12]
     case 	'Umwelt':
-      return 29
+      return [29, 9, 48, 50]
     case 'Infrastruktur':
-      return 40
+      return [40, 1, 10]
     case 'Wirtschaft und Energie':
-      return 20
+      return [20, 11, 19]
     case 	'Entwicklung':
-      return 17
+      return [17]
     case 'Auswärtiges':
-      return 21
+      return [21]
     case 'Finanzen':
-      return 41
+      return [41, 22]
     case 'Haushalt':
-      return 42
+      return [42]
     case 'Familie':
-      return 34
+      return [34, 16, 35, 36, 37]
     case 'Sport':
-      return 46
+      return [46, 12]
     case 'Verteidigung':
-      return 13
+      return [13, 21]
     case 'Parlamentsangelegenheiten':
-      return 30
+      return [30, 53, 27]
     case 'Nachrichtendienste':
       return [4197, 4083]
     default:
-      return 0 
+      return [0] 
   }
 }
