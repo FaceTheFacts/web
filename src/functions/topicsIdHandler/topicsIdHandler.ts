273ec89bccf5a3f0312ad6cc59ff0334a66f1a3a
RichardKruemmel <richard.kruemmel@code.berlin>
2021-08-02 12:14:30 +0200

    Edit function to return array

--- src/functions/topicsIdHandler/topicsIdHandler.ts
+++ src/functions/topicsIdHandler/topicsIdHandler.ts
@@ -1,54 +1,83 @@
-export const topicsIdHandler = (topic: string): Array<number> => {
-	switch (topic) {
-	case 'Digitale Agenda':
-		return [39, 1, 45];
-	case 'Arbeit und Soziales':
-		return [14, 2, 25];
-	case 'Bau und Wohnen':
-		return [18];
-	case 'Bildung und Forschung':
-		return [3, 49, 15, 51];
-	case 'Europäische Union':
-		return [4];
-	case 'Ernährung und Landwirtschaft':
-		return [5];
-	case 'Gesundheit':
-		return [28];
-	case 'Inneres':
-		return [23, 38];
-	case 'Kultur und Medien':
-		return [7, 45];
-	case 'Menschenrechte':
-		return [25, 11, 43];
-	case 'Recht und Verbraucherschutz':
-		return [44, 8];
-	case 'Tourismus':
-		return [47, 12];
-	case 'Umwelt':
-		return [29, 9, 48, 50];
-	case 'Infrastruktur':
-		return [40, 1, 10];
-	case 'Wirtschaft und Energie':
-		return [20, 11, 19];
-	case 'Entwicklung':
-		return [17];
-	case 'Auswärtiges':
-		return [21];
-	case 'Finanzen':
-		return [41, 22];
-	case 'Haushalt':
-		return [42];
-	case 'Familie':
-		return [34, 16, 35, 36, 37];
-	case 'Sport':
-		return [46, 12];
-	case 'Verteidigung':
-		return [13, 21];
-	case 'Parlamentsangelegenheiten':
-		return [30, 53, 27];
-	case 'Nachrichtendienste':
-		return [4197, 4083];
-	default:
-		return [0];
+
+export const topicsIdHandler = (topics: string[]): Array<number> => {
+	const topicArray: Array<number> = []
+	topics.map((topic: string): void => {
+		switch (topic) {
+		case 'Digitale Agenda':
+			topicArray.push(39, 1, 45);
+			break;
+		case 'Arbeit und Soziales':
+			topicArray.push(14, 2, 25);
+			break;
+		case 'Bau und Wohnen':
+			topicArray.push(18);
+			break;
+		case 'Bildung und Forschung':
+			topicArray.push(3, 49, 15, 51);
+			break;
+		case 'Europäische Union':
+			topicArray.push(4);
+			break;
+		case 'Ernährung und Landwirtschaft':
+			topicArray.push(5);
+			break;
+		case 'Gesundheit':
+			topicArray.push(28);
+			break;
+		case 'Inneres':
+			topicArray.push(23, 38);
+			break;
+		case 'Kultur und Medien':
+			topicArray.push(7, 45);
+			break;
+		case 'Menschenrechte':
+			topicArray.push(25, 11, 43);
+			break;
+		case 'Recht und Verbraucherschutz':
+			topicArray.push(44, 8);
+			break;
+		case 'Tourismus':
+			topicArray.push(47, 12);
+			break;
+		case 'Umwelt':
+			topicArray.push(29, 9, 48, 50);
+			break;
+		case 'Infrastruktur':
+			topicArray.push(40, 1, 10);
+			break;
+		case 'Wirtschaft und Energie':
+			topicArray.push(20, 11, 19);
+			break;
+		case 'Entwicklung':
+			topicArray.push(17);
+			break;
+		case 'Auswärtiges':
+			topicArray.push(21);
+			break;
+		case 'Finanzen':
+			topicArray.push(41, 22);
+			break;
+		case 'Haushalt':
+			topicArray.push(42);
+			break;
+		case 'Familie':
+			topicArray.push(34, 16, 35, 36, 37);
+			break;
+		case 'Sport':
+			topicArray.push(46, 12);
+			break;
+		case 'Verteidigung':
+			topicArray.push(13, 21);
+			break;
+		case 'Parlamentsangelegenheiten':
+			topicArray.push(30, 53, 27);
+			break;
+		case 'Nachrichtendienste':
+			topicArray.push(4197, 4083);
+			break;
+		default:
+			topicArray.push(0);
 	}
+});
+	return topicArray
 };
