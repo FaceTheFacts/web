21c14597bb307a22426d96ed5aee8871f3598800
RichardKruemmel <richard.kruemmel@code.berlin>
2021-08-02 15:45:08 +0200

    Implement weblinks and weblinkhandler

--- src/components/WebLinkComponent/WebLinkComponentCard.tsx
+++ src/components/WebLinkComponent/WebLinkComponentCard.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import { IonCard, IonCardHeader, IonCardTitle } from '@ionic/react';
+import { webLinkHandler } from '../../functions/webLinkHandler';
 
 interface WebLinkComponentProps {
 	link: string;
 }
 
 const WebLinkComponentCard: React.FC<WebLinkComponentProps> = ({ link }: WebLinkComponentProps) => {
-    return (
-        <IonCard className="sideJob-layout" href={link}>
-			<IonCardHeader>
-				<IonCardTitle className="sideJob-major" data-testid="profile-sidejob-position">
-					{link}
-				</IonCardTitle>
-			</IonCardHeader>
-		</IonCard>
-    )
+    return webLinkHandler(link)
 };
 export default WebLinkComponentCard;
