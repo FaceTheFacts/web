18ee9305695916926e6e474286090e101d7f6dbf
RichardKruemmel <richard.kruemmel@code.berlin>
2021-08-02 13:12:19 +0200

    Fix Bug #199

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -70,7 +70,7 @@ const Profile: React.FC<ProfileProps> = ({ candidate, profileId }: ProfileProps)
 					</div>
 				) : null}
 				{polls[0].data !== undefined ? (
-					<div>
+					<div className="profile-linkbutton">
 						<TitleHeader title="Wichtigste Abstimmungen">
 							<div className={showArrow ? 'show-arrow' : 'hide-arrow'}>
 								<LinkButton
