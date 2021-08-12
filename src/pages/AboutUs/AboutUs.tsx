4294235f3bb4e094efc94108f103aa85d7b72bc3
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-08-12 22:39:46 +0200

    Add Team photos

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -1,6 +1,11 @@
 import HomeHeaderFooter from '../../hoc/HomeHeaderFooter/HomeHeaderFooter';
 import './AboutUs.css';
 import React from 'react';
+import TeamMember from '../../components/TeamMember/TeamMember';
+import Victor from '../../assets/images/victor.svg';
+import Olli from '../../assets/images/olli.svg';
+import Richard from '../../assets/images/richard.svg';
+import Taka from '../../assets/images/taka.svg';
 
 const AboutUs: React.FC = () => (
 	<HomeHeaderFooter>
@@ -18,6 +23,12 @@ const AboutUs: React.FC = () => (
 			</div>
 			<div className="about-us-border"></div>
 			<div className="about-us-team">Team</div>
+			<div>
+				<TeamMember image={Victor} name={'Victor Bellu'} role={'Produkt'}/>
+				<TeamMember image={Olli} name={'Oliver Köditz'} role={'Design'}/>
+				<TeamMember image={Richard} name={'Richard Krümmel'} role={'Software Development'}/>
+				<TeamMember image={Taka} name={'Takahiro Mitsui'} role={'Software Development'}/>
+			</div>
 		</div>
 	</HomeHeaderFooter>
 );
