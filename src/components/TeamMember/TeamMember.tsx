d9928ba5610493110c0020f3b5a1511d608d4608
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-08-12 22:20:35 +0200

    Create a component

---
+++ src/components/TeamMember/TeamMember.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './TeamMember.css';
+
+interface TeamMemberProps {
+  image: string;
+  name: string;
+  role: string;
+}
+
+const TeamMember: React.FC<TeamMemberProps> = (props: TeamMemberProps) => (
+  <div className='team-member-content'>
+    <img src={props.image} alt="team-member" />
+    <div className='team-member-title'>
+      <h2>{props.name}</h2>
+      <p>{props.role}</p>
+    </div>
+  </div>
+);
+
+export default TeamMember;
