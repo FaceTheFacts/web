1e605c0c1c5c6c0dd4582a9fc0e0756ca3ce28e1
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-08-13 10:38:48 +0200

    Add Contact route

---
+++ src/pages/Contact/Contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomeHeaderFooter from '../../hoc/HomeHeaderFooter/HomeHeaderFooter';
+
+const Contact: React.FC = () => (
+	<HomeHeaderFooter>
+		<div>
+
+		</div>
+	</HomeHeaderFooter>
+);
+
+export default Contact;
