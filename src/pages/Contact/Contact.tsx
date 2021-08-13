401ca90f0af07a9e171a2cf952cdc333e41f9c03
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-08-13 10:57:10 +0200

    Add styling for items

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -6,6 +6,17 @@ import './Contact.css';
 const Contact: React.FC = () => (
 	<HomeHeaderFooter>
 		<div className="contact-content">
+			<div className='contact-item'>
+				<div className='contact-item-title'>
+					Du hast einen Fehler entdeckt?
+				</div>
+				<div className='contact-item-address'>
+					Dann lass es uns wissen mit einer Mail an:
+				</div>
+				<div className='contact-item-address'>
+					daten@facethefacts.app
+				</div>
+			</div>
 
 		</div>
 	</HomeHeaderFooter>
