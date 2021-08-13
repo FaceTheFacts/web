a28955f86f7c81743d8d89382e6b4aa2635656e1
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-08-13 11:07:23 +0200

    Add borders and dummy contents

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -17,7 +17,30 @@ const Contact: React.FC = () => (
 					daten@facethefacts.app
 				</div>
 			</div>
-
+			<div className="contact-us-border"></div>
+			<div className='contact-item'>
+				<div className='contact-item-title'>
+					Du bist Kandidat:in und möchtest uns mehr Informationen über dich mitteilen?
+				</div>
+				<div className='contact-item-address'>
+					Dann lass es uns wissen mit einer Mail an:
+				</div>
+				<div className='contact-item-address'>
+					daten@facethefacts.app
+				</div>
+			</div>
+			<div className="contact-us-border"></div>
+			<div className='contact-item'>
+				<div className='contact-item-title'>
+					Du möchtest gerne Werbung auf Face The Facts schalten?
+				</div>
+				<div className='contact-item-address'>
+					Dann lass es uns wissen mit einer Mail an:
+				</div>
+				<div className='contact-item-address'>
+					daten@facethefacts.app
+				</div>
+			</div>
 		</div>
 	</HomeHeaderFooter>
 );
