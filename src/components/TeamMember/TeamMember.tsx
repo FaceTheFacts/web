96cd0843c48177ef1e06a04375efd58796716d0a
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-08-13 13:22:37 +0200

    Run prettier

--- src/components/TeamMember/TeamMember.tsx
+++ src/components/TeamMember/TeamMember.tsx
@@ -1,17 +1,16 @@
+import './TeamMember.css';
 import React from 'react';
 
-import './TeamMember.css';
-
 interface TeamMemberProps {
-  image: string;
-  name: string;
-  role: string;
+	image: string;
+	name: string;
+	role: string;
 }
 
 const TeamMember: React.FC<TeamMemberProps> = (props: TeamMemberProps) => (
-	<div className='team-member-content'>
+	<div className="team-member-content">
 		<img src={props.image} alt="team-member" />
-		<div className='team-member-title'>
+		<div className="team-member-title">
 			<h2>{props.name}</h2>
 			<p>{props.role}</p>
 		</div>
