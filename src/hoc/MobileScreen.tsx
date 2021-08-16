b8a691f55ab5866d38f7bb2c17c6f83b3f70102c
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-08-16 15:46:09 +0200

    Pass mockup data

--- src/hoc/MobileScreen.tsx
+++ src/hoc/MobileScreen.tsx
@@ -7,65 +7,19 @@ import Spinner from '../components/Spinner/Spinner';
 import PoliticianProfile from '../components/PoliticianProfile/PoliticianProfile';
 import Tabs from '../components/Tabs';
 import './MobileScreen.css';
+import {Partialdata} from '../json/TestJsonData';
 
 interface MobileScreenProps {
 	children?: JSX.Element;
 }
 
-type ElectoralDataResult = {
-	id: number;
-	label: string;
-	party: string;
-	result: number;
-}
-
-type CVSteps = {
-	Raw: string;
-	Label: string;
-	Date: string;
-}
-
-type Sidejobs = {
-	label: string;
-	income_level: string;
-	date: string;
-	organisation: string;
-	interval: string|undefined;
-}
-
-type Votes = {
-	poll_id: number;
-	vote:'yes'|'no'|'abstain'|'no_show';
-}
-
-type TotalData = {
-	[key:string]:{name: string;
-	party: string;
-	electoral_data: {
-		constituency: {
-			state: string|undefined;
-			id: number,
-			label: string,
-			result:ElectoralDataResult[];
-	  }
-  };
-	occupation: string[]|[];
-	cv: {
-		Raw: string;
-		ShortDescription: string;
-		Steps: CVSteps[];
-	}|[];
-	weblinks: string[]|[];
-	committees: string|undefined;
-	sidejobs:Sidejobs[]|undefined;
-	votes:Votes[]|undefined
-  }
-}
-
 const MobileScreen: React.FC<MobileScreenProps> = (props: MobileScreenProps) => {
 	const { id } = useParams<{ id: string }>();
-	let mainComponent: JSX.Element | undefined;
+	const mockupId = 177393;
+	const selectedData = Partialdata[mockupId]
 
+
+	let mainComponent: JSX.Element | undefined;
 	const { data, status, error } = useQuery(
 		`politician-${id}`,
 		() => newfetch(`politicians/${id}`),
@@ -98,7 +52,7 @@ const MobileScreen: React.FC<MobileScreenProps> = (props: MobileScreenProps) => 
 
 	return (
 		<IonPage className="MobileScreen">
-			{data !== undefined ? <PoliticianProfile candidate={data} image={image} /> : null}
+			{data !== undefined ? <PoliticianProfile candidate={selectedData} image={image} /> : null}
 			<Tabs id={id} />
 			<IonContent>{mainComponent}</IonContent>
 		</IonPage>
