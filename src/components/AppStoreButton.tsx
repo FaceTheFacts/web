24ff81530f291e4dbbceb4b2c102bef4c3ad9263
RichardKruemmel <richard.kruemmel@code.berlin>
2021-08-31 16:32:26 +0200

    Refactor component to serve for Apple + Android

--- src/components/AppStoreButton.tsx
+++ src/components/AppStoreButton.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-import AppStoreLogo from '../assets/images/app-store-logo.svg';
 import 'react-lazy-load-image-component/src/effects/blur.css';
-const Mockup: React.FC = () => (
+
+interface AppStoreButtonProps {
+	link: string;
+	source: string; 
+}
+
+const Mockup: React.FC<AppStoreButtonProps> = (props: AppStoreButtonProps) => (
 	<div>
-		<a href="https://apps.apple.com/de/app/face-the-facts/id1579513133?l=en">
-			<LazyLoadImage src={AppStoreLogo} alt="logo" effect="blur" />
+		<a href={props.link}>
+			<LazyLoadImage src={props.source} alt="logo" effect="blur" />
 		</a>
 	</div>
 );
