6530cad44ab1e734a834c764673c15c81e4e68fa
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-09-06 16:01:37 +0200

    Rename

--- src/components/Mockup.tsx
+++ src/components/Mockup.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-import faceTheFactsMockUp from '../assets/images/mockup.png';
+import faceTheFactsMockUp from '../assets/images/new-mockup.png';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 const Mockup: React.FC = () => (
 	<div>
