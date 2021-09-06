3bcb206f5c7356022a4c8b8f77eae1299bb45a7d
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-09-06 15:46:04 +0200

    Change the image

--- src/components/Mockup.tsx
+++ src/components/Mockup.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-import faceTheFactsMockUp from '../assets/images/Mockup.png';
+import faceTheFactsMockUp from '../assets/images/mockup.png';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 const Mockup: React.FC = () => (
 	<div>
