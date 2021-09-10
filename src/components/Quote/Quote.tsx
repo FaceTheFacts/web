79228f586d1f5b1966c73ff2df727cbc04c75ff1
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-09-10 18:09:27 +0200

    Run formatters

--- src/components/Quote/Quote.tsx
+++ src/components/Quote/Quote.tsx
@@ -1,16 +1,16 @@
-import * as React from 'react';
 import styles from './Quote.module.css';
+import * as React from 'react';
 
 interface QuoteProps {
-  content: string;
-  name: string;
+	content: string;
+	name: string;
 }
 
 const Quote: React.FC<QuoteProps> = (props: QuoteProps) => (
-  <div className={styles.container}>
-    <div>{props.content}</div>
-    <div className={styles.name}>{props.name}</div>
-  </div>
+	<div className={styles.container}>
+		<div>{props.content}</div>
+		<div className={styles.name}>{props.name}</div>
+	</div>
 );
 
 export default Quote;
