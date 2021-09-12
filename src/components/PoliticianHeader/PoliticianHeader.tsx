d131ce8081a2d57adb7d94521ef5a9f43e2b2f6f
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-09-12 20:11:37 +0200

    Rename

--- src/components/PoliticianHeader/PoliticianHeader.tsx
+++ src/components/PoliticianHeader/PoliticianHeader.tsx
@@ -5,27 +5,27 @@ import { Politician } from '../../Types';
 import PartyChip from '../VoteCard/VoteDetails/Table/PartyChips/PartyChip/PartyChip';
 import classNames from 'classnames';
 
-interface ContainerProps {
+interface HeaderProps {
 	candidate: Politician;
 	image: string;
 }
 
-const PoliticianHeader: React.FC<ContainerProps> = ({ candidate, image }: ContainerProps) => {
+const PoliticianHeader: React.FC<HeaderProps> = (props: HeaderProps) => {
 	return (
 		<div className="header">
 			<IonGrid>
 				<IonRow>
 					<IonCol className="profile-image">
-						<img className="politician-image" src={image !== null ? image : Placeholder} alt="placeholder" />
+						<img className="politician-image" src={props.image !== null ? props.image : Placeholder} alt="placeholder" />
 					</IonCol>
 					<IonCol className="profile-details">
 						<div className="politician-name">
-							<IonCardTitle>{candidate.label}</IonCardTitle>
+							<IonCardTitle>{props.candidate.label}</IonCardTitle>
 						</div>
 						<div className="politician-details">
-							<PartyChip party={candidate.party.label} />
+							<PartyChip party={props.candidate.party.label} />
 							{/* Check Toni Jaschinski */}
-							{candidate.occupation.map(
+							{props.candidate.occupation.map(
 								(occupation: string, index: number) => {
 									const ocupationClass = classNames ('politician-detail', occupation.length < 36 ? 'politician-detail-short':'')
 									return (
