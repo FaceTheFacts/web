e6cb3e45fc030a74c510bd60519c1acb5c108b5c
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2021-09-13 08:53:58 +0200

    Outsource Amthor info

---
+++ src/components/Profile/AmthorWeblinks.ts
@@ -0,0 +1,9 @@
+export const AmthorWeblinks = [
+  'http://www.philipp-amthor.de/',
+  'https://www.facebook.com/amthor.philipp/',
+  'https://www.youtube.com/',
+  'https://www.tiktok.com/',
+  'https://twitter.com/',
+  'https://www.abgeordnetenwatch.de/profile/philipp-amthor',
+  'https://en.wikipedia.org/wiki/Philipp_Amthor',
+]
