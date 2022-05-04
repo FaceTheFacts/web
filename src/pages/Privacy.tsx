29a7bec5146fe86ed6062b3dbf5f2ed39b8d3f9d
RichardKruemmel <richard.kruemmel@code.berlin>
2022-05-04 17:55:01 +0200

    Update website privacy policy

--- src/pages/Privacy.tsx
+++ src/pages/Privacy.tsx
@@ -77,53 +77,52 @@ const Privacy: React.FC = () => (
 				gemeinsam mit anderen über die Zwecke und Mittel der Verarbeitung von
 				personenbezogenen Daten (z.B. Namen, E-Mail-Adressen o. Ä.) entscheidet.
 				<h2>3. Allgemeine Hinweise und Pflichtinformationen</h2>
-				Datenschutz Wir als Betreiber dieser Seite nehmen den Schutz Ihrer persönlichen
-				Daten sehr ernst. Wir behandeln Ihre personenbezogenen Daten vertraulich und
-				entsprechend der gesetzlichen Datenschutzvorschriften sowie dieser
-				Datenschutzerklärung. Wenn Sie diese Website benutzen, werden verschiedene
-				personenbezogene Daten erhoben. Personenbezogene Daten sind Daten, mit denen Sie
-				persönlich identifiziert werden können. Die vorliegende Datenschutzerklärung
-				erläutert, welche Daten wir erheben und wofür wir sie nutzen. Sie erläutert auch,
-				wie und zu welchem Zweck das geschieht. Wir weisen darauf hin, dass die
-				Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail)
-				Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff
-				durch Dritte ist nicht möglich. Rechtsgrundlage der Datenverarbeitung Um Ihnen
-				unsere Internetseite und die damit verbundenen Dienstleistungen anbieten zu können,
-				müssen wir personenbezogenen Daten verarbeiten. Diese Verarbeitung basiert auf
-				folgender Rechtsgrundlage: Art. 6 Abs. 1 lit. a DS-GVO dient als Rechtsgrundlage für
-				Verarbeitungsvorgänge, bei denen eine Einwilligung für den spezifischen
-				Verwendungszweck eingeholt wird. Handelt es sich bei den zu verarbeitenden Daten um
-				personenbezogene Daten die für die Erfüllung eines Vertrags erforderlich sind, bei
-				dem Sie selbst Vertragspartei sind, beruht die Verarbeitung dieser Daten auf Art. 6
-				Abs. 1 lit. b DS-GVO. Dies ist z.B. bei der Lieferung von Waren oder der Erbringung
-				sonstiger Leistungen notwendig. Gleiches gilt für Verarbeitungsvorgänge die für die
-				Durchführung vorvertraglicher Maßnahmen benötigt werden, z.B. bei Produktanfragen.
-				Die Verarbeitung von personenbezogenen Daten kann auch aufgrund rechtlicher
-				Verpflichtungen erforderlich werden, z.B. zur Erfüllung von steuerlichen Pflichten.
-				Diese Art der Datenverarbeitung beruht auf Art. 6 Abs. 1 lit. c DS-GVO In
-				Ausnahmefällen kann die Verarbeitung von personenbezogenen Daten notwendig werden,
-				um lebenswichtige Interessen der betroffenen Person oder anderer natürlicher
-				Personen zu wahren. Dies ist z.B. bei Unfällen im Betrieb notwendig, wenn
-				medizinische Hilfe hinzugezogen wird. Die rechtliche Grundlage ergibt sich aus Art.
-				6 Abs. 1 lit. d DS-GVO Personenbezogene Daten können auch auf Basis einer
-				Interessenabwägung verarbeitet werden. Dies ist gemäß Art. 6 Abs. 1 lit. f DS-GVO
-				dann möglich, wenn ein berechtigtes Interesse seitens unseres Unternehmens oder
-				eines Dritten besteht und die Datenverarbeitung dafür zwingend erforderlich ist.
-				Dabei darf das berechtigte Interesse die Grundrechte und Grundfreiheiten des
-				Betroffenen nicht überwiegen. Widerruf Ihrer Einwilligung zur Datenverarbeitung
-				Viele Datenverarbeitungsvorgänge sind nur mit Ihrer ausdrücklichen Einwilligung
-				möglich. Sie können eine bereits erteilte Einwilligung jederzeit widerrufen. Dazu
-				reicht eine formlose Mitteilung per E-Mail an uns. Die Rechtmäßigkeit der bis zum
-				Widerruf erfolgten Datenverarbeitung bleibt vom Widerruf unberührt.
-				Widerspruchsrecht gegen die Datenerhebung in besonderen Fällen sowie gegen
-				Direktwerbung (Art. 21 DSGVO) Wenn die Datenverarbeitung auf Grundlage von Art. 6
-				Abs. 1 lit. e oder f DSGVO erfolgt, haben Sie jederzeit das Recht, aus Gründen, die
-				sich aus Ihrer besonderen Situation ergeben, gegen die Verarbeitung Ihrer
-				personenbezogenen Daten Widerspruch einzulegen; dies gilt auch für ein auf diese
-				Bestimmungen gestütztes Profiling. Die jeweilige Rechtsgrundlage, auf denen eine
-				Verarbeitung beruht, entnehmen Sie dieser Datenschutzerklärung. Wenn Sie Widerspruch
-				einlegen, werden wir Ihre betroffenen personenbezogenen Daten nicht mehr
-				verarbeiten, es sei denn, wir können zwingende schutzwürdige Gründe für die
+				Wir als Betreiber dieser Seite nehmen den Schutz Ihrer persönlichen Daten sehr
+				ernst. Wir behandeln Ihre personenbezogenen Daten vertraulich und entsprechend der
+				gesetzlichen Datenschutzvorschriften sowie dieser Datenschutzerklärung. Wenn Sie
+				diese Website benutzen, werden verschiedene personenbezogene Daten erhoben.
+				Personenbezogene Daten sind Daten, mit denen Sie persönlich identifiziert werden
+				können. Die vorliegende Datenschutzerklärung erläutert, welche Daten wir erheben und
+				wofür wir sie nutzen. Sie erläutert auch, wie und zu welchem Zweck das geschieht.
+				Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der
+				Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz
+				der Daten vor dem Zugriff durch Dritte ist nicht möglich. Rechtsgrundlage der
+				Datenverarbeitung Um Ihnen unsere Internetseite und die damit verbundenen
+				Dienstleistungen anbieten zu können, müssen wir personenbezogenen Daten verarbeiten.
+				Diese Verarbeitung basiert auf folgender Rechtsgrundlage: Art. 6 Abs. 1 lit. a
+				DS-GVO dient als Rechtsgrundlage für Verarbeitungsvorgänge, bei denen eine
+				Einwilligung für den spezifischen Verwendungszweck eingeholt wird. Handelt es sich
+				bei den zu verarbeitenden Daten um personenbezogene Daten die für die Erfüllung
+				eines Vertrags erforderlich sind, bei dem Sie selbst Vertragspartei sind, beruht die
+				Verarbeitung dieser Daten auf Art. 6 Abs. 1 lit. b DS-GVO. Dies ist z.B. bei der
+				Lieferung von Waren oder der Erbringung sonstiger Leistungen notwendig. Gleiches
+				gilt für Verarbeitungsvorgänge die für die Durchführung vorvertraglicher Maßnahmen
+				benötigt werden, z.B. bei Produktanfragen. Die Verarbeitung von personenbezogenen
+				Daten kann auch aufgrund rechtlicher Verpflichtungen erforderlich werden, z.B. zur
+				Erfüllung von steuerlichen Pflichten. Diese Art der Datenverarbeitung beruht auf
+				Art. 6 Abs. 1 lit. c DS-GVO In Ausnahmefällen kann die Verarbeitung von
+				personenbezogenen Daten notwendig werden, um lebenswichtige Interessen der
+				betroffenen Person oder anderer natürlicher Personen zu wahren. Dies ist z.B. bei
+				Unfällen im Betrieb notwendig, wenn medizinische Hilfe hinzugezogen wird. Die
+				rechtliche Grundlage ergibt sich aus Art. 6 Abs. 1 lit. d DS-GVO Personenbezogene
+				Daten können auch auf Basis einer Interessenabwägung verarbeitet werden. Dies ist
+				gemäß Art. 6 Abs. 1 lit. f DS-GVO dann möglich, wenn ein berechtigtes Interesse
+				seitens unseres Unternehmens oder eines Dritten besteht und die Datenverarbeitung
+				dafür zwingend erforderlich ist. Dabei darf das berechtigte Interesse die
+				Grundrechte und Grundfreiheiten des Betroffenen nicht überwiegen. Widerruf Ihrer
+				Einwilligung zur Datenverarbeitung Viele Datenverarbeitungsvorgänge sind nur mit
+				Ihrer ausdrücklichen Einwilligung möglich. Sie können eine bereits erteilte
+				Einwilligung jederzeit widerrufen. Dazu reicht eine formlose Mitteilung per E-Mail
+				an uns. Die Rechtmäßigkeit der bis zum Widerruf erfolgten Datenverarbeitung bleibt
+				vom Widerruf unberührt. Widerspruchsrecht gegen die Datenerhebung in besonderen
+				Fällen sowie gegen Direktwerbung (Art. 21 DSGVO) Wenn die Datenverarbeitung auf
+				Grundlage von Art. 6 Abs. 1 lit. e oder f DSGVO erfolgt, haben Sie jederzeit das
+				Recht, aus Gründen, die sich aus Ihrer besonderen Situation ergeben, gegen die
+				Verarbeitung Ihrer personenbezogenen Daten Widerspruch einzulegen; dies gilt auch
+				für ein auf diese Bestimmungen gestütztes Profiling. Die jeweilige Rechtsgrundlage,
+				auf denen eine Verarbeitung beruht, entnehmen Sie dieser Datenschutzerklärung. Wenn
+				Sie Widerspruch einlegen, werden wir Ihre betroffenen personenbezogenen Daten nicht
+				mehr verarbeiten, es sei denn, wir können zwingende schutzwürdige Gründe für die
 				Verarbeitung nachweisen, die Ihre Interessen, Rechte und Freiheiten überwiegen oder
 				die Verarbeitung dient der Geltendmachung, Ausübung oder Verteidigung von
 				Rechtsansprüchen (Widerspruch nach Art. 21 Abs. 1 DSGVO). Werden Ihre
@@ -211,65 +210,15 @@ const Privacy: React.FC = () => (
 				Wir benutzen keine Analyse-Tools oder Tools von Drittanbietern. Server-Log-Dateien
 				Der Provider der Seiten erhebt und speichert automatisch Informationen in so
 				genannten Server-Log-Dateien, die Ihr Browser automatisch an uns übermittelt. Dies
-				sind: Browsertyp und Browserversion verwendetes Betriebssystem Referrer URL Hostname
-				des zugreifenden Rechners Uhrzeit der Serveranfrage IP-Adresse Eine Zusammenführung
-				dieser Daten mit anderen Datenquellen wird nicht vorgenommen. Die Erfassung dieser
-				Daten erfolgt auf Grundlage von Art. 6 Abs. 1 lit. f DSGVO. Der Websitebetreiber hat
-				ein berechtigtes Interesse an der technisch fehlerfreien Darstellung und der
-				Optimierung seiner Website – hierzu müssen die Server-Log-Files erfasst werden.
-				Webanalyse Newsletter Auf unserer Internetseite besteht die Möglichkeit einen
-				Newsletter zu abonnieren. Welche personenbezogenen Daten bei der Bestellung des
-				Newsletters an den für die Verarbeitung Verantwortlichen übermittelt werden, ergibt
-				sich aus der hierzu verwendeten Eingabemaske. Der Newsletter kann grundsätzlich nur
-				dann empfangen werden, wenn (1) die betroffene Person über eine gültige
-				E-Mail-Adresse verfügt und (2) die betroffene Person sich für den Newsletterversand
-				registriert. Zudem wird erstmalig für den Newsletterversand eingetragene
-				E-Mail-Adresse aus rechtlichen Gründen eine Bestätigungsmail im
-				Double-Opt-In-Verfahren versendet. Diese Bestätigungsmail dient der Überprüfung, ob
-				der Inhaber der E-Mail-Adresse als betroffene Person den Empfang des Newsletters
-				autorisiert hat. Bei der Anmeldung zum Newsletter speichern wir ferner die vom
-				Internet-Service-Provider (ISP) vergebene IP-Adresse des von der betroffenen Person
-				zum Zeitpunkt der Anmeldung verwendeten Computersystems sowie das Datum und die
-				Uhrzeit der Anmeldung. Die Erhebung dieser Daten ist erforderlich, um den(möglichen)
-				Missbrauch der E-Mail-Adresse einer betroffenen Person zu einem späteren Zeitpunkt
-				nachvollziehen zu können und dient deshalb der rechtlichen Absicherung des für die
-				Verarbeitung Verantwortlichen. Die im Rahmen einer Anmeldung zum Newsletter
-				erhobenen personenbezogenen Daten werden ausschließlich zum Versand unseres
-				Newsletters verwendet. Ferner könnten Abonnenten des Newsletters per E-Mail
-				informiert werden, sofern dies für den Betrieb des Newsletter-Dienstes oder eine
-				diesbezügliche Registrierung erforderlich ist, wie dies im Falle von Änderungen am
-				Newsletterangebot oder bei der Veränderung der technischen Gegebenheiten der Fall
-				sein könnte. Der Versand der Newsletter erfolgt mittels des Versanddienstleister
-				„MailChimp“, Rocket Science Group, LLC, 675 Ponce De Leon Ave NE #5000, Atlanta, GA
-				30308, USA. Die Datenschutzbestimmungen des Versanddienstleisters können Sie hier
-				einsehen:{' '}
-				<a href="https://mailchimp.com/legal/privacy/">
-					https://mailchimp.com/legal/privacy/
-				</a>
-				. The Rocket Science Group LLC d/b/a MailChimp ist unter dem Privacy-Shield-Abkommen
-				zertifiziert und bietet hierdurch eine Garantie, das europäisches Datenschutzniveau
-				einzuhalten (
-				<a href="https://www.privacyshield.gov/participant?id=a2zt0000000TO6hAAG&status=Active">
-					https://www.privacyshield.gov/participant?id=a2zt0000000TO6hAAG&status=Active
-				</a>
-				). Der Versanddienstleister wird auf Grundlage unserer berechtigten Interessen gem.
-				Art. 6 Abs. 1 lit. f DSGVO und eines Auftragsverarbeitungsvertrages gem. Art. 28
-				Abs. 3 S. 1 DSGVO eingesetzt. Der Versanddienstleister kann die Daten der Empfänger
-				in pseudonymer Form, d.h. ohne Zuordnung zu einem Nutzer, zur Optimierung oder
-				Verbesserung der eigenen Services nutzen, z.B. zur technischen Optimierung des
-				Versandes und der Darstellung der Newsletter oder für statistische Zwecke verwenden.
-				Der Versanddienstleister nutzt die Daten unserer Newsletterempfänger jedoch nicht,
-				um diese selbst anzuschreiben oder um die Daten an Dritte weiterzugeben. Das
-				Abonnement unseres Newsletters kann durch die betroffene Person jederzeit gekündigt
-				werden. Die Einwilligung in die Speicherung personenbezogener Daten, die die
-				betroffene Person uns für den Newsletterversand erteilt hat, kann jederzeit
-				widerrufen werden. Zum Zwecke des Widerrufs der Einwilligung findet sich in jedem
-				Newsletter ein entsprechender Link. Ferner besteht die Möglichkeit, sich jederzeit
-				auch direkt auf der Internetseite des für die Verarbeitung Verantwortlichen vom
-				Newsletterversand abzumelden oder dies dem für die Verarbeitung Verantwortlichen auf
-				andere Weise mitzuteilen.
+				sind: Browsertyp und Browserversion, verwendetes Betriebssystem, Referrer, URL,
+				Hostname des zugreifenden Rechners, Uhrzeit der Serveranfrage, IP-Adresse. Eine
+				Zusammenführung dieser Daten mit anderen Datenquellen wird nicht vorgenommen. Die
+				Erfassung dieser Daten erfolgt auf Grundlage von Art. 6 Abs. 1 lit. f DSGVO. Der
+				Websitebetreiber hat ein berechtigtes Interesse an der technisch fehlerfreien
+				Darstellung und der Optimierung seiner Website – hierzu müssen die Server-Log-Files
+				erfasst werden.
 				<h2>5. Änderungen der Datenschutzbestimmungen</h2>
-				Diese Datenschutzerklärung ist aktuell gültig und hat den Stand Juli 2021. Um den
+				Diese Datenschutzerklärung ist aktuell gültig und hat den Stand Mai 2022. Um den
 				Schutz Ihrer Daten auch zukünftig bestmöglich gewährleisten zu können behalten wir
 				uns vor, diese Datenschutzerklärung anzupassen, damit sie stets den aktuellen
 				rechtlichen Anforderungen entspricht oder um Änderungen unserer Leistungen in der
