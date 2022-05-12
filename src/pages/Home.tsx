786e21a59719b0ae61508a496af94da77df019c2
RichardKruemmel <richard.kruemmel@code.berlin>
2022-05-12 15:51:41 +0200

    Update Home Component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,9 +4,9 @@ import bmbfLogo from '../assets/images/bmbf.jpeg';
 import PlayStoreLogo from '../assets/images/google-play-badge.png';
 import PrototypeFund from '../assets/images/prototypeFund.png';
 import APKLogo from '../assets/images/apk-btn.svg';
-import Politrack from '../assets/images/politrack.svg';
+import Politrack from '../assets/images/politrack.png';
 import Wiki from '../assets/images/unlock.png';
-import UpdateDeutschland from '../assets/images/updateDeutschland.svg';
+import UpdateDeutschland from '../assets/images/updateDeutschland.png';
 import AppStoreButton from '../components/AppStoreButton';
 import Mockup from '../components/Mockup';
 import Quote from '../components/Quote/Quote';
@@ -25,8 +25,7 @@ const Home: React.FC = () => (
 					<br className="breakline-title" /> irgendwen.
 				</IonCardTitle>
 				<IonCardSubtitle className="home-subtitle">
-					Mit Face the Facts Wahlplakate scannen und <br />
-					herausfinden wofür Politiker:innen wirklich stehen.
+					Alle wichtigen Infos über Politiker:innen in einer App.
 				</IonCardSubtitle>
 				<div className="appstore-button-container">
 					<AppStoreButton
@@ -49,22 +48,26 @@ const Home: React.FC = () => (
 		</div>
 		<div className="quote-grid-container">
 			<Quote
+				url="https://www.focus.de/politik/die-wahrheit-ueber-politiker-neue-app-scannt-wahlplakate-und-zeigt-harte-fakten_id_21297268.html"
 				content={'„Wahlplakate durchschauen: Diese App weiß alles über die Politiker“'}
 				name={'FOCUS Online'}
 			/>
 			<Quote
+				url="https://www.chip.de/news/Face-the-Facts-App-zeigt-Fakten-hinter-Wahlplakaten_183810010.html"
 				content={
 					'“Die Idee hinter der App Face the Facts ist darum so genial wie einfach…“'
 				}
 				name={'chip.de'}
 			/>
 			<Quote
+				url="https://www.businessinsider.de/politik/deutschland/diese-app-scannt-wahlplakate-und-verraet-euch-lebenslaeufe-und-nebentaetigkeiten-von-bundestagskandidaten/"
 				content={
 					'„Unter den Bildungsapps von Apple hat sie es inzwischen schon unter die Top 60 Bildungsapps geschaﬀt.“'
 				}
 				name={'Business Insider Deutschland'}
 			/>
 			<Quote
+				url="https://www.t-online.de/nachrichten/deutschland/bundestagswahl/id_90770982/facethefact-app-von-studenten-hilft-am-wahlplakat-jedem-waehler.html"
 				content={'„Studenten entwickeln App, die jedem Wähler hilft“'}
 				name={'t-online.de'}
 			/>
@@ -72,16 +75,27 @@ const Home: React.FC = () => (
 		<div className="partners">
 			<h2>Partner</h2>
 			<div className="partner-logo-grid-container">
-				<img
-					id="bmbf"
-					src={bmbfLogo}
-					alt="gefördert vom Bundesministerium für Bildung und Forschung"
-				/>
-				<img id="update" src={UpdateDeutschland} alt="Update-Deutschland" />
-				<img id="proto" src={PrototypeFund} alt="Protype-Fund" />
-				<img id="aw" src={Aw} alt="Abgeordnetenwatch" />
-				<img id="wiki" src={Wiki} alt="wikimedia" />
-				<img id="politrack" src={Politrack} alt="Politrack" />
+				<a id="bmbf" href="https://www.bmbf.de/bmbf/de/home/home_node.html">
+					<img
+						src={bmbfLogo}
+						alt="gefördert vom Bundesministerium für Bildung und Forschung"
+					/>
+				</a>
+				<a id="update" href="https://updatedeutschland.org/">
+					<img src={UpdateDeutschland} alt="Update-Deutschland" />
+				</a>
+				<a id="proto" href="https://prototypefund.de/">
+					<img src={PrototypeFund} alt="Protype-Fund" />
+				</a>
+				<a id="aw" href="https://www.abgeordnetenwatch.de/">
+					<img src={Aw} alt="Abgeordnetenwatch" />
+				</a>
+				<a id="wiki" href="https://www.wikimedia.de/unlock/">
+					<img src={Wiki} alt="wikimedia" />
+				</a>
+				<a id="politrack" href="https://politrack.org/#/">
+					<img src={Politrack} alt="Politrack" />
+				</a>
 			</div>
 		</div>
 	</HomeHeaderFooter>
