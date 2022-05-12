fe4653efca0dcf5901bbb3d221fb9d6d03c9a47d
RichardKruemmel <richard.kruemmel@code.berlin>
2022-05-12 15:51:07 +0200

    Add links to quote and fix opacity bug

--- src/components/Quote/Quote.tsx
+++ src/components/Quote/Quote.tsx
@@ -3,12 +3,15 @@ import * as React from 'react';
 
 interface QuoteProps {
 	content: string;
+	url: string;
 	name: string;
 }
 
 const Quote: React.FC<QuoteProps> = (props: QuoteProps) => (
 	<div className={styles.container}>
-		<div>{props.content}</div>
+		<a href={props.url} className={styles.link}>
+			{props.content}
+		</a>
 		<div className={styles.name}>{props.name}</div>
 	</div>
 );
